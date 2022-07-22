9154623be53e0a60a75563ced831e4fb9c6ddf80
SilarceAnoux <silarceanoux@gmail.com>
2022-07-22 08:46:19 +0800

    使用Case Sensitive Paths - Webpack Plugin套件 使import路徑大小寫敏感

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+// 使import路徑大小寫敏感
+// https://www.npmjs.com/package/case-sensitive-paths-webpack-plugin
+CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack: (config) => {
+    config.plugins.push(new CaseSensitivePathsPlugin())
+    return config
+  }
+}
