b35124f04a8e395d8a1302d992ba3167832f1a7a
SilarceAnoux <silarceanoux@gmail.com>
2022-08-08 22:29:35 +0800

    製作小按鈕通用元件

---
+++ components/global/gear/button/smallButton/MySmButton.tsx
@@ -0,0 +1,37 @@
+
+
+
+import style from "./_smallButton.module.scss"
+
+
+
+export default function MyButton(
+  { label, onClick, className, img }:
+    {
+      label: string
+      onClick: () => void
+      className?: string
+      img?: string
+    }) {
+
+
+  return (
+    <button className={`${style.button} ${className || ""}`}
+      onClick={onClick}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      {img && <img src={img} alt="" />}
+      <span >{label}</span>
+    </button>
+  )
+}
+
+
+
+
+
+
+
+
+
+
