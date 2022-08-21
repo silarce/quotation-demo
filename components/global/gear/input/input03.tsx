1fc8dc9dc916c276cbcf5a53f08f277b1fd76ce2
SilarceAnoux <silarceanoux@gmail.com>
2022-08-21 22:33:08 +0800

    製作通用元件input03

---
+++ components/global/gear/input/input03.tsx
@@ -0,0 +1,66 @@
+
+import { ChangeEvent } from "react"
+// 產生隨機字串(作為id)
+import { nanoid } from 'nanoid'
+
+// css
+import style from "./input03.module.scss"
+
+const Input03 = (
+  {  stateValue, placeholder, onChange,
+    id, className, width, labelWidth, gap,
+    disabled
+  }:
+    {
+      stateValue: string | number,
+      onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+      placeholder?: string | false,
+      id?: string | number
+      className?: string
+      width?: string
+      labelWidth?: string
+      gap?: string
+      disabled?: boolean | undefined
+    }) => {
+  // ========================================================
+  className = className ? className : ""
+  // ========================================================
+  if (id === undefined) id = nanoid()
+  id = `${id}`
+
+
+
+  if (placeholder === undefined) placeholder = `請輸入`
+  if (placeholder === false) placeholder = ""
+
+  // ========================================================
+
+  const lableStyle = {
+    width: width ? width : "D",
+    gridTemplateColumns: labelWidth ? `${labelWidth} auto` : "",
+    gap: gap ? gap : "",
+  }
+
+  const styleDisabled = disabled ? style.disabled : ""
+
+  // ========================================================
+
+  return (
+    <label className={`${style.label} ${className} ${styleDisabled}`} htmlFor={id}
+      style={lableStyle}
+    >
+      <input id={id} type="text" placeholder={placeholder}
+        autoComplete="off"
+        value={stateValue}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <hr />
+    </label>
+  )
+}
+
+
+export default Input03
+
+
