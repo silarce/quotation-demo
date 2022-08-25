b0b15195a92636af6507f52c45896346d1b67977
SilarceAnoux <silarceanoux@gmail.com>
2022-08-25 16:29:36 +0800

    刪除沒用的備份檔案，修正型別錯誤

--- pages/domestic/contract/quotation/[quotation].tsx
+++ pages/domestic/contract/quotation/[quotation].tsx
@@ -3,16 +3,21 @@ import { useRouter } from "next/router"
 
 // components
 import QuotationProfile from "components/page/domestic/contract/quotation/quotationProfile"
-
+import QuotationProduction from "components/page/domestic/contract/quotation/quotationProduct"
+import QuotationComponent from "components/page/domestic/contract/quotation/quotationComponent"
+import QuotationAccessory from "components/page/domestic/contract/quotation/quotationAccessory"
 // global gear
 import PageHeader02, { TtagList, TpanelList } from "components/PageHeader/pageHeader02"
 
-
+// hook
+import useProfile from "components/page/domestic/contract/quotation/hook/useProfile"
+import useProduct from "components/page/domestic/contract/quotation/hook/useProduct"
 
 // css
 import style from "./[quotation].module.scss"
 
-
+// fakeData
+import fakeQuotationData from "meta/fakeData/fakeQuotation"
 
 
 
@@ -21,11 +26,24 @@ import style from "./[quotation].module.scss"
 export default function Quotation() {
 
   const router = useRouter()
-  const { quotation } = router.query
+  const { newQuotatinId } = router.query
+
+  // =========================================================
+  // profile
+  const stateQuotation = useProfile(fakeQuotationData.profile)
+  if (stateQuotation.quotation.quotationId === "" && typeof newQuotatinId === "string") {
+    stateQuotation.setQuotation.setQuotationId(newQuotatinId)
+  }
+
+  // product
+
+  const productStates = useProduct(fakeQuotationData.productList)
+
+
 
   // =========================================================
   const tagList: TtagList = [
-    { label: `報價編號 ${quotation}`, onClick: () => alert(quotation) },
+    { label: `報價編號 ${newQuotatinId}`, onClick: () => alert(newQuotatinId) },
     { label: "工程聯絡單", onClick: () => alert("工程聯絡單") },
   ]
   const panelList: TpanelList = [
@@ -33,25 +51,31 @@ export default function Quotation() {
     { type: "myButton", label: "取消", onClick: () => router.back() },
   ]
   // =========================================================
-
+  if (!newQuotatinId) return null
   return (
-    <div>
+    <div className={style.container}>
 
       <PageHeader02 tagList={tagList} panelList={panelList} />
 
       <div className={style.mainContainer}>
+        <div className={style.quotation}> {/* scroll wrapper */}
+          {/* 工程名稱 */}
+          <QuotationProfile stateQuotation={stateQuotation} />
+          {/*  */}
+          <h5 className={style.titleHr}>合約項目</h5>
+          {/* 主產品設定 */}
+          <QuotationProduction productStates={productStates} />
+
+          <div className={style.redWrapper}>
+            {/* 材料配件設定 */}
+            <QuotationComponent productStates={productStates} />
+            <hr />
+            <QuotationAccessory productStates={productStates} />
+          </div>
 
 
-        <div> {/* scroll wrapper */}
-          <QuotationProfile />
 
 
-          {/* <p>合約項目</p> */}
-          {/* <div></div> */}
-          {/* <div>
-          <div></div>
-          <div></div>
-        </div> */}
           {/* <div></div> */}
           {/* <div>
           <div></div>
@@ -67,3 +91,7 @@ export default function Quotation() {
     </div>
   )
 }
+
+
+
+
