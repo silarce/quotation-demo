c08d34cdc7dca5f808767e871e35ba8d9c7b86a3
SilarceAnoux <silarceanoux@gmail.com>
2022-10-04 21:39:30 +0800

    工務部-工程聯絡單 備註部分

--- pages/worksDepartment/contractList/[contractId]/workContactDoc.tsx
+++ pages/worksDepartment/contractList/[contractId]/workContactDoc.tsx
@@ -8,6 +8,7 @@ import ContractNavLayout from "components/page/worksDepartment/contracList/contr
 // component
 import Profile from "components/page/worksDepartment/contracList/contract/workContactDoc/profile"
 import WorkProject from "components/page/worksDepartment/contracList/contract/workContactDoc/workProject"
+import Remark from "components/page/worksDepartment/contracList/contract/workContactDoc/remark"
 
 // css
 import style from "./contract.module.scss"
@@ -17,10 +18,11 @@ import style from "./contract.module.scss"
 export default function WorkContactDoc() {
 
   return (
-      <div className={style.workContactDoc}>
-        <Profile />
-        <WorkProject />
-      </div>
+    <div className={style.workContactDoc}>
+      <Profile />
+      <WorkProject />
+      <Remark />
+    </div>
   )
 }
 // ===========================================================
