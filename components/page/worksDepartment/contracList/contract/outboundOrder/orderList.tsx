d3f74e59d6d0b8ecf0e6f33af210a48341e07aa0
SilarceAnoux <silarceanoux@gmail.com>
2022-10-05 15:59:31 +0800

    工務部-合約-出庫單 剛開始做

---
+++ components/page/worksDepartment/contracList/contract/outboundOrder/orderList.tsx
@@ -0,0 +1,38 @@
+
+import style from "./outboundOrder.module.scss"
+
+
+export default function OrderList() {
+
+
+
+
+
+  return (
+    <div className={style.orderList}>
+      <div className={style.thead}>
+
+      </div>
+
+      <div className={style.tbody}>
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+// =======================================================
+// dndCellConfigOri
+
+
+
+// 要把工務部的部分分出去為一個branch
+
+
+
+
+
+
--- pages/worksDepartment/contractList/[contractId]/outboundOrder.tsx
+++ pages/worksDepartment/contractList/[contractId]/outboundOrder.tsx
@@ -1,3 +1,6 @@
+// 出庫單
+// 出庫單
+// 出庫單
 
 
 
@@ -8,19 +11,26 @@ import React from "react"
 
 
 
-
+import style from "./contract.module.scss"
 
 
 
 export default function OutboundOrder() {
 
   return (
-    <div>
-      <h1>出庫單</h1>
-      <h1>出庫單</h1>
-      <h1>出庫單</h1>
-      <h1>出庫單</h1>
-      <h1>出庫單</h1>
+    <div className={style.outboundOrder}>
+
+      <div className={style.title}>
+        <div>
+          <span>工程編號</span>
+          <span>{"M-1101201"}</span>
+        </div>
+        <div>
+          <span>工程名稱</span>
+          <span>{"台灣日鑛金屬(股)公司~JX金屬台灣彰濱廠房增建工程"}</span>
+        </div>
+      </div>
+
     </div>
   )
 }
