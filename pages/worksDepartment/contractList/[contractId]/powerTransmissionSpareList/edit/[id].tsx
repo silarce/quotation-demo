e2db4f88d6a2bdbda6598ba95b24423b67330297
SilarceAnoux <silarceanoux@gmail.com>
2022-10-24 21:15:08 +0800

    送電備品列表-列表 完成

---
+++ pages/worksDepartment/contractList/[contractId]/powerTransmissionSpareList/edit/[id].tsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function Edit() {
+
+
+
+  return (
+
+    <div>
+      EDIT
+      EDIT
+      EDIT
+      EDIT
+      EDIT
+      EDIT
+    </div>
+
+
+  )
+}
+
+
+
+
+
