33c8cceb9567d6ce431fa930ef9e0d751e93d060
SilarceAnoux <silarceanoux@gmail.com>
2022-10-26 00:06:27 +0800

    調(退)貨單列表>新增 完成

--- pages/worksDepartment/contractList/[contractId]/listOfDeliveryOrders/add.tsx
+++ pages/worksDepartment/contractList/[contractId]/listOfDeliveryOrders/add.tsx
@@ -1,25 +1,175 @@
+import {
+  Dispatch, SetStateAction,
+  useState, useEffect
+} from "react"
+import { useRouter } from "next/router"
 
 
 
+// component
+import PageHeader, { TpanelList } from "components/page/worksDepartment/contracList/contract/gear/PageHeader"
+import Profile from "components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/profile"
+import EditTransfer from "components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/editTransfer"
+import IconEdit from "components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/iconEdit"
+import Signature from "components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/signature"
+
+// css
+import style from "./listOfDeliveryOrders.module.scss"
 
 
 
+export default function Edit() {
+  const [isReady, setIsReady] = useState(false)
+  const router = useRouter()
+  const [editable, setEditable] = useState(true)
+  // ----------------------------------------------------
+  const [profile, setProfile] = useState<Partial<Tprofile>>({})
+  const [transferList, setTransferList] = useState<Ttransfer[]>([])
+  const [signature, setSignature] = useState<Partial<Tsignature>>({})
+
+  const addTransfer = () => {
+    transferList.push(fakeTransferOri())
+    setTransferList([...transferList])
+  }
+  const delTransfer = (index: number) => {
+    transferList.splice(index, 1)
+    setTransferList([...transferList])
+  }
+
+  const init = () => {
+    setProfile(fakeProfileOri())
+    // setTransferList(fakeTransferListOri())
+    setSignature(fakeSignatureOri())
+  }
+
+  useEffect(() => {
+    init()
+    setIsReady(true)
+  }, [])
 
 
 
+  // ----------------------------------------------------
+  const tagCallback = (contractId: string) => {
+    return `新增調(退)貨單 ${contractId}`
+  }
 
-export default function Add() {
+  const panelList: TpanelList = [
+    {
+      type: "redButton",
+      label: "儲存",
+      onClick: () => { alert("test") }
+    },
+    // {
+    //   type: "exportButton",
+    //   label: "匯出調(退)貨憑單",
+    //   onClick: () => alert("test")
+    // },
+    {
+      type: "myButton",
+      label: "返回",
+      onClick: () => router.back()
+    },
+  ]
 
-
+  // ----------------------------------------------------
+  if (!isReady) return null
   return (
-    <div>
-      ADD
-      ADD
-      ADD
-      ADD
-      ADD
-      ADD
-      ADD
+
+    <div className={style.container}>
+
+      <PageHeader panelList={panelList} tagCallback={tagCallback}/>
+
+      <div className={`${style.mainContainer}`}>
+        <div>
+          <Profile data={profile} setData={setProfile}
+            editable={editable} />
+          <EditTransfer
+            transferList={transferList}
+            setTransferList={setTransferList}
+            addTransfer={addTransfer}
+            delTransfer={delTransfer}
+            editable={editable}
+          />
+          <IconEdit />
+          <Signature
+            signature={signature}
+            setSignature={setSignature}
+            editable={editable}
+          />
+        </div>
+      </div>
+
     </div>
+
+
   )
 }
+
+// ===========================================================
+
+export type Tprofile = {
+  // id: string
+  projectId: string
+  projectName: string
+  neededDate: string
+  applyDate: string
+}
+
+
+const fakeProfileOri = (): Tprofile => ({
+  // id: "111001",
+  projectId: "",
+  projectName: "台中港加工處理區-宇隆科技廠房增建工程A",
+  neededDate: "",
+  applyDate: "",
+})
+
+
+// ===========================================================
+
+export type Ttransfer = {
+  itemName: string
+  material: string
+  qty: string
+  reason: string
+}
+
+const fakeTransferListOri = (): Ttransfer[] => {
+  const transferOri = () => ({
+    itemName: "SD1",
+    material: "不鏽鋼304#",
+    qty: "1",
+    reason: "退貨理由退貨理由退貨理由退貨理由退貨理由退貨理由退貨理由",
+  })
+  return Array(4)
+    .fill(undefined)
+    .map(() => transferOri())
+}
+
+const fakeTransferOri = (): Ttransfer => ({
+  itemName: "",
+  material: "",
+  qty: "",
+  reason: "",
+})
+
+// ===========================================================
+
+export type Tsignature = {
+  會計: string
+  倉庫: string
+  廠務主管: string
+  單位主管: string
+  填表: string
+}
+
+const fakeSignatureOri = (): Tsignature => ({
+  會計: "",
+  倉庫: "",
+  廠務主管: "",
+  單位主管: "",
+  填表: "",
+})
+
+
