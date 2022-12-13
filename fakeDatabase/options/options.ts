429a6c32648e9fa664071d42673b410e12b1d402
Silarce <silarceanoux@gmail.com>
2022-12-14 01:21:14 +0800

    options.ts 新增optionsCreator_prodClass函數

--- fakeDatabase/options/options.ts
+++ fakeDatabase/options/options.ts
@@ -54,6 +54,19 @@ export const optionsCreator_customerCategory =
   ]
 
 
+// 類別
+
+export const optionsCreator_prodClass =
+  (): Toption[] => [
+    { value: "防火防煙捲門系列", label: "防火防煙捲門系列" },
+    { value: "防水防洪門系列", label: "防水防洪門系列" },
+    { value: "抗風防颱捲門系列", label: "抗風防颱捲門系列" },
+    { value: "廠辦管制門", label: "廠辦管制門" },
+    { value: "圍牆大門", label: "圍牆大門" },
+    { value: "機械門", label: "機械門" },
+    { value: "客製化", label: "客製化" },
+  ]
+
 // 門型
 export const optionsCreator_doorType =
   (): Toption[] => [
