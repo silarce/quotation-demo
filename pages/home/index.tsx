c75bdfc05264e8a31cfd609bd816736bbd28b6ea
Silarce <silarceanoux@gmail.com>
2022-12-13 17:10:33 +0800

    把首頁改回為空白頁

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -14,52 +14,11 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <div className={styles.foo}>
         <h1>首頁</h1>
-
-
-        <div style={{
-          width: "400px",
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          gap: "50px"
-        }}>
-          <InputSel
-            label="ABC"
-            placeholder='測試測試測試測試'
-            inputProps={
-              {
-                value: "2222",
-                onChange: (value: string) => { },
-              }
-            }
-          />
-          <InputSel
-            label="ABC"
-            placeholder='測試'
-            selectProps={
-              {
-                value: { label: "AA", value: "aa" },
-                // value: undefined,
-                options: fooArr,
-                onChange: (a) => { console.log(a?.value) },
-              }
-            }
-          />
-        </div>
-
-
       </div>
     </div>
   )
 }
 
 export default Home
-
-
-
-const fooArr = [
-  { label: "AAA", value: "aaa" },
-  { label: "BBB", value: "bbb" },
-  { label: "CCC", value: "ccc" },
-]
 
 
