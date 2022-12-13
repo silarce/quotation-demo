a52d45075970bc216d44930b7faa7bc372e8c95a
Silarce <silarceanoux@gmail.com>
2022-12-13 17:10:57 +0800

    新增lab頁面，測試用

---
+++ pages/lab/index.tsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+import scss from "./index.module.scss"
+
+
+
+
+export default function Lab() {
+
+
+  return (
+    <div>
+
+
+      <div className={scss.foo}>
+
+        <img src="/_next/static/media/doorRail75.46dfc626.svg" alt="" />
+        <img src="/_next/static/media/doorRail75.46dfc626.svg" alt="" />
+        <img src="/_next/static/media/doorRail75.46dfc626.svg" alt="" />
+        
+      </div>
+
+
+    </div>
+  )
+
+
+}
+
+
+
+
+
