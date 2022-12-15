fc161eb835d134f65104b4db8f56ef1e24e2ee7d
Silarce <silarceanoux@gmail.com>
2022-12-15 16:28:29 +0800

    建立 ERP功能權限與ERP操作權限 檔案

---
+++ pages/setting/hrManage/erpCtrlPermissions/index.tsx
@@ -0,0 +1,33 @@
+// ERP操作權限
+// ERP操作權限
+// ERP操作權限
+
+
+
+import scss from "./erpCtrlPermissions.module.scss"
+
+
+
+
+
+
+export default function ErpCtrlPermissions() {
+
+
+
+  return (
+    <div>
+
+      <h1>ERP操作權限</h1>
+
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
---
+++ pages/setting/hrManage/erpFuncPermissions/index.tsx
@@ -0,0 +1,33 @@
+// ERP功能權限
+// ERP功能權限
+// ERP功能權限
+
+
+
+import scss from "./erpFuncPermissions.module.scss"
+
+
+
+
+
+
+export default function ErpFuncPermissions() {
+
+
+
+  return (
+    <div>
+
+      <h1>ERP功能權限</h1>
+
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
