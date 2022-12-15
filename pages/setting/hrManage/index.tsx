ee7717ebe06400aa81d8f9e34bdecb9f33656676
Silarce <silarceanoux@gmail.com>
2022-12-15 20:21:13 +0800

    刪除多餘程式碼

--- pages/setting/hrManage/index.tsx
+++ pages/setting/hrManage/index.tsx
@@ -39,26 +39,6 @@ export default function HrManage() {
     setSelIndex(selIndex)
   }
 
-  // --------------------------------------------------------------------------
-
-
-
-  const linkArr = [
-    {
-      label: "人事權限管理",
-      href: "/setting/hrManage",
-    },
-    {
-      label: "ERP功能權限",
-      href: "/setting/hrManage/erpFuncPermissions",
-    },
-    {
-      label: "ERP操作權限",
-      href: "/setting/hrManage/erpCtrlPermissions",
-    },
-  ]
-
-
 
   // --------------------------------------------------------------------------
   return (
