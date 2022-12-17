07ba109324c47ac572ac4655db74854aa8a11564
Silarce <silarceanoux@gmail.com>
2022-12-17 22:54:29 +0800

    ERP操作權限 複製人員資料的元件來用

---
+++ components/page/setting/hrManage/table/table.tsx
@@ -0,0 +1,186 @@
+import {
+  MouseEvent,
+  useState, useContext
+} from "react"
+import Link from "next/link";
+
+
+
+
+// global gear
+import CellWithBar from "components/global/gear/cell/cellWithBar"
+import TwoButtonModal from "components/global/gear/modal/simpleModal/twoButtonModal"
+import { ModalSuccess, ModalErr } from "components/global/gear/modal/simpleModal/alertModals";
+import { setRootLoading } from "components/global/gear/loadingCover/rootLoadingCover";
+// api
+import { apiDeleteEmployee } from "js/api/api_employee";
+
+// icon
+import { IconDelete01 } from 'public/image/icon/svgComponent/svgIcons';
+// css
+import scss from "./table.module.scss"
+// type
+import { Temployee } from "js/api/api_employee"
+
+
+
+export default function Table({ employeeList, toUpdate }: {
+  employeeList: Temployee[]
+  toUpdate: () => void
+}) {
+
+
+  const [selInfo, setSelInfo] = useState({
+    id: "",
+    idNumber: "",
+    chName: ""
+  })
+  const closeDelPanel = () => {
+    setSelInfo({
+      id: "",
+      idNumber: "",
+      chName: ""
+    })
+  }
+
+  const openDelPanel = (e: MouseEvent, data: Temployee) => {
+    e.stopPropagation()
+    const { id, idNumber, chName } = data
+    setSelInfo({ id, idNumber, chName })
+  }
+
+  const deleteEmployee = async () => {
+    if (!selInfo.id) return
+    try {
+      setRootLoading(true)
+      await apiDeleteEmployee(selInfo.id)
+      await toUpdate()
+      ModalSuccess({ title: "刪除完成" })
+    }
+    catch {
+      await toUpdate()
+      ModalErr({ title: "刪除失敗" })
+    }
+    finally {
+      setRootLoading(false)
+      closeDelPanel()
+    }
+  }
+
+  return (
+    <div className={scss.employeeList}>
+      <div className={scss.thead}>
+        {tableKeyIndex.map((key, index) => {
+          const { label, width, flex } = tableConfig[key]
+          const theStyle = { width, flex }
+          return (
+            <div className={scss.column} key={index}
+              style={theStyle}>
+              <span>{label}</span>
+            </div>
+          )
+        })}
+      </div>
+
+      <div className={scss.tbody}>
+        {employeeList.map((row, index) => {
+          const id = row.id
+          return (
+            <CellWithBar key={index}>
+              <Link href={`/setting/employees/edit/${id}`}>
+                <div className={scss.row} >
+                  {tableKeyIndex.map((key, index) => {
+                    const data = row[key]
+                    const { width, flex } = tableConfig[key]
+                    const theStyle = { width, flex }
+
+                    if (key === "jobs" && Array.isArray(data)) {
+
+
+                      return (
+                        <div className={`${scss.column} ${scss.departmentInfo}`} key={index}
+                          style={theStyle}
+                        >
+                          {data.map((item, index) => {
+                            const { grade, name } = item
+                            const department = item.department
+                            const { name: departmentName } = department
+                            return (
+                              <div key={index}>
+                                {`字母 / ${departmentName} / ${name} / Level${grade}`}
+                              </div>
+                            )
+                          })}
+                        </div>
+                      )
+                    }
+
+                    if (typeof data === "string")
+                      return (
+                        <div className={scss.column} key={index}
+                          style={theStyle}
+                        >
+                          <span>{data ?? "無資料"}</span>
+                        </div>
+                      )
+                  })}
+
+                  <div className={`${scss.column} ${scss.btnCell}`}>
+                    <IconDelete01 onClick={(e) => { openDelPanel(e, row) }} />
+                  </div>
+                </div>
+              </Link>
+            </CellWithBar>
+          )
+        })}
+      </div>
+      <TwoButtonModal
+        {...{
+          visible: !!selInfo.id,
+          text: `請確定要刪除「${selInfo.idNumber}」「${selInfo.chName}」?`,
+          onConfirm: deleteEmployee,
+          onCancel: closeDelPanel,
+        }} />
+    </div>
+  )
+}
+
+// ============================================================
+
+type TtableKeysIndex = keyof Pick<Temployee,
+  "idNumber" | "chName" | "phone1" | "jobs">
+
+const tableKeyIndex: TtableKeysIndex[] = [
+  "idNumber", "chName", "phone1", "jobs"
+]
+
+const tableConfig
+  : {
+    [key in TtableKeysIndex]: {
+      label: string
+      width: string
+      flex?: string
+    }
+  }
+  = {
+  "idNumber": {
+    label: "使用者代號",
+    width: "150px"
+  },
+  "chName": {
+    label: "姓名",
+    width: "150px"
+  },
+  "phone1": {
+    label: "電話",
+    width: "170px"
+  },
+  "jobs": {
+    label: "部門編號/部門名稱/職稱/職等",
+    width: "auto",
+    flex: "auto"
+  },
+}
+
+
+
--- pages/setting/hrManage/erpCtrlPermissions/index.tsx
+++ pages/setting/hrManage/erpCtrlPermissions/index.tsx
@@ -2,12 +2,21 @@
 // ERP操作權限
 // ERP操作權限
 
-
-import { useState } from "react"
+import {
+  useState, useEffect
+} from "react"
+import { useRouter } from "next/router";
 
 // component
+import Table from "components/page/setting/hrManage/table/table";
+
+// gear
 import Header from "components/page/setting/hrManage/header/header"
 import TwoButtonModal from "components/global/gear/modal/simpleModal/twoButtonModal"
+import LoadingCover01 from "components/global/gear/loadingCover/loadingCover01";
+
+// api
+import { useEmployee, TapiGetEmployeeParams } from "js/api/api_employee";
 
 
 import scss from "./erpCtrlPermissions.module.scss"
@@ -16,11 +25,47 @@ import scss from "./erpCtrlPermissions.module.scss"
 
 
 
-
+// ==========================================================================
 export default function ErpCtrlPermissions() {
 
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isReady, setIsReady] = useState(false)
+  // ------------------------------------------------------------------------
+  const [params, setParams] = useState<TapiGetEmployeeParams>({
+    order: "ASC",
+    page: 1,
+    pageSize: 12,
+    filter: {
+      $or: {
+        idNumber: {
+          $contains: router.query.searchValue,
+        },
+        chName: {
+          $contains: router.query.searchValue,
+        },
+      }
+    },
+    populate: ["jobs.department"]
+  })
+
+  let { data, update } = useEmployee(params)
+  const employeeList = data?.data || []
+  const meta = data?.meta
+
+  // ------------------------------------------------------------------------
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      await update()
+      setIsReady(true)
+      setIsLoading(false)
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params])
 
 
+  // ------------------------------------------------------------------------
   return (
     <div className={scss.container}>
       <div className={scss.header}>
@@ -32,10 +77,12 @@ export default function ErpCtrlPermissions() {
         </div>
       </div>
 
-      <div className={scss.mainContainer}>
-        <div>
+      <div className={scss.mainContainer} >
+        <div className={scss.main}>
+          <Table
+            employeeList={employeeList} toUpdate={update}
+          />
 
-          
         </div>
       </div>
 
