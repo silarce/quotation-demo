76c62cff8e770f2abf664ff074790a56888737aa
Silarce <silarceanoux@gmail.com>
2022-12-17 21:10:57 +0800

    ERP功能權限 製作各個按鈕功能。靜態頁面完成

--- pages/setting/hrManage/erpFuncPermissions/index.tsx
+++ pages/setting/hrManage/erpFuncPermissions/index.tsx
@@ -2,10 +2,13 @@
 // ERP功能權限
 // ERP功能權限
 
+import { useState } from "react"
 
 // component
 import Header from "components/page/setting/hrManage/header/header"
 import Card from "components/page/setting/hrManage/card/card"
+import GridPanel from "components/page/setting/hrManage/modal/gridSelector.tsx/gridPanel"
+import TwoButtonModal from "components/global/gear/modal/simpleModal/twoButtonModal"
 
 // css
 import scss from "./erpFuncPermissions.module.scss"
@@ -19,30 +22,94 @@ export default function ErpFuncPermissions() {
 
 
 
+  const [showPanel, setShowPanel] = useState(false)
+
+  const openPanel = () => {
+    setShowPanel(true)
+  }
+
+  const panelOnCancel = () => {
+    setShowPanel(false)
+  }
+  const panelOnConfirm = (indexArr: number[]) => {
+    alert("被選擇的index" + indexArr.join(","))
+    setShowPanel(false)
+  }
+
+  // --------------------------------------------------------------------------
+
+
+  // 製作removeData功能
+  // 製作removeData功能
+  // 製作removeData功能
+  // 製作removeData功能
+  // 製作removeData功能
+  const [preDelData, setPreDelData] = useState<TdepartmentInfo>()
+
+  const [showDelete, setShowDelete] = useState(false)
+
+  const openDelete = (department: TdepartmentInfo) => {
+    setPreDelData(department)
+    setShowDelete(true)
+  }
+
+  const confirmDelete = () => {
+    if (!preDelData) return;
+
+    const { label } = preDelData
+    alert(`移除${label}`)
+  }
+
+
+
+  // --------------------------------------------------------------------------
   return (
     <div className={scss.container}>
 
       <Header />
 
       <div className={scss.mainContainer}>
-
         <div>
           {indexKey.map((key, index) => {
             const { label } = config[key]
             const dataArr = fakeData[key].map((item) => item.label)
+
+            const removeData = (index: number) => {
+              openDelete(fakeData[key][index])
+            }
+
             return (
               <Card key={index}
                 label={label}
                 addLabel={"新增管理部門"}
                 noDataTip="尚未新增管理部門"
                 dataArr={dataArr}
-                showAdd={() => { }}
-                removeData={() => { alert("未串接API，目前無功能") }}
+                showAdd={openPanel}
+                removeData={removeData}
               />
             )
           })}
         </div>
       </div>
+
+      <GridPanel
+        visible={showPanel}
+        title="請選擇部門"
+        note="可複選"
+        dataArr={fakeDepartments}
+        onCancel={panelOnCancel}
+        onConfirm={panelOnConfirm}
+      />
+
+      <TwoButtonModal
+        visible={showDelete}
+        setVisible={setShowDelete}
+        // 用刪除的字眼不準確，應該是請確認是否從xxx移除yy部
+        // 串接api時再處理
+        text={`請確認是否刪除${preDelData?.label}`}
+        onConfirm={confirmDelete}
+        onCancel={() => setShowDelete(false)}
+      />
 
     </div>
   )
@@ -196,7 +263,24 @@ const fakeData: TfakeData = {
   ],
 }
 
+// ============================================================================
 
+const fakeDepartments = [
+  "管理部",
+  "管理部",
+  "管理部",
+  "管理部",
+
+  "管理部",
+  "管理部",
+  "管理部",
+  "管理部",
+
+  "管理部",
+  "管理部",
+  "管理部",
+  "管理部",
+]
 
 
 
