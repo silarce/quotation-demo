60b0a951e19be85747c45d477f0e1d5d1a9bace3
Silarce <silarceanoux@gmail.com>
2022-12-25 00:03:33 +0800

    公司設定>產品列表 開始製作列表

---
+++ components/page/setting/productList/table/table.tsx
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+// css
+import scss from "./table.module.scss"
+
+
+export default function Table() {
+
+
+
+  return (
+    <div>
+      <h1>table</h1>
+      <h1>table</h1>
+      <h1>table</h1>
+      <h1>table</h1>
+      <h1>table</h1>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
