a91c6e437d769093bc18f6b5ea2cce1ef7372205
Silarce <silarceanoux@gmail.com>
2022-12-26 19:42:52 +0800

    公司設定>人事權限管理>人事權限管理 新增刪除前的確認提醒

--- pages/setting/hrManage/index.tsx
+++ pages/setting/hrManage/index.tsx
@@ -14,18 +14,21 @@ import Card from "components/page/setting/hrManage/card/card";
 import { showRootLoading } from "components/global/gear/loadingCover/rootLoadingCover";
 import LoadingCover01 from "components/global/gear/loadingCover/loadingCover01";
 import myAlert from "components/global/gear/modal/simpleModal/alertModals";
+import TwoButtonModal from "components/global/gear/modal/simpleModal/twoButtonModal";
 
 // css
 import scss from "./hrManage.module.scss"
 
 // api
 import {
+  TdepartmentDto,
   useDepartments_managers,
   apiPostDepartments_id_managers, apiDeleteDepartments_id_managers,
 } from "js/api/api_department";
 import { useEmployee, TemployeeDto } from "js/api/api_employee";
 
 
+// ==========================================================================
 export default function HrManage() {
   const [isLoading, setIsLoading] = useState(false)
   // --------------------------------------------------------------------------
@@ -63,19 +66,12 @@ export default function HrManage() {
   }, [])
   // --------------------------------------------------------------------------
 
-  const [showAddManager, setShowAddManager] = useState(false)
   // 被選中部門的id
   const [selDepartmentId, setDelDepartmentId] = useState<string>()
+  // 打開新增管理人員面板
+  const showAdd = (departmentId: string) => setDelDepartmentId(departmentId)
 
-  const showAdd = (departmentId: string) => {
-    setDelDepartmentId(departmentId)
-    setShowAddManager(true)
-  }
-
-
-  // --------------------------------------------------------------------------
-
-
+  // 確定新增
   const onConfirm = async (indexArr: number[]) => {
     if (!selDepartmentId) return;
 
@@ -83,12 +79,13 @@ export default function HrManage() {
     employeeList.forEach((item, index) => {
       if (indexArr?.includes(index)) selEmployeeIdArr.push(item.id)
     })
+
     const body = { employeeIds: selEmployeeIdArr }
 
     showRootLoading(true)
     try {
       await apiPostDepartments_id_managers(selDepartmentId, body)
-      setShowAddManager(false)
+      setDelDepartmentId(undefined)
     }
     catch (err) {
       myAlert.err({ title: "新增管理人員失敗", content: err as string })
@@ -97,8 +94,41 @@ export default function HrManage() {
 
     await doUpdate()
   }
+  // 關閉新增管理人員面板
+  const onCancel = () => setDelDepartmentId(undefined)
 
-  const onCancel = () => setShowAddManager(false)
+  // --------------------------------------------------------------------------
+  // 刪除功能
+
+  type TdelTarget = {
+    department: TdepartmentDto
+    employee: TemployeeDto
+  }
+
+  // 設定刪除目標的同時會叫出確定面板
+  const [delTarget, setDelTarget] = useState<TdelTarget>()
+
+  // 確定刪除
+  const delConfirm = async () => {
+    if (!delTarget) return
+    const departmentId = delTarget.department.id
+    const employeeId = delTarget.employee.id
+    const body = { employeeIds: [employeeId] }
+    showRootLoading(true)
+    try {
+      await apiDeleteDepartments_id_managers(departmentId, body)
+      setDelTarget(undefined)
+    }
+    catch (err) {
+      myAlert.err({ title: "刪除管理人員失敗", content: err as string })
+    }
+    showRootLoading(false)
+    await doUpdate()
+  }
+  // 取消刪除
+  const delCancel = () => {
+    setDelTarget(undefined)
+  }
 
   // --------------------------------------------------------------------------
   return (
@@ -112,18 +142,11 @@ export default function HrManage() {
             const { id, name, employees } = item
 
             const removeData = async (itemIndex: number) => {
-              const employeeId = employees?.[itemIndex].id
-              if (!employeeId) return
-              const body = { employeeIds: [employeeId] }
-              showRootLoading(true)
-              try {
-                await apiDeleteDepartments_id_managers(id, body)
-              }
-              catch (err) {
-                myAlert.err({ title: "刪除管理人員失敗", content: err as string })
-              }
-              showRootLoading(false)
-              await doUpdate()
+              // 設定刪除目標
+              setDelTarget({
+                department: item,
+                employee: employees![itemIndex]
+              })
             }
 
             return (
@@ -143,13 +166,21 @@ export default function HrManage() {
       </div>
 
       <SelectEmployeePanel
-        visible={showAddManager}
+        visible={!!selDepartmentId}
         label="請選擇管理人員"
         tip="可複選"
         employeeList={employeeList}
         onConfirm={onConfirm}
         onCancel={onCancel}
       />
+
+      <TwoButtonModal
+        visible={!!delTarget}
+        text={`請確定要從「${delTarget?.department.name}」移除「${delTarget?.employee.chName}」?`}
+        onConfirm={delConfirm}
+        onCancel={delCancel}
+      />
+
     </div>
   )
 }
