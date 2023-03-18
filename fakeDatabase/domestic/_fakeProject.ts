0c290f3e20bffb4f3126feeeaacd2ce1f8cc9bba
Silarce <silarceanoux@gmail.com>
2023-03-19 00:58:22 +0800

    feat: 國內工程 報價單 建立新的假資料

--- fakeDatabase/domestic/_fakeProject.ts
+++ fakeDatabase/domestic/_fakeProject.ts
@@ -7,26 +7,77 @@
 // clientName: string
 
 
-type Tproject = {
+type Tquotation = {
   [key: string]: {
     quotationId: string //報價單Id // 報價編號
     tempQuotationAging: number // 報價時效 天數 顯示`${quotationAging}天內`
     date: string  //報價日期
-    projectName: string // 專案(報價單)名稱
+    constructionName: string // 工程名稱
     undertaker: string //承辦人
-    discount: number // 總折數
+    totalDiscount: number // 總折數
     tempDoorQty: number// 橖數
     tempBudgetAmount: number //合約金額
-    projectCounty: string // 工程地點城市
-    projectDistrict: string // 工程地點行政區
-    projectAddress: string // 工程地點剩餘地址
-    trackingStatus: string // 追蹤狀態
-    siteProgress: string // 工地進度
-
-    quoStatus: "預算" | "投標" | "發包" | "合約"
-
-    // 客戶資料id
-    clientId: string,
+    constructionCounty: string // 工程地點城市
+    constructionDistrict: string // 工程地點行政區
+    constructionAddress: string // 工程地點剩餘地址
+    trackingStatus: string // 追蹤狀態 輸入字串
+    siteProgress: string // 工地進度 輸入字串
+    quoStatus: "預算" | "投標" | "發包" | "合約" // 報價單狀態，會使報價單出現在不同的頁面
+    // doorType :string // doorType可能是複數，還不確定怎麼不處理
+    // ---------------------------------------------------------
+    mainProduct: {
+      discount: number
+      category: string // 項目
+      series: string // 報價別 // 下拉式選單
+      L: number
+      W: number
+      h: number
+      B: number // 下拉式選單
+      doorType: string // 下拉式選單
+      material: string // 下拉式選單
+      surface: string // 下拉式選單
+      doorRail: string // 下拉式選單
+      horsepower: string // 下拉式選單
+      qty: number
+      memo: string
+      ejectionDoor: boolean
+      typhoonProof: boolean
+      unitWeight: number
+      part: {
+        partType: string
+        partName: string
+        partId: string
+        material: string
+        basicWeight: number | undefined
+        unit: "m2" | "M" | "組" | "套" | "支" | string
+        // qty: number // 從mainProduct的資料運算出來
+        listPrice: number  // 牌價
+      }[]
+    }[]
+    // ---------------------------------------------------------
+    accessory: {
+      accessoryId: string
+      name: string
+      unit: "M" | "組" | string
+      qty: number
+      listPrice: number
+      totalListPrice: number
+      price: number
+      totalPrice: number
+    }[]
+    // ---------------------------------------------------------
+    sinature: {
+      manager: string
+      director: string
+      attn: string
+    }
+    // ---------------------------------------------------------
+    // 
+    clientId: string, // 客戶id 用來關聯客戶資料以取得客戶名稱、聯絡人、電話那些資料
+    memoIdArr: number[]
+    rangeIdArr: number[]
+    // 
+    // ---------------------------------------------------------
     tempRecord: {
       date: string
       discount: string
@@ -37,89 +88,92 @@ type Tproject = {
   }
 }
 
-const fakeProjectData: Tproject = {
+const fakeProjectData: Tquotation = {
   "S-110211-01": {
+    // ---------------------------------------------------------
     quotationId: "S-110211-01",
     tempQuotationAging: 10,
     date: "110-02-02",
-    projectName: "台灣日鑛金屬(股)公司~JX金屬台灣彰濱廠房增建工程",
+    constructionName: "台灣日鑛金屬(股)公司~JX金屬台灣彰濱廠房增建工程",
     undertaker: "陳小明小華",
-    discount: 99.99,
+    totalDiscount: 99.99,
     tempDoorQty: 99,
     tempBudgetAmount: 999999,
-    projectCounty: "臺北市",
-    projectDistrict: "大安區",
-    projectAddress: "什麼什麼路",
-    trackingStatus: "",
-    siteProgress: "",
+    constructionCounty: "臺北市",
+    constructionDistrict: "大安區",
+    constructionAddress: "什麼什麼路",
+    trackingStatus: "", // 追蹤狀態 輸入字串
+    siteProgress: "", //  工地進度 輸入字串
     quoStatus: "預算",
-    clientId: "S00001",
-    tempRecord: [
+    // ---------------------------------------------------------
+    mainProduct: [
       {
-        date: "111-01-02",
-        discount: "88.88",
-        doorQty: "88",
-        budgetAmount: "888,888",
-        Remark: "備註備註備註備註",
-      },
+        discount: 100,
+        category: "SD1",
+        series: "不是捲門",
+        L: 3,
+        W: 0,
+        h: 2,
+        B: 3.5,
+        doorType: "SJ-302", //門型
+        material: "SST304#",
+        surface: "2B",
+        doorRail: "60",
+        horsepower: "1/4 HP",
+        qty: 1,
+        memo: "防颱",
+        ejectionDoor: false,
+        typhoonProof: false,
+        unitWeight: 22,
+        part: [
+          {
+            partType: "SJ00",
+            partName: "捲門片",
+            partId: "SJ0000A0000",
+            material: "SST 304",
+            basicWeight: 99.99,
+            unit: "m2",
+            listPrice: 499,
+          },
+          {
+            partType: "SJ00",
+            partName: "馬達機",
+            partId: "SJ0000A0000",
+            material: "SST 304",
+            basicWeight: 99.99,
+            unit: "組",
+            listPrice: 499,
+          }
+        ]
+      }
+    ],
+    // ---------------------------------------------------------
+    accessory: [
       {
-        date: "111-01-01",
-        discount: "77.77",
-        doorQty: "77",
-        budgetAmount: "777,777",
-        Remark: "備註備註備註備註",
+        accessoryId: "SJ0A09",
+        name: "鋁合金障礙感知器",
+        unit: "M",
+        qty: 1,
+        listPrice: 999,
+        totalListPrice: 999,
+        price: 999,
+        totalPrice: 999,
       },
-    ]
-  },
-  "S-110211-02": {
-    quotationId: "S-110211-02",
-    tempQuotationAging: 10,
-    date: "110-02-05",
-    projectName: "台灣東西南北雜衣(股)公司~東拼西湊大拍賣企劃",
-    undertaker: "陳小明小華",
-    discount: 88.88,
-    tempDoorQty: 88,
-    tempBudgetAmount: 888888,
-    projectCounty: "桃園市",
-    projectDistrict: "楊梅區",
-    projectAddress: "什麼什麼路",
-    trackingStatus: "",
-    siteProgress: "",
-    quoStatus: "預算",
-    clientId: "S00002",
-    tempRecord: [
-      {
-        date: "111-01-02",
-        discount: "88.88",
-        doorQty: "88",
-        budgetAmount: "888,888",
-        Remark: "備註備註備註備註",
-      },
-      {
-        date: "111-01-01",
-        discount: "77.77",
-        doorQty: "77",
-        budgetAmount: "777,777",
-        Remark: "備註備註備註備註",
-      },
-    ]
-  },
-  "S-110211-03": {
-    quotationId: "S-110211-03",
-    tempQuotationAging: 10,
-    date: "110-03-12",
-    projectName: "有間客棧大飯店五百周年慶暨北海分館開幕儀式企劃",
-    undertaker: "陳小明小華",
-    discount: 77.77,
-    tempDoorQty: 77,
-    tempBudgetAmount: 777777,
-    projectCounty: "新竹市",
-    projectDistrict: "北區",
-    projectAddress: "什麼什麼路",
-    trackingStatus: "",
-    siteProgress: "",
-    quoStatus: "預算",
-    clientId: "S00003",
+    ],
+    // ---------------------------------------------------------
+    sinature: {
+      manager: "王小明",
+      director: "李小華",
+      attn: "林小善",
+    },
+    // ---------------------------------------------------------
+    // 
+    clientId: "S00001", // 客戶id 用來關聯客戶資料
+    memoIdArr: [3, 4, 5],
+    rangeIdArr: [3, 4, 5],
+    // 
+    // ---------------------------------------------------------
+    // tempRecord 暫時先這樣，之後要改用編號關聯其他的資料
     tempRecord: [
       {
         date: "111-01-02",
@@ -141,7 +195,7 @@ const fakeProjectData: Tproject = {
 
 
 
-function checkData(projectData: Tproject): void {
+function checkData(projectData: Tquotation): void {
   for (const [key, value] of Object.entries(projectData)) {
     if (key !== value.quotationId) {
       // 建立資料時quotationId必須要與其所屬物件的key相符
@@ -152,10 +206,150 @@ function checkData(projectData: Tproject): void {
 
 checkData(fakeProjectData)
 
-export type { Tproject }
+export type { Tquotation }
 export { fakeProjectData }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fakeProjectData: Tproject = {
+//   "S-110211-01": {
+//     quotationId: "S-110211-01",
+//     tempQuotationAging: 10,
+//     date: "110-02-02",
+//     projectName: "台灣日鑛金屬(股)公司~JX金屬台灣彰濱廠房增建工程",
+//     undertaker: "陳小明小華",
+//     discount: 99.99,
+//     tempDoorQty: 99,
+//     tempBudgetAmount: 999999,
+//     projectCounty: "臺北市",
+//     projectDistrict: "大安區",
+//     projectAddress: "什麼什麼路",
+//     trackingStatus: "",
+//     siteProgress: "",
+//     quoStatus: "預算",
+//     clientId: "S00001",
+//     tempRecord: [
+//       {
+//         date: "111-01-02",
+//         discount: "88.88",
+//         doorQty: "88",
+//         budgetAmount: "888,888",
+//         Remark: "備註備註備註備註",
+//       },
+//       {
+//         date: "111-01-01",
+//         discount: "77.77",
+//         doorQty: "77",
+//         budgetAmount: "777,777",
+//         Remark: "備註備註備註備註",
+//       },
+//     ]
+//   },
+//   "S-110211-02": {
+//     quotationId: "S-110211-02",
+//     tempQuotationAging: 10,
+//     date: "110-02-05",
+//     projectName: "台灣東西南北雜衣(股)公司~東拼西湊大拍賣企劃",
+//     undertaker: "陳小明小華",
+//     discount: 88.88,
+//     tempDoorQty: 88,
+//     tempBudgetAmount: 888888,
+//     projectCounty: "桃園市",
+//     projectDistrict: "楊梅區",
+//     projectAddress: "什麼什麼路",
+//     trackingStatus: "",
+//     siteProgress: "",
+//     quoStatus: "預算",
+//     clientId: "S00002",
+//     tempRecord: [
+//       {
+//         date: "111-01-02",
+//         discount: "88.88",
+//         doorQty: "88",
+//         budgetAmount: "888,888",
+//         Remark: "備註備註備註備註",
+//       },
+//       {
+//         date: "111-01-01",
+//         discount: "77.77",
+//         doorQty: "77",
+//         budgetAmount: "777,777",
+//         Remark: "備註備註備註備註",
+//       },
+//     ]
+//   },
+//   "S-110211-03": {
+//     quotationId: "S-110211-03",
+//     tempQuotationAging: 10,
+//     date: "110-03-12",
+//     projectName: "有間客棧大飯店五百周年慶暨北海分館開幕儀式企劃",
+//     undertaker: "陳小明小華",
+//     discount: 77.77,
+//     tempDoorQty: 77,
+//     tempBudgetAmount: 777777,
+//     projectCounty: "新竹市",
+//     projectDistrict: "北區",
+//     projectAddress: "什麼什麼路",
+//     trackingStatus: "",
+//     siteProgress: "",
+//     quoStatus: "預算",
+//     clientId: "S00003",
+//     tempRecord: [
+//       {
+//         date: "111-01-02",
+//         discount: "88.88",
+//         doorQty: "88",
+//         budgetAmount: "888,888",
+//         Remark: "備註備註備註備註",
+//       },
+//       {
+//         date: "111-01-01",
+//         discount: "77.77",
+//         doorQty: "77",
+//         budgetAmount: "777,777",
+//         Remark: "備註備註備註備註",
+//       },
+//     ]
+//   },
+// }
