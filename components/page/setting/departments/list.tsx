0da09d16b5a7e1bb43320c127824552c7ae6c69d
Silarce <silarceanoux@gmail.com>
2023-04-06 21:00:18 +0800

    feat: 公司設定/公司職等職稱 調整部門input的顏色還有部門代號input的文字大小與底線

--- components/page/setting/departments/list.tsx
+++ components/page/setting/departments/list.tsx
@@ -1,5 +1,6 @@
 
 import { MouseEvent, } from 'react';
+import classNames from 'classnames';
 
 // component
 import AutosizeInput from 'react-input-autosize';
@@ -10,7 +11,7 @@ import iconAdd from "public/image/icon/add.svg"
 import { RedoOutlined } from '@ant-design/icons';
 
 // css
-import style from "./departments.module.scss"
+import scss from "./departments.module.scss"
 
 // type
 import { ClassDepartment } from 'pages/setting/departments';
@@ -34,20 +35,20 @@ export default function List(
 
   // 這個return裡面根據不同的情況有三種return
   return (
-    <div className={style.list}>
-      <div className={style.row}>
+    <div className={scss.list}>
+      <div className={scss.row}>
         {CdepartmentArr.map((department, dIndex) => {
           const { id, name, jobs, isFocus,
             dWillDelete, dWillPatch, dIsNew,
             dShowDeletePanel, addJob, removeJob,
           } = department
 
-          let styleHead: string = `${style.cell} ${style.head}`
+          let styleHead: string = `${scss.cell} ${scss.head}`
 
           if (dWillDelete) styleHead
-            = `${styleHead} ${style.delete}`
+            = `${styleHead} ${scss.delete}`
 
-          if (isFocus) styleHead = `${styleHead} ${style.isFocus}`
+          if (isFocus) styleHead = `${styleHead} ${scss.isFocus}`
 
           const DeleteSwitch = dWillDelete ? RedoOutlined : IconCross01
 
@@ -57,10 +58,10 @@ export default function List(
           }
 
           return (
-            <div className={`${style.columns}`} key={dIndex}>
+            <div className={`${scss.columns}`} key={dIndex}>
               {/* 建立在thead裡面*/}
               <div className={styleHead}>
-                <label className={style.inputBox}>
+                <label className={scss.inputBox}>
                   <AutosizeInput type="text"
                     value={name}
                     onChange={(e) => { department.name = e.target.value }}
@@ -69,12 +70,15 @@ export default function List(
                     onBlur={() => department.isFocus = false}
                   />
                   {editable &&
-                    <DeleteSwitch className={style.iconCross01}
+                    <DeleteSwitch className={scss.iconCross01}
                       onClick={editable ? onDelete : undefined} />
                   }
 
                 </label>
-                <label className={style.inputBox}>
+
+                <label className={classNames(
+                  scss.inputBox,
+                  { [scss.notEditable]: !editable || dWillDelete })}>
                   <AutosizeInput type="text"
                     defaultValue={"A" + dIndex + 1}
                     // value={name}
@@ -84,7 +88,7 @@ export default function List(
                     onBlur={() => department.isFocus = false}
                   />
                 </label>
-                <div className={style.focusBg} />
+                <div className={scss.focusBg} />
               </div>
 
               {/* ============================ */}
@@ -92,8 +96,8 @@ export default function List(
               {jobs.map((job, jIndex) => {
 
                 if (job === undefined) {
-                  const styleDelete = dWillDelete ? style.delete : ""
-                  let className = `${style.cell} ${style.empty} ${styleDelete}`
+                  const styleDelete = dWillDelete ? scss.delete : ""
+                  let className = `${scss.cell} ${scss.empty} ${styleDelete}`
 
                   return (
                     <div className={className} key={jIndex}
@@ -114,14 +118,14 @@ export default function List(
                   jIsNew ? removeJob(jIndex) : jShowDeletePanel(e)
                 }
 
-                let className: string = style.cell
-                if (jWillDelete || dWillDelete) className = `${style.cell} ${style.delete}`
-                if (isFocus) className = `${style.cell} ${style.isFocus}`
+                let className: string = scss.cell
+                if (jWillDelete || dWillDelete) className = `${scss.cell} ${scss.delete}`
+                if (isFocus) className = `${scss.cell} ${scss.isFocus}`
                 const DeleteSwitch = jWillDelete ? RedoOutlined : IconCross01
 
                 return (
                   <div className={className} key={jIndex}>
-                    <label className={style.inputBox}>
+                    <label className={scss.inputBox}>
                       <AutosizeInput type="text"
                         value={name ?? ""}
                         onChange={(e) => job.name = e.target.value}
@@ -130,11 +134,11 @@ export default function List(
                         onBlur={() => { job.isFocus = false }}
                       />
                       {editable && !dWillDelete &&
-                        <DeleteSwitch className={style.iconCross01}
+                        <DeleteSwitch className={scss.iconCross01}
                           onClick={onDelete} />
                       }
                     </label>
-                    <div className={style.focusBg} />
+                    <div className={scss.focusBg} />
                   </div>
                 )
               })}
@@ -143,7 +147,7 @@ export default function List(
         })}
 
       </div>{/* coulmns */}
-      <div className={style.rowBg} />
+      <div className={scss.rowBg} />
 
     </div>
   )
