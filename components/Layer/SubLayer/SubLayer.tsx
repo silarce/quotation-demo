5b40fe458a46d7e5a1f780988e1acd5875907350
Silarce <silarceanoux@gmail.com>
2023-04-12 13:43:02 +0800

    feat: 製作布局元件，取代原本用css module做主要內容布局的作法

---
+++ components/Layer/SubLayer/SubLayer.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import classNames from "classnames"
+
+import scss from "./subLayer.module.scss"
+
+export default function SubLayer(
+  { children, className, bodyClassName }:
+    {
+      children: React.ReactNode
+      className?: string
+      bodyClassName?: string
+    }
+) {
+  const childredArr = React.Children.toArray(children)
+  return (
+    <div className={classNames(scss.container, className)}>
+      {childredArr[0]}
+
+      <div className={classNames(scss.body, bodyClassName)}>
+        {childredArr[1]}
+      </div>
+
+    </div>
+  )
+}
