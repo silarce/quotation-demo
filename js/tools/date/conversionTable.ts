fb9c316ac450ed6d9687cfb3be6f20c26c9ee8f8
Silarce <silarceanoux@gmail.com>
2023-04-14 22:21:46 +0800

    feat: 製作年、月、地區的optionCreator以及搭配使用的方法

--- js/tools/date/conversionTable.ts
+++ js/tools/date/conversionTable.ts
@@ -18,7 +18,21 @@ const month_chToNumber = {
   十一: "11",
   十二: "12",
 }
+const month_numberToCh = {
+  "1": "一",
+  "2": "二",
+  "3": "三",
+  "4": "四",
+  "5": "五",
+  "6": "六",
+  "7": "七",
+  "8": "八",
+  "9": "九",
+  "10": "十",
+  "11": "十一",
+  "12": "十二",
+}
 
 
-export { month_chToNumber }
+export { month_chToNumber, month_numberToCh }
 
