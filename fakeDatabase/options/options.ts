5920c3c4bdbf01a89c08f42546d150fc3df930dd
Silarce <silarceanoux@gmail.com>
2023-04-19 16:58:55 +0800

    feat: options.js 新增addEmpty函式，並應用到其中三個option上

--- fakeDatabase/options/options.ts
+++ fakeDatabase/options/options.ts
@@ -30,6 +30,13 @@ interface ToptionPlus {
   options: Toption[]
 }
 
+/**會改變原本的陣列 */
+const addEmpty = (optionArr: Toption[]) => {
+  optionArr.unshift({ value: "", label: "不拘" })
+}
+
+// =======================================================================
+
 
 // 性別
 export const optionsCreator_gender =
@@ -63,37 +70,53 @@ export const optionsCreator_taxDeductionCategory =
 
 // 類別
 export const optionsCreator_prodClass =
-  (): Toption[] => [
-    { value: "", label: "不拘" },
-    { value: "防火防煙捲門系列", label: "防火防煙捲門系列" },
-    // { value: "防水防洪門系列", label: "防水防洪門系列" },
-    // { value: "抗風防颱捲門系列", label: "抗風防颱捲門系列" },
-    // { value: "廠辦管制門", label: "廠辦管制門" },
-    // { value: "圍牆大門", label: "圍牆大門" },
-    // { value: "機械門", label: "機械門" },
-    // { value: "客製化", label: "客製化" },
-  ]
+  (props: { haveEmpty?: boolean } = {}
+  ): Toption[] => {
+    const { haveEmpty } = props
+    const arr = [
+      { value: "防火防煙捲門系列", label: "防火防煙捲門系列" },
+      // { value: "防水防洪門系列", label: "防水防洪門系列" },
+      // { value: "抗風防颱捲門系列", label: "抗風防颱捲門系列" },
+      // { value: "廠辦管制門", label: "廠辦管制門" },
+      // { value: "圍牆大門", label: "圍牆大門" },
+      // { value: "機械門", label: "機械門" },
+      // { value: "客製化", label: "客製化" },
+    ]
+    if (haveEmpty) addEmpty(arr)
+    return arr
+  }
 
 // 門型
 export const optionsCreator_doorType =
-  (): Toption[] => [
-    { value: "", label: "不拘" },
-    { value: "SJ-302", label: "SJ-302" },
-    // { value: "SJ-302A", label: "SJ-302A" },
-    // { value: "SJ-302AS", label: "SJ-302AS" },
-    // { value: "SJ-305D", label: "SJ-305D" },
-    // { value: "SJ-312", label: "SJ-312" },
-    // { value: "SJ-120A", label: "SJ-120A" },
-    // { value: "SJ-303S", label: "SJ-303S" },
-  ]
+  (props: { haveEmpty?: boolean } = {}
+  ): Toption[] => {
+    const { haveEmpty } = props
+    const arr = [
+      { value: "SJ-302", label: "SJ-302" },
+      // { value: "SJ-302A", label: "SJ-302A" },
+      // { value: "SJ-302AS", label: "SJ-302AS" },
+      // { value: "SJ-305D", label: "SJ-305D" },
+      // { value: "SJ-312", label: "SJ-312" },
+      // { value: "SJ-120A", label: "SJ-120A" },
+      // { value: "SJ-303S", label: "SJ-303S" },
+    ]
+    if (haveEmpty) addEmpty(arr)
+    return arr
+  }
 
 // 門的形式
 export const optionsCreator_doorForm =
-  (): Toption[] => [
-    { value: "", label: "不拘" },
-    { value: "一般", label: "一般" },
-    { value: "防颱", label: "防颱" },
-  ]
+  (props: { haveEmpty?: boolean } = {}
+  ): Toption[] => {
+    const { haveEmpty } = props
+    const arr = [
+      // { value: "", label: "不拘" },
+      { value: "一般", label: "一般" },
+      { value: "防颱", label: "防颱" },
+    ]
+    if (haveEmpty) addEmpty(arr)
+    return arr
+  }
 
 // 客戶列表搜尋用
 export const optionsCreator_clientSearch =
