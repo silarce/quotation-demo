0cda16fb7af1ce792ce31756cf7d017146d55599
Silarce <silarceanoux@gmail.com>
2023-04-19 21:52:33 +0800

    feat: optionWithIcon.tsx 調整html結構使其可以受到css的影響

--- components/global/gear/select/optionWithIcon.tsx
+++ components/global/gear/select/optionWithIcon.tsx
@@ -1,25 +1,28 @@
 
 
 import { components } from "react-select";
+
+import { OptionProps, }
+  from 'react-select';
+import { GroupBase } from 'react-select/dist/declarations/src/types.d';
+
+import type { Toption } from "fakeDatabase/options/options"
+
 const { Option } = components
 
 import style from "./optionWithIcon.module.scss"
 
 
-
-
-export function OptionWithIcon01(props: any) {
+export function OptionWithIcon01(props: OptionProps<Toption, false, GroupBase<Toption>>) {
 
   const { data } = props
   const { label, icon } = data
 
   return (
-    <Option {...props}>
-      <div className={style.option}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        {icon && <img src={icon} alt="iconImg" />}
-        <span>{label}</span>
-      </div>
+    <Option {...props} className={style.option}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      {icon && <img src={icon} alt="iconImg" />}
+      <span>{label}</span>
     </Option>
   )
 }
