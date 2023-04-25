115ff1a48cb058a7aa9ca9eb3720f262de365571
Silarce <silarceanoux@gmail.com>
2023-04-25 16:24:55 +0800

    fix: 把忘記拿掉的測試Error註解掉

--- pages/demo/addCustomers/index.tsx
+++ pages/demo/addCustomers/index.tsx
@@ -170,7 +170,7 @@ function ExcelReader() {
 
       setIsUploading(true)
       try {
-        if (index === 3) throw Error
+        // if (index === 3) throw Error
         const response = await apiPostCustomers(body)
         console.log("項次", data.項次, "完成", "---", `筆數 ${index + 1}/${dataArr.length}`)
         console.log("response", response)
