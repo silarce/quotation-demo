89c69387f87f7bce4067f2c7725d19d8436d714e
Silarce <silarceanoux@gmail.com>
2023-04-28 23:12:38 +0800

    feat: 製作checkButton.tsx元件

---
+++ components/global/gear/button/checkButton.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import classNames from "classnames"
+
+import Image from "next/image"
+
+// icon
+import iconCheck from "public/image/icon/check.svg"
+
+// css
+import scss from "./_button.module.scss"
+import scss2 from "./checkButton.module.scss"
+
+export default function CheckButton(
+  {
+    checkLabel,
+    uncheckLable,
+    onClick,
+    className,
+    px }:
+    {
+      checkLabel?: string
+      uncheckLable?: string
+      onClick: (isCheck: boolean) => void
+      className?: string
+      px?: "px22" | "px44" | "px2227"
+    }) {
+
+  const [isCheck, setIsCheck] = useState(false)
+
+  const theOnClick = () => {
+    onClick(isCheck)
+    setIsCheck(state => !state)
+  }
+
+  const label = isCheck ? checkLabel : uncheckLable
+
+  return (
+    <button className={classNames(scss.button, px && scss[px], className)}
+      onClick={theOnClick}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <div className={classNames(scss2.checkbox)}>
+        {isCheck && <Image src={iconCheck} alt="check" />}
+      </div>
+
+      {label && <span >{label}</span>}
+
+    </button>
+  )
+}
+
+// ======================================================
+
