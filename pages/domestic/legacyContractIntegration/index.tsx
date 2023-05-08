053befc2a4ea19f67e7b9ab66a612a690cba1fea
Silarce <silarceanoux@gmail.com>
2023-05-08 18:41:59 +0800

    feat:建立舊合約整合頁面
    /domestic/legacyContractIntegration

---
+++ pages/domestic/legacyContractIntegration/index.tsx
@@ -0,0 +1,40 @@
+
+
+import SubLayer from "components/Layer/SubLayer/SubLayer"
+import PageHeader02 from "components/PageHeader/PageHeader02/PageHeader02"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function LegacyContractIntegration() {
+
+
+  return (
+    <SubLayer>
+
+      <PageHeader02 tag="舊合約" />
+
+      <div>
+        舊合約
+
+      </div>
+    </SubLayer>
+  )
+
+
+}
