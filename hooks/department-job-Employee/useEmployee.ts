bfcebafb0fc9e031b9e484ef9d9bdd31651cac01
Silarce <silarceanoux@gmail.com>
2023-05-25 17:57:02 +0800

    fix: 公司設定 人員資料 修正日期的問題，變更日期的處理辦法

--- hooks/department-job-Employee/useEmployee.ts
+++ hooks/department-job-Employee/useEmployee.ts
@@ -27,25 +27,18 @@ class Class_employee {
 
     if (!this._employeeData.qualifications) this._employeeData.qualifications = []
 
-    this._employeeData.birthday = convertDate(this._employeeData.birthday)
+    // this._employeeData.birthday = convertDate(this._employeeData.birthday)
+    this._employeeData.birthday = convertDate_reduce1911(this._employeeData.birthday)
 
     this._employeeData.startDate = (() => {
       if (!this._employeeData.startDate) {
-        const today = moment().format("yyyy-MM-DD")
-        this._employeeData.startDate = today
+        return convertDate_reduce1911(new Date().toISOString())
       }
-
-      // const foo = convertDate(this._employeeData.startDate)
-      // console.log(this._employeeData.startDate)
-      // console.log(foo)
-
-      return convertDate(this._employeeData.startDate)
+      return convertDate_reduce1911(this._employeeData.startDate)
     })()
-
-    this._employeeData.leaveDate = convertDate(this._employeeData.leaveDate)
-    this._employeeData.retireDate = convertDate(this._employeeData.retireDate)
-    this._employeeData.severanceDate = convertDate(this._employeeData.severanceDate)
-
+    this._employeeData.leaveDate = convertDate_reduce1911(this._employeeData.leaveDate)
+    this._employeeData.retireDate = convertDate_reduce1911(this._employeeData.retireDate)
+    this._employeeData.severanceDate = convertDate_reduce1911(this._employeeData.severanceDate)
 
     this.classJobGroupArr
       = this._employeeData.jobs.map((job) => new Class_JobGroup(reRender, job))
@@ -67,7 +60,6 @@ class Class_employee {
     this.classJobGroupArr.splice(index, 1)
     this._reRender()
   }
-
 
   get idNumber() {
     return this._employeeData.idNumber
@@ -213,26 +205,20 @@ class Class_employee {
     this._reRender()
   }
 
-
   get seniority() {
-    const mStartDate = moment(this.startDate, "yyyy-MM-DD") //到職日
+    const mStartDate = moment(this.startDate) //到職日
     if (!this.startDate) return "請輸入到職日"
     if (this.leaveDate || this.severanceDate) {
-      const mEndDate = moment(this.leaveDate || this.severanceDate, "yyyy-MM-DD")
+      const mEndDate = moment(this.leaveDate || this.severanceDate)
       const duration = moment.duration(mEndDate.diff(mStartDate))
       return `${duration.years()}年${duration.months()}月${duration.days()}天`
     }
     else {
-      const mNow = moment(new Date(), "yyyy-MM-DD").subtract(1911, "year")
+      const mNow = moment(new Date()).subtract(1911, "year")
       const duration = moment.duration(mNow.diff(mStartDate))
       return `${duration.years()}年${duration.months()}月${duration.days()}天`
     }
   }
-
-  // set seniority(v: string) {
-  //   // this._employeeData.seniority = v
-  //   // this._reRender()
-  // }
 
   get startDate() {
     return this._employeeData.startDate
@@ -327,34 +313,16 @@ class Class_employee {
       = this.qualifications.filter((item) => !!item.name)
     this._reRender()
 
-    const convertToDate = (dateString: string) => {
-      if (!dateString) return ""
-
-      const chDateArr = dateString.split("-")
-      if (chDateArr[0].length === 4 && chDateArr[0][0] === "0") {
-        chDateArr[0] = chDateArr[0].replace("0", "")
-      }
-      const chDate = chDateArr.join("-")
-
-      const theDateString = yearConversion_chToStandard(chDate)
-
-      if (!theDateString) return ""
-      return moment(theDateString).toDate()
-    }
-
     const birthday =
-      convertToDate(this._employeeData.birthday)
+      convertDate_add1911(this._employeeData.birthday)
     const startDate =
-      convertToDate(this._employeeData.startDate)
-
-    // console.log(startDate)
-
+      convertDate_add1911(this._employeeData.startDate)
     const leaveDate =
-      convertToDate(this._employeeData.leaveDate)
+      convertDate_add1911(this._employeeData.leaveDate)
     const retireDate =
-      convertToDate(this._employeeData.retireDate)
+      convertDate_add1911(this._employeeData.retireDate)
     const severanceDate =
-      convertToDate(this._employeeData.severanceDate)
+      convertDate_add1911(this._employeeData.severanceDate)
 
     // 直接傳date物件的話，時區會被轉變
     return {
@@ -368,7 +336,6 @@ class Class_employee {
       severanceDate: severanceDate,
     }
   }
-
 } // Class_employee
 
 // ====================================================================
@@ -473,12 +440,20 @@ const emptyDataOri = (): TemployeeDto_jobs => ({
 })
 // =======================================================================
 
-const convertDate = (dateString: string) => {
-  if (!dateString) return "";
-  dateString =
-    moment(dateString).tz('Asia/Taipei').format("YYYY-MM-DD")
-  const chDateString = yearConversion_standardToCh(dateString)
-  // const formatedDateString = moment(chDateString).format("YYYY-MM-DD")
-  return chDateString
+const convertDate_reduce1911 = (ISOString: string) => {
+  if (!ISOString) return ISOString
+  const dateTime = new Date(ISOString)
+  const year = dateTime.getFullYear()
+  const chYear = year - 1911
+  dateTime.setFullYear(chYear)
+  return dateTime.toISOString()
 }
 
+const convertDate_add1911 = (ISOString: string) => {
+  if (!ISOString) return ISOString
+  const dateTime = new Date(ISOString)
+  const twYear = dateTime.getFullYear()
+  const year = twYear + 1911
+  dateTime.setFullYear(year)
+  return dateTime.toISOString()
+}
