eb6e832dd569e2faed083ce898733ccaafaf819a
Silarce <silarceanoux@gmail.com>
2023-05-30 17:52:42 +0800

    chore: 修改next.config.js的images參數。
    使next.js的Image可以正常運作

--- next.config.js
+++ next.config.js
@@ -11,8 +11,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'akamai',
-    path: '/',
+    unoptimized: true,
     domains: ["sanjeou-erp-be.caprover.credot-web.com"],
   },
 }
