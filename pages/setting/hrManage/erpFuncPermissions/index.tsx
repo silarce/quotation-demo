07026ff966819b3b60227dc8be2025ee09e3e7ea
Silarce <silarceanoux@gmail.com>
2023-05-30 10:38:42 +0800

    feat: ERP功能權限 完成新的介面

--- pages/setting/hrManage/erpFuncPermissions/index.tsx
+++ pages/setting/hrManage/erpFuncPermissions/index.tsx
@@ -9,13 +9,16 @@ import SubLayer from "components/Layer/SubLayer/SubLayer"
 
 // component
 import PageHeader02 from "components/PageHeader/PageHeader02/PageHeader02";
-import Card from "components/page/setting/hrManage/card/card"
 import GridPanel from "components/page/setting/hrManage/modal/gridSelector.tsx/gridPanel"
 
 // gear
 import { setRootLoading } from "components/global/gear/loadingCover/rootLoadingCover"
 import LoadingCover01 from "components/global/gear/loadingCover/loadingCover01"
 import TwoButtonModal from "components/global/gear/modal/simpleModal/twoButtonModal"
+import MyButton from "components/global/gear/button/myButton";
+
+// icon
+import { IconRemoveCircle } from "public/image/icon/svgComponent/svgIcons"
 
 // api
 import {
@@ -34,6 +37,7 @@ import myAlert from "components/global/gear/modal/simpleModal/alertModals"
 
 // config
 import { hrManageLinkArr } from "components/page/setting/hrManage/hrManageLinkArr";
+import CellWithBar from "components/global/gear/cell/cellWithBar";
 
 
 // =============================================================================
@@ -108,9 +112,7 @@ export default function ErpFuncPermissions() {
 
   const confirmDelete = async () => {
     if (!erpData_del || !departmentData_del) return;
-
     const body = { departmentIds: [departmentData_del.id] }
-
     try {
       setRootLoading(true)
       await apiDeleteErpFeatures_id_departments(erpData_del.id, body)
@@ -135,30 +137,50 @@ export default function ErpFuncPermissions() {
       <PageHeader02 linkList={hrManageLinkArr} />
 
       <div className={scss.body}>
-        <div>
-          {erpArr.map((erp, index) => {
+        {erpArr.map((erp) => {
+          const { departments: depArr, id: erpId, name } = erp
+          const removeData = (index: number) => {
+            if (isLoading) return
+            setErpData_del(erp)
+            setDepartmentData_del(depArr[index])
+          }
+          return (
+            <div key={erpId} className={scss.card}>
+              <div className={scss.left}><span>{name}</span></div>
+              <div className={scss.right}>
+                {depArr.map((dep, index) => {
+                  const { id: depId, name: depName } = dep
+                  return (
+                    <CellWithBar key={depId} className={scss.row}>
+                      <div><span>{depName}</span></div>
+                      <div >
+                        <IconRemoveCircle className={scss.removeBtn}
+                          onClick={() => removeData(index)} />
+                      </div>
+                    </CellWithBar>
+                  )
+                })}
+                <div className={scss.row_btn}>
+                  <CellWithBar>
+                    <MyButton className={scss.btn}
+                      label="新增管理部門"
+                      preImg="add"
+                      onClick={() => openAddPanel(erpId)}
+                    />
+                  </CellWithBar>
+                  <CellWithBar>
+                    <MyButton className={scss.btn}
+                      label="新增管理人員"
+                      preImg="add"
+                      onClick={() => alert("test")}
+                    />
+                  </CellWithBar>
+                </div>
+              </div>
 
-            const { departments, id, name } = erp
-            const demparmentsNameArr = departments.map((item) => item.name)
-
-            const removeData = (index: number) => {
-              if (isLoading) return
-              setErpData_del(erp)
-              setDepartmentData_del(departments[index])
-            }
-
-            return (
-              <Card key={index}
-                label={name}
-                addLabel={"新增管理部門"}
-                noDataTip="尚未新增管理部門"
-                dataArr={demparmentsNameArr}
-                showAdd={() => openAddPanel(id)}
-                removeData={removeData}
-              />
-            )
-          })}
-        </div>
+            </div>
+          )
+        })}
         {/* <LoadingCover01 isLoading={isLoading} /> */}
       </div>
 
@@ -173,7 +195,7 @@ export default function ErpFuncPermissions() {
 
       <TwoButtonModal
         visible={!!erpData_del && !!departmentData_del}
-        text={`請確認是否從${"ERP_name"}移除${departmentData_del?.name}`}
+        text={`請確認是否從${erpData_del?.name}移除${departmentData_del?.name}`}
         onConfirm={confirmDelete}
         onCancel={() => cancelDelete()}
       />
