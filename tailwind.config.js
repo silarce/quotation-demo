2646d2776072f4dfc1132a975ddadd32093e9b9d
Silarce <silarceanoux@gmail.com>
2023-06-05 11:18:20 +0800

    chore: 設定tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      xxl: { max: "1439px" },
+      // => @media (max-width: 1439px) { ... }
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
   },
   plugins: [],
 }
