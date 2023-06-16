8c61b05706b755996379976a719c70dca43354fd
Silarce <silarceanoux@gmail.com>
2023-06-16 15:02:56 +0800

    feat: 備註列表 新增類別選項

--- js/utils/options/productOptions.ts
+++ js/utils/options/productOptions.ts
@@ -16,11 +16,12 @@ export const optionsCreator_category =
     const arr = [
       { value: "防火防煙捲門系列", label: "防火防煙捲門系列" },
       { value: "防水防洪門系列", label: "防水防洪門系列" },
-      // { value: "抗風防颱捲門系列", label: "抗風防颱捲門系列" },
-      // { value: "廠辦管制門", label: "廠辦管制門" },
-      // { value: "圍牆大門", label: "圍牆大門" },
-      // { value: "機械門", label: "機械門" },
-      // { value: "客製化", label: "客製化" },
+      { value: "抗風防颱捲門系列", label: "抗風防颱捲門系列" },
+      { value: "上折門", label: "上折門" },
+      { value: "廠辦管制門", label: "廠辦管制門" },
+      { value: "圍牆大門", label: "圍牆大門" },
+      { value: "機械門", label: "機械門" },
+      { value: "客製化", label: "客製化" },
     ]
     if (haveEmpty) addEmpty(arr)
     return arr
