055134b040ad87e9b53206e0b8c4bb7c1a795f3d
Silarce <silarceanoux@gmail.com>
2023-06-28 23:06:16 +0800

    feat: InputSel元件 製作、新增checkBar模組

---
+++ components/global/gear/inputAndSel/cog/checkBar.tsx
@@ -0,0 +1,108 @@
+import {
+  ChangeEvent, InputHTMLAttributes, CSSProperties, FocusEvent,
+  HTMLInputTypeAttribute,
+  Dispatch, SetStateAction,
+  useState, useMemo, useEffect,
+} from "react"
+import classNames from "classnames";
+
+// antd
+import { Checkbox } from 'antd';
+
+// css
+import scss from "../inputSel.module.scss"
+
+
+type TcheckProps = {
+  propsList: {
+    [key: string]: {
+      value: boolean
+      label: string
+      disabled?: boolean
+      className?: string
+      style?: CSSProperties
+    }
+  }
+  onChange: (v: Tcheck) => void,
+  checkStyle?: CSSProperties
+}
+
+type Tcheck = { [key: string]: boolean }
+
+export type { TcheckProps }
+
+export default function CheckBar(
+  {
+    checkProps,
+
+  }:
+    {
+      checkProps: TcheckProps
+    }
+) {
+
+  const { propsList, onChange, checkStyle } = checkProps
+
+
+  const [checkList, setCheckList] = useState<Tcheck>({})
+
+
+  const keyArr = useMemo(() => {
+    return Object.keys(propsList)
+  }, [propsList])
+
+  useEffect(() => {
+    const list: Tcheck = {}
+    keyArr.forEach((key) => {
+      list[key] = propsList[key].value
+    })
+    setCheckList(list)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [propsList])
+
+
+  useEffect(() => {
+    onChange(checkList)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [checkList])
+
+  return (
+    <div className={classNames(scss.checkBar)}>
+      {keyArr.map((key) => {
+        const value = checkList[key]
+        const { label, disabled, className, style } = propsList[key]
+        const onChange = (v: boolean) => {
+          const copy = { ...checkList }
+          copy[key] = v
+          setCheckList(copy)
+        }
+        return (
+          <label key={key} style={{ ...checkStyle, ...style }}
+            className={classNames(scss.wrapper, className)} >
+            <span>{label}</span>
+            <Checkbox className={classNames(scss.antdCheck)} checked={value}
+              onChange={(e) => onChange(e.target.checked)}
+              disabled={disabled}
+            />
+          </label>
+        )
+      })}
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
