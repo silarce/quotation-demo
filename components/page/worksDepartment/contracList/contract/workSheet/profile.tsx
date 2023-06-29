a375307ca25836325892d8a8dfe64bdd3d4d140c
Silarce <silarceanoux@gmail.com>
2023-06-29 17:23:26 +0800

    feat: 工務部/合約/工作表 完成profile

---
+++ components/page/worksDepartment/contracList/contract/workSheet/profile.tsx
@@ -0,0 +1,122 @@
+import { Control, Controller } from "react-hook-form";
+import classNames from "classnames";
+
+import InputSel from "components/global/gear/inputAndSel/inputSel"
+
+
+import scss from "./profile.module.scss"
+
+// fake
+import { TfakeworkSheet } from "pages/worksDepartment/contractList/contract/workSheet";
+
+
+
+export default function WorkSheetProfile(
+  { control, fakeWorkSheet, disabled }:
+    {
+      control: Control<TfakeworkSheet, any>
+      fakeWorkSheet: TfakeworkSheet
+      disabled: boolean
+    }
+) {
+
+  return (
+    <div className={scss.profile}>
+
+      <div className={scss.left}>
+        <div className={scss.top}>
+          <Controller name="projectDesc" control={control}
+            render={({ field }) =>
+              <InputSel className={scss.inputSel} label="工程名稱"
+                inputProps={field}
+                captionColor="main" disabled={disabled} showBaseline="auto" />
+            } />
+          <Controller name="projectName" control={control}
+            render={({ field }) =>
+              <InputSel className={scss.inputSel} label="工程內容"
+                inputProps={field}
+                captionColor="main" disabled={disabled} showBaseline="auto" />
+            } />
+        </div>
+
+        <hr />
+        {/* 工程地點之後要改成下拉式選單 */}
+        <div className={scss.bottom}>
+          {configArr_left.map((item) => {
+            const { key, label, className } = item
+            return (
+              <Controller key={key} name={key} control={control}
+                render={({ field }) =>
+                  <InputSel className={classNames(scss.inputSel, className)} label={label}
+                    inputProps={field}
+                    captionColor="main" disabled={disabled} showBaseline="auto" />
+                } />
+            )
+          })}
+        </div>
+      </div>
+      
+      <div className={scss.right}>
+        {configArr_right.map((item) => {
+          const { key, label, className } = item
+          return (
+            <Controller key={key} name={key} control={control}
+              render={({ field }) =>
+                <InputSel className={classNames(scss.inputSel, className)} label={label}
+                  inputProps={field}
+                  captionWidth="80px" captionColor="main"
+                  disabled={disabled} showBaseline="auto" />
+              } />
+          )
+        })}
+      </div>
+
+    </div>
+  )
+
+}
+
+
+// ==============================================================
+
+
+
+const configArr_left = [
+  {
+    key: "constructionSiteNumber",
+    label: "工地電話",
+    className: undefined
+  },
+  {
+    key: "projectPrincipal",
+    label: "工程負責人",
+    className: undefined
+  },
+  {
+    key: "constructionSiteFax",
+    label: "工地號碼",
+    className: undefined
+  },
+  {
+    key: "projectPrincipalPhone",
+    label: "負責人電話",
+    className: undefined
+  },
+  {
+    key: "projectAddress",
+    label: "工程地點",
+    className: "col-span-2"
+  },
+] as const
+
+
+const configArr_right = [
+  { key: "projectNumber", label: "工程編號", className: undefined },
+  { key: "contractor", label: "承包商", className: undefined },
+  { key: "principal", label: "負責人", className: undefined },
+  { key: "companyPhone", label: "公司電話", className: undefined },
+  { key: "companyFax", label: "公司傳真", className: undefined },
+] as const
+
+
+
--- pages/worksDepartment/contractList/contract/workSheet.tsx
+++ pages/worksDepartment/contractList/contract/workSheet.tsx
@@ -1,28 +1,134 @@
+import { useState } from "react"
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 
-import React from "react"
+// layer
+import PageHeader, { TpanelList } from "components/page/worksDepartment/contracList/contract/gear/PageHeader"
+import SubLayer from "components/Layer/SubLayer/SubLayer"
 
 // component
-import PageHeader from "components/page/worksDepartment/contracList/contract/gear/PageHeader"
+import WorkSheetProfile from "components/page/worksDepartment/contracList/contract/workSheet/profile"
+
+// gear
+import InputSel from "components/global/gear/inputAndSel/inputSel";
+import { OptionWithIcon01 } from "components/global/gear/select/optionWithIcon";
+import { SingleValueWithIcon01 } from "components/global/gear/select/singleValueWithIcon";
+
 
 // css
-import style from "./contract.module.scss"
+import scss from "./workSheet.module.scss"
+
+
+// other
+import { optionsCre_doorTrack_normal } from "js/utils/options/doorTrackOptions";
+
+const optionArr_doorTrack = optionsCre_doorTrack_normal()
 
 
 export default function WorkSheet() {
+  const [disabled, setDisabled] = useState(true)
+
+  const { control, handleSubmit } = useForm({ defaultValues: fakeWorkSheet });
+
+  const onSubmit: SubmitHandler<TfakeworkSheet> = data => {
+    alert(JSON.stringify(data))
+    console.log(data)
+  };
+
+
+  const panelList_allow: TpanelList = [
+    {
+      type: "myButton",
+      label: "編輯",
+      onClick: () => { setDisabled(false) },
+    }
+  ]
+
+  const panelList_notAllow: TpanelList = [
+    {
+      type: "myButton",
+      label: "取消",
+      onClick: () => { setDisabled(true) },
+    }
+  ]
+
+
+  const panelList = disabled ? panelList_allow : panelList_notAllow
+
 
   return (
-    <div className={style.container}>
-    <PageHeader />
-    <div className={style.mainContainer}>
-      <div className={style.workContactDoc}>
-        <h1>工作表</h1>
-        <h1>工作表</h1>
-        <h1>工作表</h1>
-        <h1>工作表</h1>
-        <h1>工作表</h1>
-      </div>
-    </div>
-  </div>
+    <SubLayer className={scss.container}>
+      <PageHeader panelList={panelList} />
+      <form >
+
+        <WorkSheetProfile control={control} disabled={disabled}
+          fakeWorkSheet={fakeWorkSheet}
+        />
+
+
+
+      </form>
+
+    </SubLayer>
   )
 }
+
+// ====================================================================
+
+export type TfakeworkSheet = {
+  /**工程編號 */
+  projectNumber: string
+  /**承包商 */
+  contractor: string
+  /**負責人 */
+  principal: string
+  /**公司電話 */
+  companyPhone: string
+  /**公司傳真 */
+  companyFax: string
+
+  /**工程名稱 */
+  projectName: string
+  /**工程內容 */
+  projectDesc: string,
+  /**工地電話 */
+  constructionSiteNumber: string
+  /**工地傳真 */
+  constructionSiteFax: string
+  /**工地位置縣市 */
+  projectCity: string
+  /**工地位置地區 */
+  projectDistrict: string
+  /**工地位置地址 */
+  projectAddress: string
+  /**工程負責人 */
+  projectPrincipal: string
+  /**工程負責人電話 */
+  projectPrincipalPhone: string
+
+};
+
+
+const fakeWorkSheet: TfakeworkSheet = {
+  projectNumber: "工程編號",
+  contractor: "承包商",
+  principal: "負責人",
+  companyPhone: "公司電話",
+  companyFax: "公司傳真",
+
+  projectName: "工程名稱",
+  projectDesc: "工程內容",
+  constructionSiteNumber: "工地電話",
+  constructionSiteFax: "工地傳真",
+  projectCity: "高雄市",
+  projectDistrict: "大樹區",
+  projectAddress: "花巷草弄20號",
+  projectPrincipal: "工程負責人",
+  projectPrincipalPhone: "工程負責人電話",
+}
+
+
+
+
+
+
 
