eaa0f7ac49c099b5b783ea78b001360c0b499919
Silarce <silarceanoux@gmail.com>
2023-07-21 21:23:06 +0800

    feat: 首頁 報表 匯出excel檔功能製作中

--- pages/home/monthReport/index.tsx
+++ pages/home/monthReport/index.tsx
@@ -172,10 +172,10 @@ export default function MonthReport() {
   panelList.unshift({
     type: 'exportButton',
     label: '下載Excel檔',
-    // onClick: () => exportExcel(accountingReport),
-    onClick: () => {
-      alert('功能開發中');
-    },
+    onClick: () => exportExcel(accountingReport),
+    // onClick: () => {
+    //   alert('功能開發中');
+    // },
     className: scss.btn,
   });
 
@@ -230,14 +230,170 @@ export default function MonthReport() {
 
 // =============================================================
 
-// import ExcelJs from 'exceljs';
+import ExcelJs from 'exceljs';
 
-// const exportExcel = (data: TaccountingReportDto[] | undefined) => {
-//   console.log(data);
+const exportExcel = async (dataArr: TaccountingReportDto[] | undefined) => {
+  // console.log(data);
+  const workbook = new ExcelJs.Workbook();
 
-//   const workbook = new ExcelJs.Workbook();
-//   // const sheetName = 'foo';
-//   const sheet = workbook.addWorksheet('foo');
+  // const sheetName = 'foo';
+  // const sheet = workbook.addWorksheet('foo');
 
-//   sheet.columns = [{ header: '日期', key: 'date', width: 20 }];
-// };
+  // sheet.columns = [
+  //   { header: '日期1', key: 'date', width: 20 },
+  //   { header: '日期2', key: 'date1', width: 20 },
+  //   { header: '日期3', key: 'date2', width: 20 },
+  //   { header: '日期4', key: 'date3', width: 20 },
+  // ];
+  // const worksheet = workbook.getWorksheet('foo');
+
+  // sheet.mergeCells('A1:B1');
+
+  // 第一個值為row1，會蓋掉header
+  // undefined會被當成空白
+  // sheet.getColumn(1).values = [1, 2, 3, 4, 5];
+  // sheet.getColumn('date1').values = [5, 4, 3, 2, 1, 0];
+  // sheet.getColumn('date2').values = [undefined, undefined, 3, 2, , 0];
+
+  // sheet.addTable({
+  //   name: 'MyTable',
+  //   ref: 'A1',
+  //   // style: {
+  //   //   showFirstColumn: true,
+  //   // },
+
+  //   columns: [{ name: 'a' }],
+  //   rows: [['1', '1', '1', '1', '1']],
+  // });
+
+  // sheet.addTable({
+  //   name: 'MyTable',
+  //   ref: 'A1',
+  //   columns: [{ name: 'a' }, { name: 'b' }],
+  //   rows: [['1', '5']],
+  // });
+  // sheet.addTable({
+  //   name: 'MyTable',
+  //   ref: 'C1',
+  //   columns: [{ name: 'a' }, { name: 'b' }],
+  //   rows: [['1', '5']],
+  // });
+  // sheet.addTable({
+  //   name: 'MyTable',
+  //   ref: 'E1',
+  //   columns: [{ name: 'a' }, { name: 'b' }],
+  //   rows: [['1', '5']],
+  // });
+
+  // sheet.addTable({
+  //   name: 'MyTotal',
+  //   ref: 'G1',
+  //   headerRow: true,
+  //   totalsRow: true,
+  //   columns: [
+  //     { name: 'Date', totalsRowLabel: 'Totals:', filterButton: true },
+  //     // { name: 'Amount', totalsRowFunction: 'sum', filterButton: false },
+  //     { name: 'Amount', totalsRowFunction: 'average', filterButton: false },
+  //   ],
+  //   rows: [
+  //     [new Date('2019-07-20'), 70.1],
+  //     [new Date('2019-07-21'), 70.6],
+  //     [new Date('2019-07-22'), 70.1],
+  //   ],
+  // });
+
+  // -----------------------------------------------------------
+  const sheet = workbook.addWorksheet('報表');
+  // -----------------------------------------------------------
+
+  const cA = sheet.getColumn(1);
+  cA.width = 15;
+  cA.key = 'caption';
+  sheet.mergeCells('A1:A2');
+
+  const dateDayArr = new Array(31).fill(0).map((item, index) => index + 1);
+
+  cA.values = [undefined, '日期', ...dateDayArr, '合計', '餐費', '外宿費', '餐+宿', '本月總合計'];
+  // -----------------------------------------------------------
+
+  dataArr?.forEach((data, index) => {
+    const { employeeName, employeeId, statistic } = data;
+    // const { date, meals, stayLength, dailyReportId, isWorker } = statistic;
+
+    const dataRow: Array<Array<'V' | '1' | undefined>> = new Array(31).fill([]);
+
+    statistic.forEach((item) => {
+      const { date, meals, stayLength, dailyReportId } = item;
+
+      const stayLengthV = stayLength ? '1' : undefined;
+
+      let breakfast: 'V' | undefined = undefined;
+      let lunch: 'V' | undefined = undefined;
+      let dinner: 'V' | undefined = undefined;
+
+      meals?.forEach((meal) => {
+        if (meal === 'breakfast') {
+          breakfast = 'V';
+        }
+
+        if (meal === 'lunch') {
+          lunch = 'V';
+        }
+
+        if (meal === 'dinner') {
+          dinner = 'V';
+        }
+      });
+
+      const dateDay = new Date(date).getDate();
+      dataRow[dateDay - 1] = [breakfast, lunch, dinner, stayLengthV];
+    });
+
+    // console.log(dataRow[0]);
+
+    sheet.addTable({
+      name: employeeId,
+      ref: `${numberToLetters(index * 4 + 2)}1`,
+      headerRow: false,
+      columns: [{ name: 'breakfast' }, { name: 'lunch' }, { name: 'dinner' }, { name: 'stayLength' }],
+      rows: [[employeeName], ['早', '中', '晚', '外宿'], ...dataRow],
+    });
+
+    sheet.mergeCells(`${numberToLetters(index * 4 + 2)}1:${numberToLetters(index * 4 + 2 + 3)}1`);
+  });
+
+  // -----------------------------------------------------------
+
+  await workbook.xlsx.writeBuffer();
+
+  // -----------------------------------------------------------
+  // 表格裡面的資料都填寫完成之後，訂出下載的callback function
+  // 異步的等待他處理完之後，創建url與連結，觸發下載
+  workbook.xlsx.writeBuffer().then((content) => {
+    const link = document.createElement('a');
+    const blobData = new Blob([content], {
+      type: 'application/vnd.ms-excel;charset=utf-8;',
+    });
+
+    const id = 'foo';
+    // const today = Moment().format('yyyy-MM-DD');
+    // link.download = `${id}_${today}.xlsx`;
+    link.download = `foo.xlsx`;
+    link.href = URL.createObjectURL(blobData);
+    link.click();
+    link.remove();
+  });
+};
+
+// 數字轉字母 // from gpt3.5
+function numberToLetters(num: number) {
+  let result = '';
+
+  while (num > 0) {
+    const remainder = (num - 1) % 26;
+    result = String.fromCharCode(remainder + 65) + result;
+    num = Math.floor((num - 1) / 26);
+  }
+
+  return result;
+}
