862e26614302d67dee42124b55c7b33caf072259
Silarce <silarceanoux@gmail.com>
2023-08-14 20:16:14 +0800

    feat: 變更quotationSinature.tsx的參數型別，並調整相關元件

--- components/page/domestic/quotation/quotationSinature.tsx
+++ components/page/domestic/quotation/quotationSinature.tsx
@@ -1,31 +1,26 @@
 // global gear
-import InputSel from 'components/global/gear/inputAndSel/inputSel';
+import InputSel, { TinputProps } from 'components/global/gear/inputAndSel_v2/inputSel';
 // css
 import style from './quotationSinature.module.scss';
+
+export type { TinputProps };
 
 export default function QuotationSinature({
   signatureArr,
   disabled = false,
 }: {
-  signatureArr: { label: string; signature: string; onChange: (v: string) => void }[];
+  signatureArr: { label: string; inputProps: TinputProps }[];
   disabled: boolean;
 }) {
   return (
     <div className={style.container}>
       {signatureArr.map((item, index) => {
-        const { label, signature, onChange } = item;
+        const { label, inputProps } = item;
 
         return (
           <div key={index}>
             <span>{label}</span>
-            <InputSel
-              inputProps={{
-                value: signature ?? '',
-                onChange: (v) => onChange(v),
-              }}
-              disabled={disabled}
-              showBaseline={'always'}
-            />
+            <InputSel inputProps={inputProps} disabled={disabled} showBaseline={'always'} />
           </div>
         );
       })}
