8aaf097155d6f794ad1bf2e2ff2b5752f3e4c04d
Silarce <silarceanoux@gmail.com>
2023-08-17 11:23:03 +0800

    feat: 舊合約整合 新增舊合約時會預設四項付款辦法

--- hooks/quotation/useLegacyContract.ts
+++ hooks/quotation/useLegacyContract.ts
@@ -1394,7 +1394,24 @@ const emptyLegacyContract = (): TemptyLegacyContract => {
     total: 0,
     deliveryLocation: '',
     deliveryDate: '',
-    paymentMethods: [],
+    paymentMethods: [
+      {
+        milestone: '訂製同時付總金額',
+        totalPaymentRatio: '0',
+      },
+      {
+        milestone: '交貨同時付總金額',
+        totalPaymentRatio: '0',
+      },
+      {
+        milestone: '按裝同時付總金額',
+        totalPaymentRatio: '0',
+      },
+      {
+        milestone: '接電同時付總金額',
+        totalPaymentRatio: '0',
+      },
+    ],
     notes: [],
     quoteScopes: [],
     managerName: '',
