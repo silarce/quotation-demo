1c67c85eb50501de979b173b5ced4f52d052eeaf
Silarce <silarceanoux@gmail.com>
2023-08-21 13:13:43 +0800

    feat: customerSelector.tsx 外層元件改用selectorShell

--- components/global/gear/modal/customerSelector.tsx
+++ components/global/gear/modal/customerSelector.tsx
@@ -3,7 +3,8 @@ import { useInView } from 'react-intersection-observer';
 import _ from 'lodash';
 
 // global gear
-import ModalListSelectorWithSearch from 'components/global/gear/modal/modalListSelectorWithSearch';
+// import ModalListSelectorWithSearch from 'components/global/gear/modal/modalListSelectorWithSearch';
+import SelectorShell, { TsearcbBarProps } from './selectorShell';
 import CellWithBar from 'components/global/gear/cell/cellWithBar';
 import myAlert, { ModalInfo } from 'components/global/gear/modal/simpleModal/alertModals';
 import LoadingCoverWrapper01 from '../loadingCover/loadingCoverWrapper01';
@@ -159,22 +160,37 @@ export default function CustomerSelector({
     setSelEmployeeArr([]);
   };
 
-  const onSearch = (v: string) => {
-    setSearchValue(v);
+  const onSearch = (v: string[]) => {
+    setSearchValue(v[0]);
   };
 
   // ==================================================
+  const inputSelPropsArr: TsearcbBarProps['inputSelPropsArr'] = [
+    {
+      inputProps: {
+        wrapperClassName: '',
+        wrapperStyle: { width: '160px' },
+        props: {
+          placeholder: '搜尋關鍵字',
+        },
+      },
+    },
+  ];
+  // ==================================================
 
   return (
-    <ModalListSelectorWithSearch
+    <SelectorShell
       label={label ?? ''}
       visible={showModal}
       onConfirm={theOnConfirm}
       onCancel={theOnCancel}
-      onSearch={onSearch}
       width={'800'}
       className={style.container}
       tip={tip}
+      searcbBarProps={{
+        inputSelPropsArr: inputSelPropsArr,
+        onClick: onSearch,
+      }}
     >
       <LoadingCoverWrapper01 isLoading={isLoading}>
         <div className={style.listContainer}>
@@ -202,6 +218,6 @@ export default function CustomerSelector({
           })}
         </div>
       </LoadingCoverWrapper01>
-    </ModalListSelectorWithSearch>
+    </SelectorShell>
   );
 }
