c1af569fec9b21012b8fc4961cc3c78854c734a2
Silarce <silarceanoux@gmail.com>
2023-08-25 21:48:50 +0800

    feat: 追加追減 變更主產品 變更配件 製作垂直拖拉功能

--- components/page/domestic/quotation/quotationProduct/exProductList_legacy.tsx
+++ components/page/domestic/quotation/quotationProduct/exProductList_legacy.tsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react';
+import _ from 'lodash';
 import Image from 'next/image';
 import classNames from 'classnames';
-import { useState } from 'react';
 // global gear
 import CellWithBar from 'components/global/gear/cell/cellWithBar';
 import Checkbox01 from 'components/global/gear/checkbox/checkbox01';
 import InputSel from 'components/global/gear/inputAndSel/inputSel';
 import { OptionWithIcon01 } from 'components/global/gear/select/optionWithIcon';
 import { SingleValueWithIcon01 } from 'components/global/gear/select/singleValueWithIcon';
+
+// dnd
+import {
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragStartEvent,
+  DragEndEvent,
+  DraggableAttributes,
+} from '@dnd-kit/core';
+
+import {
+  arrayMove,
+  SortableContext,
+  // horizontalListSortingStrategy,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import {
+  restrictToVerticalAxis,
+  //  restrictToHorizontalAxis, restrictToWindowEdges
+} from '@dnd-kit/modifiers';
+
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+import { SyntheticListenerMap } from '@dnd-kit/core/dist/hooks/utilities';
 
 // icon
 import iconMove from 'public/image/icon/move.svg';
@@ -35,8 +64,9 @@ export default function ExProductList_legacy({
   disabled: boolean;
 }) {
   // ---------------------------------------------------------------
-  const { prodExchangeList: exchangeList, prodCellConfig } = classQuotation;
-  const exChnageArr = Object.values(exchangeList);
+  const { prodExchangeList, prodCellConfig } = classQuotation;
+  // const exChnageArr = Object.values(prodExchangeList);
+  const exChnageKeyArr = Object.keys(prodExchangeList);
 
   // const theadIndex = prodCellConfig.keyList;
   const theadIndex = classQuotation.exchangeKeyList;
@@ -48,46 +78,80 @@ export default function ExProductList_legacy({
   const [activeIndex, setActiveIndex] = useState(-1);
 
   // ---------------------------------------------------------------
+
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  const [movingId, setMovingId] = useState<string>();
+  const [dndKeyArr, setDndKeyArr] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newArr = Object.keys(prodExchangeList);
+
+    if (newArr.length > dndKeyArr.length) {
+      const newKeyArr = _.difference(newArr, dndKeyArr);
+      setDndKeyArr([...dndKeyArr, ...newKeyArr]);
+    }
+
+    if (newArr.length < dndKeyArr.length) {
+      const delDndKey = _.difference(dndKeyArr, newArr)[0];
+      const delIndex = dndKeyArr.indexOf(delDndKey);
+      dndKeyArr.splice(delIndex, 1);
+      setDndKeyArr([...dndKeyArr]);
+    }
+  }, [exChnageKeyArr.length]);
+
+  const onDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+
+    if (active.id !== over?.id) {
+      const oldIndex = dndKeyArr.indexOf(active.id as string);
+      const newIndex = dndKeyArr.indexOf(over?.id as string);
+
+      const newKeyArr = arrayMove(dndKeyArr, oldIndex, newIndex);
+      setDndKeyArr(newKeyArr);
+    }
+
+    setMovingId(undefined);
+  };
+
+  function onDragStart(e: DragStartEvent) {
+    const { id } = e.active;
+    setMovingId(id as string);
+  }
+
+  // ---------------------------------------------------------------
   return (
     <div className={scss.container}>
-      {exChnageArr.map((item, pIndex) => {
-        const { prod, delSelf } = item;
+      <DndContext
+        sensors={sensors}
+        modifiers={[
+          restrictToVerticalAxis,
+          // restrictToWindowEdges,
+        ]}
+        onDragEnd={onDragEnd}
+        onDragStart={onDragStart}
+      >
+        <SortableContext items={dndKeyArr} strategy={verticalListSortingStrategy}>
+          {dndKeyArr.map((key, pIndex) => {
+            const { prod, delSelf } = prodExchangeList[key];
+            const isActive = pIndex === activeIndex;
+            const isMoving = movingId === key;
 
-        const isActive = pIndex === activeIndex;
-
-        return (
-          <CellWithBar key={pIndex} isActive={isActive} onClick={() => setActiveIndex(pIndex)}>
-            <div className={scss.row} onClick={() => (classQuotation.activeProd = pIndex)}>
-              {/*  */}
-              <ControlBox delSelf={delSelf} index={pIndex + 1} />
-              {/*  */}
-              {theadIndex.map((key) => {
-                const { width, id, type, inputType, options } = prodCellConfig.cellConfig[key];
-                const textCenter = centerReg.test(id) ? scss_l.textCenter : '';
-                const theStyle = { width };
-                const stateValue = prod[key];
-                const TheCell = cellSwitcher({
-                  dataItem: prod,
-                  key,
-                  type,
-                  disabled,
-                  stateValue,
-                  inputType,
-                  options,
-                });
-
-                return (
-                  <div className={`${scss.column} ${textCenter}`} key={key} style={theStyle}>
-                    {TheCell}
-                  </div>
-                );
-              })}{' '}
-              {/* column */}
-            </div>{' '}
-            {/* row */}
-          </CellWithBar>
-        );
-      })}
+            return (
+              <ExchangeRow
+                key={key}
+                id={key}
+                //
+                pIndex={pIndex}
+                isActive={isActive}
+                isMoving={isMoving}
+                delSelf={delSelf}
+                prod={prod}
+              />
+            );
+          })}
+        </SortableContext>
+      </DndContext>
     </div>
   ); // return
 
@@ -222,14 +286,91 @@ export default function ExProductList_legacy({
         return null;
     }
   }
+  // ------------------------
+
+  function ExchangeRow({
+    pIndex,
+    isActive,
+    isMoving,
+    delSelf,
+    prod,
+    id,
+  }: {
+    pIndex: number;
+    isActive: boolean;
+    isMoving: boolean;
+    delSelf?: () => void;
+    prod: Class_product;
+    id: string;
+  }) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+      id,
+    });
+
+    const itemStyle = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+
+    return (
+      <div style={itemStyle} ref={setNodeRef} className={classNames(isMoving && 'z-10', 'relative')}>
+        <CellWithBar
+          isActive={isActive}
+          onClick={() => {
+            // 有bug
+            // setActiveIndex(pIndex);
+          }}
+        >
+          <div className={scss.row} onClick={() => (classQuotation.activeProd = pIndex)}>
+            {/*  */}
+            <ControlBox delSelf={delSelf} index={pIndex + 1} dndAttr={attributes} dndListener={listeners} />
+            {/*  */}
+            {theadIndex.map((key) => {
+              const { width, id, type, inputType, options } = prodCellConfig.cellConfig[key];
+              const textCenter = centerReg.test(id) ? scss_l.textCenter : '';
+              const theStyle = { width };
+              const stateValue = prod[key];
+              const TheCell = cellSwitcher({
+                dataItem: prod,
+                key,
+                type,
+                disabled,
+                stateValue,
+                inputType,
+                options,
+              });
+
+              return (
+                <div className={`${scss.column} ${textCenter}`} key={key} style={theStyle}>
+                  {TheCell}
+                </div>
+              );
+            })}{' '}
+            {/* column */}
+          </div>{' '}
+          {/* row */}
+        </CellWithBar>
+      </div>
+    );
+  }
 } //ProductList
 
 // ================================================
 
-const ControlBox = ({ delSelf, index }: { delSelf?: () => void; index: number | string }) => {
+const ControlBox = ({
+  delSelf,
+  index,
+  dndAttr,
+  dndListener,
+}: {
+  delSelf?: () => void;
+  index: number | string;
+  dndAttr: DraggableAttributes;
+  dndListener: SyntheticListenerMap | undefined;
+}) => {
   return (
     <div className={classNames(scss.buttonBox, scss.exchange, 'chameleon')}>
-      <Image className={scss.move} src={iconMove} alt="move" />
+      <Image className={scss.move} src={iconMove} alt="move" {...dndAttr} {...dndListener} />
       <button className={classNames(scss.btn, !delSelf && scss.hidden)} onClick={delSelf}>
         刪除
       </button>
@@ -237,3 +378,36 @@ const ControlBox = ({ delSelf, index }: { delSelf?: () => void; index: number | 
     </div>
   );
 };
+
+// <div key={pIndex}>
+//   <CellWithBar isActive={isActive} onClick={() => setActiveIndex(pIndex)}>
+//     <div className={scss.row} onClick={() => (classQuotation.activeProd = pIndex)}>
+//       {/*  */}
+//       <ControlBox delSelf={delSelf} index={pIndex + 1} />
+//       {/*  */}
+//       {theadIndex.map((key) => {
+//         const { width, id, type, inputType, options } = prodCellConfig.cellConfig[key];
+//         const textCenter = centerReg.test(id) ? scss_l.textCenter : '';
+//         const theStyle = { width };
+//         const stateValue = prod[key];
+//         const TheCell = cellSwitcher({
+//           dataItem: prod,
+//           key,
+//           type,
+//           disabled,
+//           stateValue,
+//           inputType,
+//           options,
+//         });
+
+//         return (
+//           <div className={`${scss.column} ${textCenter}`} key={key} style={theStyle}>
+//             {TheCell}
+//           </div>
+//         );
+//       })}{' '}
+//       {/* column */}
+//     </div>{' '}
+//     {/* row */}
+//   </CellWithBar>
+// </div>
