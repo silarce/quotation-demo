bbcd17ab821004442af3612084cfd4147aec0de3
Silarce <silarceanoux@gmail.com>
2023-10-04 11:50:22 +0800

    feat: 合約追加追減 放入基本資料

--- pages/domestic/contract/attachContract/index.tsx
+++ pages/domestic/contract/attachContract/index.tsx
@@ -3,17 +3,54 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { NextRouter } from 'next/router';
 import _ from 'lodash';
+import classNames from 'classnames';
 
 // layer
 import SubLayer from 'components/Layer/SubLayer/SubLayer';
+
+// components
+import QuotationProfile, { TreturnBody, TquotationProfile } from 'components/page/domestic/quotation/quotationProfile';
+
 // global gear
 import PageHeader02, { TtagList, TpanelList } from 'components/PageHeader/PageHeader02/PageHeader02';
 import { showRootLoading } from 'components/global/gear/loadingCover/rootLoadingCover';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
 
+import { useGetContract_id } from 'js/api/api_quotation';
+
+// css
+import scss from 'pages/domestic/quotationList/quotation/quotation.module.scss';
+
+// ===========================================================================
 export default function AttachContract() {
   const router = useRouter();
   const contractId = router.query.contractId as string | undefined;
+
+  // ----------------------------------------------------
+  const { data, update } = useGetContract_id(contractId);
+  useEffect(() => {
+    update();
+  }, [contractId]);
+
+  console.log(data);
+
+  // const profile: TquotationProfile = {
+  //   id: '',
+  //   quotationNumber: '',
+  //   quotationDate: '',
+  //   validityPeriod: '',
+  //   projectName: '',
+  //   county: '',
+  //   district: '',
+  //   address: '',
+  //   contactPerson: '',
+  //   contactNumber: '',
+  //   customer: '',
+  //   trackProgress: '',
+  //   projectProgress: '',
+  // };
+
+  // ------------------------------------------------------------------
 
   const tagList: TtagList = [
     {
@@ -71,12 +108,23 @@ export default function AttachContract() {
     },
   ];
 
+  // ==========================================================================
   return (
     <SubLayer>
       <PageHeader02 tagList={tagList} panelList={panel} />
-      <div>foooooo</div>
-      <div></div>
-      <div></div>
+      <div>
+        <div className={scss.quotation}>
+          {/*  */}
+          <QuotationProfile profile={data?.content} disabled={true} onProfileChange={() => {}} />
+          {/*  */}
+          <div className={classNames(scss.switchBar)}>
+            <div>合約項目</div>
+          </div>
+          {/*  */}
+
+          {/*  */}
+        </div>
+      </div>
     </SubLayer>
   );
 }
