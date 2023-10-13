ffc16a9a321bf67307c6248b8d61388fc9a1eec4
Silarce <silarceanoux@gmail.com>
2023-10-13 10:46:28 +0800

    feat: 報價單 匯出報價單 修改html2canvas的引數

--- components/page/domestic/pdf/quotationPdf/quotationPdf_new.tsx
+++ components/page/domestic/pdf/quotationPdf/quotationPdf_new.tsx
@@ -98,10 +98,12 @@ export default function QuotationPdf({
       }
 
       const image = await html2canvas(
-        item
+        item,
         // FIXME 似乎是因為套件發出取得圖片的請求時，沒有帶上cookie，所以會被擋下來
-        // useCORS 與 allowTaint 的方案無效
-        // { useCORS: true, allowTaint: true }
+        {
+          useCORS: true,
+          allowTaint: true,
+        }
       ).then((canvas) => {
         const image = canvas.toDataURL('image/JPEG');
 
