6db613c63a0a5e34e8541795cd7e743c9a7fdf8c
Silarce <silarceanoux@gmail.com>
2023-10-20 21:33:52 +0800

    feat: 舊合約與報價單 PDF 厚度若為0則不顯示

--- components/page/domestic/pdf/quotationPdf/quotationPdf_legacyContract.tsx
+++ components/page/domestic/pdf/quotationPdf/quotationPdf_legacyContract.tsx
@@ -188,7 +188,7 @@ export default function QuotationPdf({
         doorType: prod.doorType,
         material: prod.material,
         // thickness: prod.thickness,
-        thickness: prod.thickness,
+        thickness: prod.thickness === '0' ? '' : prod.thickness,
         surface: prod.surface,
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
