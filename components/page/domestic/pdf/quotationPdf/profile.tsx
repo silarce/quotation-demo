b639fc5ba23bf36ad1384ec851c40ab1a749d03c
Silarce <silarceanoux@gmail.com>
2023-10-26 12:05:49 +0800

    feat:報價單PDF 修改樣式

--- components/page/domestic/pdf/quotationPdf/profile.tsx
+++ components/page/domestic/pdf/quotationPdf/profile.tsx
@@ -50,7 +50,12 @@ export default function Profile({
             <span>{contactPerson}</span>
           </div>
           <div className={style.info}>
-            <span>客戶名稱</span>
+            <span className={style.flexSpan}>
+              <span>客</span>
+              <span>戶</span>
+              <span>名</span>
+              <span>稱</span>
+            </span>
             <span className={style.semi}>:</span>
             <span>{clientName}</span>
           </div>
@@ -102,13 +107,23 @@ export default function Profile({
         </div>
       </div>
 
-      <div className={style.info}>
-        <span>工程名稱</span>
+      <div className={style.info2}>
+        <span className={style.flexSpan}>
+          <span>工</span>
+          <span>程</span>
+          <span>名</span>
+          <span>稱</span>
+        </span>
         <span className={style.semi}>:</span>
         <span>{projectName}</span>
       </div>
-      <div className={style.info}>
-        <span>工程地點</span>
+      <div className={style.info2}>
+        <span className={style.flexSpan}>
+          <span>工</span>
+          <span>程</span>
+          <span>地</span>
+          <span>點</span>
+        </span>
         <span className={style.semi}>:</span>
         <span>{projectAddress}</span>
       </div>
