c3f513f4a3dfeccf33dfa43c60dd3f2c8cc1e1f1
Silarce <silarceanoux@gmail.com>
2023-10-30 20:28:16 +0800

    fix: 舊合約 修正PDF元件內的undefined錯誤

--- components/page/domestic/pdf/quotationPdf/quotationPdf_legacyContract.tsx
+++ components/page/domestic/pdf/quotationPdf/quotationPdf_legacyContract.tsx
@@ -182,9 +182,9 @@ export default function QuotationPdf({
 
     return verticalKeyArr.map((key) => {
       const prod = prodList[key];
-      const lw = new Decimal(Number(prod.width || 0) || Number(prod.length || 0)).mul(100).toString();
-      const h = new Decimal(Number(prod.height || 0)).mul(100).toString();
-      const b = new Decimal(Number(prod.boxB || 0)).mul(100).toNumber();
+      const lw = new Decimal(Number(prod?.width || 0) || Number(prod?.length || 0)).mul(100).toString();
+      const h = new Decimal(Number(prod?.height || 0)).mul(100).toString();
+      const b = new Decimal(Number(prod?.boxB || 0)).mul(100).toNumber();
 
       const size = `${lw} X ${h} ${b ? `+ ${b}` : ''}`;
 
