170084abb57b42aa1f7bc4892deeacf846f5517f
Silarce <silarceanoux@gmail.com>
2023-11-15 17:37:49 +0800

    feat:應收帳款明細 製作異常燈號checkBar

--- pages/worksDepartment/contractList/contract/accountReceivable/index.tsx
+++ pages/worksDepartment/contractList/contract/accountReceivable/index.tsx
@@ -25,6 +25,7 @@ export default function AccountReceivable() {
   // --------------------------------------------------------------------------
 
   const [checkBar01, setCheckBar01] = useState<string[]>([]);
+  const [checkBar02, setCheckBar02] = useState<string[]>([]);
 
   // --------------------------------------------------------------------------
   const control_profile: Tcontrol_profile = {
@@ -73,31 +74,6 @@ export default function AccountReceivable() {
 
   // --------------------------------------------------------------------------
 
-  const control_table_requestPayment: Tcontrol_table_requestPayment = {
-    columnArr: [
-      {
-        caption: 'foo',
-        paymentRatio: { value: 'foo' },
-        loanPeriod: { value: 'foo' },
-        remark: { value: 'foo' },
-      },
-      {
-        caption: 'foo',
-        paymentRatio: { value: 'foo' },
-        loanPeriod: { value: 'foo' },
-        remark: { value: 'foo' },
-      },
-      {
-        caption: 'foo',
-        paymentRatio: { value: 'foo' },
-        loanPeriod: { value: 'foo' },
-        remark: { value: 'foo' },
-      },
-    ],
-  };
-
-  // --------------------------------------------------------------------------
-
   const control_checkBar01: TcheckboxProps = {
     onChange: (arr) => {
       setCheckBar01(arr);
@@ -125,6 +101,65 @@ export default function AccountReceivable() {
         value: checkBar01.includes('warrantyCheque'),
         props: {
           className: classNames(checkBar01.includes('warrantyCheque') && scss.checkActive),
+        },
+      },
+    ],
+  };
+
+  // --------------------------------------------------------------------------
+
+  const control_table_requestPayment: Tcontrol_table_requestPayment = {
+    columnArr: [
+      {
+        caption: 'foo',
+        paymentRatio: { value: 'foo' },
+        loanPeriod: { value: 'foo' },
+        remark: { value: 'foo' },
+      },
+      {
+        caption: 'foo',
+        paymentRatio: { value: 'foo' },
+        loanPeriod: { value: 'foo' },
+        remark: { value: 'foo' },
+      },
+      {
+        caption: 'foo',
+        paymentRatio: { value: 'foo' },
+        loanPeriod: { value: 'foo' },
+        remark: { value: 'foo' },
+      },
+    ],
+  };
+
+  // --------------------------------------------------------------------------
+
+  const control_checkBar02: TcheckboxProps = {
+    onChange: (arr) => {
+      setCheckBar02(arr);
+    },
+    propsArr: [
+      {
+        key: 'abnormal',
+        label: '異常燈號：工作表已開立，合約尚未簽回',
+        value: checkBar02.includes('abnormal'),
+        props: {
+          className: classNames(checkBar02.includes('abnormal') && scss.checkActive),
+        },
+      },
+      {
+        key: 'remind',
+        label: '提醒燈號：已出具說明，尚未收足款項 ',
+        value: checkBar02.includes('remind'),
+        props: {
+          className: classNames(checkBar02.includes('remind') && scss.checkActive),
+        },
+      },
+      {
+        key: 'notInstalled',
+        label: '已出貨因故尚未安裝',
+        value: checkBar02.includes('notInstalled'),
+        props: {
+          className: classNames(checkBar02.includes('notInstalled') && scss.checkActive),
         },
       },
     ],
@@ -169,7 +204,12 @@ export default function AccountReceivable() {
         <div className={scss.checkBar01}>
           <InputSel showBaseline="invisible" checkBoxProps={control_checkBar01} />
         </div>
+
         <Table_requestPayment disabled={disabled} control={control_table_requestPayment} />
+
+        <div className={scss.checkBar02}>
+          <InputSel showBaseline="invisible" checkBoxProps={control_checkBar02} />
+        </div>
       </div>
     </SubLayer>
   );
