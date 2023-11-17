914466c2afcb98344c7ff363a5947ef4fda9089c
Silarce <silarceanoux@gmail.com>
2023-11-17 11:21:43 +0800

    feat: utils convertDate.ts 新增直接取得民國日期字串的方法

--- js/utils/helpers/date/convertDate.ts
+++ js/utils/helpers/date/convertDate.ts
@@ -37,3 +37,11 @@ export const convertDate_add1911 = (ISOString: string) => {
 
   return dateTime.toISOString();
 };
+
+export const getTaiwanDateStr = (ISOString: string | undefined | null) => {
+  if (!ISOString) {
+    return '';
+  }
+
+  return moment(convertDate_reduce1911(ISOString)).format('yy-MM-DD');
+};
