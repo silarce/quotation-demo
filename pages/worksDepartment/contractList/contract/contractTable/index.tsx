af83cc18f8e8ce55ff712e37ea3e9ce1152281df
Silarce <silarceanoux@gmail.com>
2023-11-20 11:04:47 +0800

    feat: 工務部 合約 進度20%

--- pages/worksDepartment/contractList/contract/contractTable/index.tsx
+++ pages/worksDepartment/contractList/contract/contractTable/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, Fragment } from 'react';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import moment from 'moment';
@@ -72,7 +72,158 @@ export default function ContracTable() {
         contractNumber={engineeringContact?.contractNumber ?? ''}
       />
 
-      <div></div>
+      <div className={scss.main}>
+        <div className={scss.tableContainer}>
+          <div className={scss.tablewrapper}>
+            <div className={scss.top}>
+              {/*  */}
+              <div>
+                <span>本期請款金額：{'63,160'}</span>
+              </div>
+            </div>
+            {/*  */}
+            <div className={classNames(scss.row, scss.thead)}>
+              <Left />
+              <Center dataArr={[undefined, undefined, undefined, undefined]} />
+              <Right />
+            </div>
+            <div className={scss.row}>
+              <Left
+                data={{
+                  projectNumber: 'foooo',
+                  itemName: 'foooo',
+                  size: 'foooo',
+                  quantity: 'foooo',
+                  unitPrice: 'foooo',
+                  totalPrice: 'foooo',
+                }}
+              />
+              <Center dataArr={[foooooooo, foooooooo, foooooooo, foooooooo]} />
+              <Right
+                data={{
+                  exchangedQuantity: 'foo',
+                  exchangedPrice: 'foo',
+                  notes: 'foo',
+                }}
+              />
+            </div>
+
+            {/*  */}
+          </div>
+        </div>
+      </div>
     </SubLayer>
   );
 }
+
+const Left = ({
+  data = {
+    projectNumber: '追加追減',
+    itemName: '項目',
+    size: '尺寸 (cm)',
+    quantity: '數量',
+    unitPrice: '合約單價',
+    totalPrice: '合約金額',
+  },
+}: {
+  data?: {
+    projectNumber?: string;
+    itemName?: string;
+    size?: string;
+    quantity?: string;
+    unitPrice?: string;
+    totalPrice?: string;
+  };
+}) => {
+  return (
+    <>
+      <div className={'w-[115px]'}>
+        <span>{data.projectNumber}</span>
+      </div>
+      <div className={'w-[80px]'}>
+        <span>{data.itemName}</span>
+      </div>
+      <div className={'w-[110px]'}>
+        <span>{data.size}</span>
+      </div>
+      <div className={'w-[50px]'}>
+        <span>{data.quantity}</span>
+      </div>
+      <div className={'w-[90px]'}>
+        <span>{data.unitPrice}</span>
+      </div>
+      <div className={'w-[90px]'}>
+        <span>{data.totalPrice}</span>
+      </div>
+    </>
+  );
+};
+
+const Right = ({
+  data = {
+    exchangedQuantity: '變更後數量',
+    exchangedPrice: '變更後金額',
+    notes: '備註',
+  },
+}: {
+  data?: {
+    exchangedQuantity: string;
+    exchangedPrice: string;
+    notes: string;
+  };
+}) => {
+  return (
+    <>
+      <div className={classNames('w-[80px]', scss.rightCell)}>
+        <span>{data.exchangedQuantity}</span>
+      </div>
+      <div className={classNames('w-[110px]', scss.rightCell)}>
+        <span>{data.exchangedPrice}</span>
+      </div>
+      <div className={classNames('w-[86px]', scss.rightCell)}>
+        <span>{data.notes}</span>
+      </div>
+    </>
+  );
+};
+
+const Center = ({
+  dataArr,
+}: {
+  dataArr: (
+    | {
+        quantity: string;
+        price: string;
+      }
+    | undefined
+  )[];
+}) => {
+  return (
+    <>
+      {dataArr.map((data, index) => {
+        const quantity = data?.quantity ?? `數量(變更${index + 1})`;
+        const price = data?.price ?? `金額(變更${index + 1})`;
+
+        const isOdd = index % 2 === 0;
+
+        return (
+          <Fragment key={index}>
+            <div className={classNames('w-[92px]', scss.centerCell, isOdd && scss.odd)}>
+              <span>{quantity}</span>
+            </div>
+            <div className={classNames('w-[120px]', scss.centerCell, isOdd && scss.odd)}>
+              <span>{price}</span>
+            </div>
+          </Fragment>
+        );
+      })}
+    </>
+  );
+};
+
+// ========================================================================
+
+const foooooooo = {
+  quantity: 'foo',
+  price: 'foo',
+};
