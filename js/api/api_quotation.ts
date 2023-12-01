0de44f130efb060dcc0ebcfd24c9f6ec50b0208b
Silarce <silarceanoux@gmail.com>
2023-12-01 16:10:40 +0800

    feat: 工務部 合約 加上loading cover

--- js/api/api_quotation.ts
+++ js/api/api_quotation.ts
@@ -509,13 +509,18 @@ export const useGetContract_id_noItems = (id: string | undefined) => {
       return;
     }
 
-    const newRes = await apiGetContract_Id(id, params);
+    try {
+      const newRes = await apiGetContract_Id(id, params);
 
-    if (newRes) {
-      setRes(newRes);
+      if (newRes) {
+        setRes(newRes);
+      }
+
+      return newRes;
+    } catch (error) {
+      const err = error as Error;
+      myAlert.err({ title: '取得合約資料失敗', content: err.message });
     }
-
-    return newRes;
   };
 
   return {
