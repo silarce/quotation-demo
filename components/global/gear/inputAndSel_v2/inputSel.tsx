a3205e4b7922905e3a24d22fd639b59359c71c73
Silarce <silarceanoux@gmail.com>
2023-12-11 15:15:36 +0800

    feat: 舊合約 門型欄位使用 inpuAndSelect 元件

--- components/global/gear/inputAndSel_v2/inputSel.tsx
+++ components/global/gear/inputAndSel_v2/inputSel.tsx
@@ -89,6 +89,7 @@ export type {
   TtimePickerProps,
   TtimePickerProps_mui,
   TinputSelBarProps,
+  TinputPropsAndSelectProps,
 };
 
 // =============================================================================
