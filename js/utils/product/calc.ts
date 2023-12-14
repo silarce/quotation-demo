17f17d2da3cb15250460e6870a2c795eb974e078
Silarce <silarceanoux@gmail.com>
2023-12-14 08:38:20 +0800

    fix: 報價單 修正W的計算公式

--- js/utils/product/calc.ts
+++ js/utils/product/calc.ts
@@ -50,7 +50,7 @@ export const calcProductFullWidth = ({ WG, gapA, gapC }: { WG: number; gapA: num
 // G就是門軌的width
 // 例如這個api get https://sanjeou-erp-be.caprover.credot-web.com/products/door/models
 // G = guideRails.width
-// 單位為mm
+// 注意要同樣長度單位
 export const calcW = ({ WG, G }: { WG: number; G: number }) => {
   return new Decimal(WG).sub(G).sub(G).toNumber();
 };
