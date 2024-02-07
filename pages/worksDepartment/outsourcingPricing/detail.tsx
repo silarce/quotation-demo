7b59e5067e85b79ec2af1ade4bd7a9a2084d8ef0
Your Name <you@example.com>
2024-02-07 18:38:23 +0800

    feat: 外包計價單明細 拿掉刪除按鈕。將特殊項目的options抽出

--- pages/worksDepartment/outsourcingPricing/detail.tsx
+++ pages/worksDepartment/outsourcingPricing/detail.tsx
@@ -31,6 +31,7 @@ import {
 
 // utils
 import { getTaiwanDateStr } from 'js/utils/helpers/date/convertDate';
+import { optionsCreator_otherWorkItems } from 'js/utils/options/options';
 
 // type
 import type { TquotationProductItemDto } from 'js/api/dtoTypes';
@@ -189,11 +190,11 @@ export default function OutsourcingPricingDetail() {
         setDisabled(false);
       },
     },
-    {
-      type: 'redButton',
-      label: '刪除',
-      onClick: () => {},
-    },
+    // {
+    //   type: 'redButton',
+    //   label: '刪除',
+    //   onClick: () => {},
+    // },
     {
       type: 'myButton',
       label: '返回',
@@ -655,7 +656,7 @@ const useTable02 = ({
               selectProps={{
                 value: otherInstallation,
                 placeholder: '工作內容...',
-                options: fakeData02_options,
+                options: optionsCreator_otherWorkItems(),
                 onChange: (str) => {
                   editState_otherWorkItem(index, 'otherInstallation', str);
                 },
@@ -872,58 +873,3 @@ const config_useTable02: Tconfig = {
 };
 
 // ===========================================================================
-
-const fakeData02_options = [
-  {
-    label: '打石',
-    value: '打石',
-  },
-  {
-    label: '門楣',
-    value: '門楣',
-  },
-  {
-    label: '特殊門軌',
-    value: '特殊門軌',
-  },
-  {
-    label: '修改補貼',
-    value: '修改補貼',
-  },
-  {
-    label: '卸貨',
-    value: '卸貨',
-  },
-  {
-    label: '公工',
-    value: '公工',
-  },
-  {
-    label: '遠程',
-    value: '遠程',
-  },
-  {
-    label: '外宿',
-    value: '外宿',
-  },
-  {
-    label: '載貨',
-    value: '載貨',
-  },
-  {
-    label: '活動中柱',
-    value: '活動中柱',
-  },
-  {
-    label: '防颱支撐',
-    value: '防颱支撐',
-  },
-  {
-    label: '送電',
-    value: '送電',
-  },
-  {
-    label: '修繕',
-    value: '修繕',
-  },
-];
