dcdf172af0aca8c3990982f4e9df39b93976404e
Your Name <you@example.com>
2024-03-01 11:43:11 +0800

    feat: 建立待辦事項page

---
+++ components/page/worksDepartment/todoList/table_TodoList.tsx
@@ -0,0 +1,256 @@
+import { useMemo } from 'react';
+
+// gear
+import InputSel from 'components/global/gear/inputAndSel_v2/inputSel';
+import Table01, { Ttable, Tconfig_table } from 'components/global/gear/table/table01';
+import Wrapper_tab from 'components/global/gear/wrapper_tab/wrapper_tab01';
+
+// icon
+import { IconRemoveCircle } from 'public/image/icon/svgComponent/svgIcons';
+
+// css
+import scss from './table_TodoList.module.scss';
+
+// =======================================================================
+
+// =======================================================================
+export default function Table_todoList() {
+  const control_table: Ttable = useMemo(() => {
+    const theadCellArr = [
+      {
+        ...configList.notificationDate,
+        children: configList.notificationDate.label,
+      },
+      {
+        ...configList.projectNumber,
+        children: configList.projectNumber.label,
+      },
+      {
+        ...configList.projectName,
+        children: configList.projectName.label,
+      },
+      {
+        ...configList.willArrivalDate,
+        children: configList.willArrivalDate.label,
+      },
+      {
+        ...configList.qty,
+        children: configList.qty.label,
+      },
+      {
+        ...configList.purpose,
+        children: configList.purpose.label,
+      },
+      {
+        ...configList.contactPerson,
+        children: configList.contactPerson.label,
+      },
+      {
+        ...configList.btnPanel,
+        children: null,
+      },
+    ];
+
+    const tbodyRowArr: Ttable['tbody']['rowArr'] = [
+      {
+        cellArr: [
+          {
+            ...configList.notificationDate,
+            children: '999年09月09號',
+          },
+          {
+            ...configList.projectNumber,
+            children: 'M-99999',
+          },
+          {
+            ...configList.projectName,
+            children: '肚子餓工程',
+          },
+          {
+            ...configList.willArrivalDate,
+            children: '999年09月09號',
+          },
+          {
+            ...configList.qty,
+            children: 99,
+          },
+          {
+            ...configList.purpose,
+            children: '早餐吃太少',
+          },
+          {
+            ...configList.contactPerson,
+            children: '沒有人',
+          },
+          {
+            ...configList.btnPanel,
+            children: (
+              <div className={scss.btnBar}>
+                <button onClick={() => alert('新增')}>新增</button>
+                <button onClick={() => alert('派工')}>派工</button>
+              </div>
+            ),
+          },
+        ],
+      },
+      //
+      {
+        cellArr: [
+          {
+            className: scss.fullCell,
+            children: (
+              <SubRow
+                onRemoveClick={() => alert('foooo')}
+                // onCheck={(arr) => alert(arr)}
+                content={
+                  '好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺'
+                }
+              />
+            ),
+          },
+        ],
+      },
+      {
+        cellArr: [
+          {
+            className: scss.fullCell,
+            children: (
+              <SubRow
+                onRemoveClick={() => alert('foooo')}
+                // onCheck={(arr) => alert(arr)}
+                isChecked={true}
+                content={
+                  '好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺'
+                }
+              />
+            ),
+          },
+        ],
+      },
+      {
+        cellArr: [
+          {
+            className: scss.fullCell,
+            children: (
+              <SubRow
+                onRemoveClick={() => alert('foooo')}
+                // onCheck={(arr) => alert(arr)}
+                content={
+                  '好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺\n好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺好想睡覺'
+                }
+              />
+            ),
+          },
+        ],
+      },
+    ];
+
+    const thead: Ttable['thead'] = {
+      cellArr: theadCellArr,
+    };
+
+    const tbody: Ttable['tbody'] = {
+      rowArr: tbodyRowArr,
+    };
+
+    return {
+      thead,
+      tbody,
+    };
+  }, []);
+
+  return (
+    <Wrapper_tab>
+      <div></div>
+      <div></div>
+      <div></div>
+      <Table01 {...control_table} />
+    </Wrapper_tab>
+  );
+}
+
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+
+const SubRow = ({
+  onRemoveClick,
+  onCheck,
+  isChecked,
+  content,
+}: {
+  onRemoveClick?: () => void;
+  onCheck?: (strArr: string[]) => void;
+  isChecked?: boolean;
+  content?: string;
+}) => {
+  return (
+    <div className={scss.subRow}>
+      <div>
+        <IconRemoveCircle onClick={onRemoveClick} />
+      </div>
+      <div>
+        <InputSel
+          className={scss.inputSel}
+          showBaseline="invisible"
+          checkBoxProps={{
+            onChange: onCheck,
+            propsArr: [
+              {
+                key: 'foo',
+                value: isChecked,
+              },
+            ],
+          }}
+        />
+      </div>
+      <div>{content}</div>
+    </div>
+  );
+};
+
+// ===================================================================
+
+const configList: { [key: string]: Tconfig_table } = {
+  notificationDate: {
+    label: '通知日期',
+    width: 135,
+    justifyContent: 'center',
+  },
+  projectNumber: {
+    label: '工程編號',
+    width: 135,
+    justifyContent: 'center',
+  },
+  projectName: {
+    label: '工程名稱',
+    width: 180,
+    justifyContent: 'center',
+  },
+  willArrivalDate: {
+    label: '預計進場日期',
+    width: 135,
+    justifyContent: 'center',
+  },
+  qty: {
+    label: '數量',
+    width: 60,
+    justifyContent: 'center',
+  },
+  purpose: {
+    label: '主旨',
+    flex: 'auto',
+    justifyContent: 'center',
+  },
+  contactPerson: {
+    label: '聯絡人',
+    width: 100,
+    justifyContent: 'center',
+  },
+  btnPanel: {
+    width: 160,
+    justifyContent: 'center',
+  },
+};
---
+++ pages/worksDepartment/todoList/index.tsx
@@ -0,0 +1,143 @@
+import { useRouter } from 'next/router';
+
+// conmponent
+import Table_todoList from 'components/page/worksDepartment/todoList/table_TodoList';
+
+// global gear
+import SubLayer from 'components/Layer/SubLayer/SubLayer';
+import PageHeader02, { TpanelList, TsearchGroup } from 'components/PageHeader/PageHeader02/PageHeader02';
+import SelectBar from 'components/global/gear/select/selectBar/selectBar';
+
+// option
+import { optionsCreator_year, optionsCreator_month, optionsCreator_region } from 'js/utils/options/options';
+
+// ===============================================================================
+
+type Tquery = {
+  year: string | undefined;
+  month: string | undefined;
+  region: string | undefined;
+  keyWord: string | undefined;
+};
+
+type TselectPropsArr = Parameters<typeof SelectBar>[0]['selectPropsArr'];
+// ===============================================================================
+
+const today = new Date();
+const thisYear = today.getFullYear() - 1911;
+const thisMonth = today.getMonth() + 1;
+
+const yearOptionArr = optionsCreator_year();
+const monthOptionArr = optionsCreator_month({ emptyOption: true });
+const regionOptionArr = optionsCreator_region({ emptyOption: true });
+
+// ===============================================================================
+export default function TodoList() {
+  const router = useRouter();
+  const {
+    //
+    year = String(thisYear),
+    month = String(thisMonth),
+    region,
+    keyWord,
+  }: Tquery = router.query as Tquery;
+
+  // ----------------------------------------------------------------
+
+  const selectPropsArr: TselectPropsArr = [
+    {
+      selectProps: {
+        value: year,
+        options: yearOptionArr,
+        onChange: (option) => {
+          if (typeof option?.value === 'string') {
+            router.push({
+              query: {
+                ...router.query,
+                year: option.value,
+              },
+            });
+          }
+        },
+      },
+      placeholder: '選擇年份',
+      boxStyle: { width: '140px' },
+    },
+    {
+      selectProps: {
+        value: month,
+        options: monthOptionArr,
+        onChange: (option) => {
+          if (typeof option?.value === 'string') {
+            router.push({
+              query: {
+                ...router.query,
+                month: option.value,
+              },
+            });
+          }
+        },
+      },
+      placeholder: '選擇月份',
+      boxStyle: { width: '140px' },
+    },
+    {
+      selectProps: {
+        value: region,
+        options: regionOptionArr,
+        onChange: (option) => {
+          if (typeof option?.value === 'string') {
+            router.push({
+              query: {
+                ...router.query,
+                region: option.value,
+              },
+            });
+          }
+        },
+      },
+      placeholder: '選擇區域',
+      boxStyle: { width: '140px' },
+    },
+  ];
+
+  const customeLeft = [
+    //
+    <SelectBar key="0" className="ml-[6px]" selectPropsArr={selectPropsArr} />,
+  ];
+
+  // ----------------------------------------------------------------
+
+  const searchTargetList = [
+    {
+      placeholder: '搜尋...',
+      defaultValue: keyWord,
+    },
+  ];
+
+  const searchGroup: TsearchGroup = {
+    searchTargetList,
+    doSearch: (arr) => {
+      const keyWord = arr[0] as string;
+
+      router.push({
+        query: {
+          ...router.query,
+          keyWord,
+        },
+      });
+    },
+  };
+
+  const panelList: TpanelList = [{ searchGroup }];
+
+  // ----------------------------------------------------------------
+  return (
+    <SubLayer>
+      <PageHeader02 tag="代辦事項" customeLeft={customeLeft} panelList={panelList} />
+      <div className={'py-24 px-10'}>
+        <Table_todoList />
+      </div>
+    </SubLayer>
+  );
+}
