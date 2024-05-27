a24d73e7a706b1646a4324f29d37a1d6f5c18f76
Your Name <you@example.com>
2024-05-27 15:53:04 +0800

    feat: 匯出報價單 將dlPdf與dlExcel抽出

---
+++ components/page/domestic/pdf/quotationPdf/quotationPdf_new3/dlPdf.ts
@@ -0,0 +1,60 @@
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import moment from 'moment';
+
+// global gear
+import { showRootLoading } from 'components/global/gear/loadingCover/rootLoadingCover';
+
+const dlPdf = async ({
+  ref_pdf,
+  fileName,
+}: {
+  ref_pdf: React.MutableRefObject<HTMLDivElement[]>;
+  fileName: string;
+}) => {
+  if (!ref_pdf.current[0]) {
+    return;
+  }
+
+  showRootLoading(true, '正在處理PDF');
+
+  const doc = new jsPDF('p', 'px', 'a4');
+  const pageWidth = doc.internal.pageSize.getWidth();
+
+  const pageHeight = doc.internal.pageSize.getHeight();
+
+  let isFirst = true;
+  let item;
+
+  for (item of ref_pdf.current) {
+    if (!item) {
+      continue;
+    }
+
+    const image = await html2canvas(item, {
+      scale: 3,
+      // useCORS: true,
+      // allowTaint: true,
+    }).then((canvas) => {
+      const image = canvas.toDataURL('image/JPEG');
+
+      return image;
+    });
+
+    if (!isFirst) {
+      doc.addPage();
+    }
+
+    isFirst = false;
+    // 留作參考
+    // doc.addImage(image, "JPEG", 0, 0, 595, 842);
+    // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
+    doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
+  }
+
+  const today = moment().format('yyyy-MM-DD');
+  doc.save(`${fileName}_${today}.pdf`);
+  showRootLoading(false);
+};
+
+export { dlPdf };
