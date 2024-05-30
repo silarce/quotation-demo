ae5644d015c3191f23b19f62be8c2f13e31f6002
Your Name <you@example.com>
2024-05-30 11:16:28 +0800

    feat: 收款管理 建立api client

--- js/api/api_accountant.ts
+++ js/api/api_accountant.ts
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useInView } from 'react-intersection-observer';
 import _ from 'lodash';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
 
 import { axi } from './_axiosCreator';
+import { AxiosError } from 'axios';
 
 import { createUseInfinite } from './createUseInfinite';
 
@@ -12,24 +13,25 @@ import type {
   //
   Tparams,
   TpageMetaDto,
+  TpageResponse,
   TaccountantDto,
   TcreateAccountantDto,
+  TupdateAccountantDto,
 } from './dtoTypes';
 
 export type {
   //
   Tparams,
   TpageMetaDto,
+  TpageResponse,
   TaccountantDto,
   TcreateAccountantDto,
+  TupdateAccountantDto,
 } from './dtoTypes';
 
-type TgetAccountant = {
-  data: TaccountantDto[];
-  meta: TpageMetaDto;
-};
+type TgetAccountant = TpageResponse<TaccountantDto>;
 
-export const apiGetAccountant = async (params?: Tparams) => {
+const apiGetAccountant = async (params?: Tparams) => {
   const api = '/accountant';
 
   return axi
@@ -38,16 +40,127 @@ export const apiGetAccountant = async (params?: Tparams) => {
     .catch((err) => Promise.reject(err));
 };
 
-export const useGetAccountant = createUseInfinite<TgetAccountant>({
+const useGetAccountant_infinite = createUseInfinite<TgetAccountant>({
   apiClient: apiGetAccountant,
   errTitle: '取得收款紀錄列表失敗',
 });
 
-export const apiPostAccountant = async (dto: TcreateAccountantDto) => {
+const useGetAccountant = ({
+  params,
+  autoUpdate = true,
+  callAlert = true,
+}: {
+  params?: Tparams;
+  autoUpdate?: boolean;
+  callAlert?: boolean;
+}) => {
+  const [res, setRes] = useState<TgetAccountant>();
+  const [isFetching, setIsFetching] = useState(false);
+
+  const update = useCallback(async () => {
+    setIsFetching(true);
+
+    try {
+      const res = await apiGetAccountant(params);
+      setRes(res);
+
+      return res;
+    } catch (error) {
+      const err = error as AxiosError;
+      callAlert &&
+        myAlert.err({
+          title: '取得收款紀錄列表失敗',
+          content: err.message,
+        });
+
+      return err;
+    } finally {
+      setIsFetching(false);
+    }
+  }, [callAlert, params]);
+
+  useEffect(() => {
+    autoUpdate && update();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [autoUpdate, params]);
+
+  return {
+    data: res?.data,
+    meta: res?.meta,
+    update,
+    isFetching,
+  };
+};
+
+const apiPostAccountant = async ({
+  //
+  body,
+  callAlert = true,
+}: {
+  body: TcreateAccountantDto;
+  callAlert?: boolean;
+}) => {
   const api = '/accountant';
 
   return axi
-    .post<TaccountantDto>(api, dto)
+    .post<TaccountantDto>(api, body)
     .then(({ data }) => data)
-    .catch((err) => Promise.reject(err));
+    .catch((err) => {
+      callAlert && myAlert.err({ title: '新增收款紀錄失敗', content: err.message });
+
+      return Promise.reject(err);
+    });
+};
+
+const apiPatchAccountant = async (
+  id: string,
+  {
+    //
+    body,
+    callAlert = true,
+  }: {
+    body: TupdateAccountantDto;
+    callAlert?: boolean;
+  }
+) => {
+  const api = `/accountant/${id}`;
+
+  return axi
+    .patch<TaccountantDto>(api, body)
+    .then(({ data }) => data)
+    .catch((err) => {
+      callAlert && myAlert.err({ title: '更新收款紀錄失敗', content: err.message });
+
+      return Promise.reject(err);
+    });
+};
+
+const deleteAccountant = async (
+  id: string,
+  {
+    callAlert = true,
+  }: {
+    callAlert?: boolean;
+  } = {}
+) => {
+  const api = `/accountant/${id}`;
+
+  return axi
+    .delete(api)
+    .then(({ data }) => data)
+    .catch((err) => {
+      callAlert && myAlert.err({ title: '刪除收款紀錄失敗', content: err.message });
+
+      return Promise.reject(err);
+    });
+};
+
+export {
+  apiGetAccountant,
+  apiPostAccountant,
+  apiPatchAccountant,
+  deleteAccountant,
+  //
+  useGetAccountant_infinite,
+  useGetAccountant,
 };
