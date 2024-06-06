1243bab6bab622e2cb96f76407e81c5bc5782b63
Your Name <you@example.com>
2024-06-06 09:30:42 +0800

    feat:  應收帳款管理 完成UI

--- components/page/worksDepartment/contracList/contract/accountReceivable/accountantSorting.tsx
+++ components/page/worksDepartment/contracList/contract/accountReceivable/accountantSorting.tsx
@@ -8,6 +8,30 @@ import MyButton_v2 from 'components/global/gear/button/myButton_v2';
 import scss from './accountantSorting.module.scss';
 
 // ================================================================================
+
+type Tinvoice = {
+  id: string;
+
+  invoiceNumber: React.ReactNode;
+  invoiceDate: React.ReactNode;
+  price: React.ReactNode;
+};
+
+type Taccountant = {
+  id: string;
+
+  insertDate: React.ReactNode;
+  importAccountingNumber: React.ReactNode;
+  noteMaturityDate: React.ReactNode; // 票據到期日
+  price: React.ReactNode;
+};
+
+type Tstate = {
+  invoice: Tinvoice;
+  accountantArr: Taccountant[];
+};
+
+// ================================================================================
 // region START
 export default function AccountantSorting({ className }: { className?: string }) {
   // -----------------------------------------------------------------------------
@@ -22,28 +46,104 @@ export default function AccountantSorting({ className }: { className?: string })
       </TopBar>
       <div className={scss.table}>
         <Group className={classNames(scss.top)}>
-          <Left className={scss.left}>發票開立資訊</Left>
-          <Right className={scss.right}>已收款項明細</Right>
+          <Left>發票開立資訊</Left>
+          <Right>已收款項明細</Right>
         </Group>
         {/*  */}
         <Group className={scss['thead']}>
-          <Left className={scss.left}>
-            <span>發票號碼</span>
-            <span>開立日期</span>
-            <span>開立金額</span>
+          <Left>
+            <Row>
+              <span>發票號碼</span>
+              <span>開立日期</span>
+              <span>開立金額</span>
+            </Row>
           </Left>
-          <Right className={scss.right}>
-            <span>收款日期</span>
-            <span>帳號/號碼</span>
-            <span>到期日</span>
-            <span>收款金額</span>
+          <Right>
+            <Row>
+              <span>收款日期</span>
+              <span>帳號/號碼</span>
+              <span>到期日</span>
+              <span>收款金額</span>
+            </Row>
           </Right>
         </Group>
         {/*  */}
-        <Group className={scss['foo']}></Group>
-        <Group className={scss['foo']}></Group>
-        <Group className={scss['foo']}></Group>
-        <Group className={scss['foo']}></Group>
+
+        {fakeStateArr.map((state) => {
+          const { invoice, accountantArr } = state;
+          const { id, invoiceNumber, invoiceDate, price } = invoice;
+
+          return (
+            <Group key={id} className={scss['tbody']}>
+              <Left>
+                <Row>
+                  <span>{invoiceNumber}</span>
+                  <span>{invoiceDate}</span>
+                  <span>{price}</span>
+                </Row>
+              </Left>
+              <Right>
+                {accountantArr.map((accountant) => {
+                  const {
+                    //
+                    id,
+                    insertDate,
+                    importAccountingNumber,
+                    noteMaturityDate,
+                    price,
+                  } = accountant;
+
+                  return (
+                    <Row key={id}>
+                      <span>{insertDate}</span>
+                      <span>{importAccountingNumber}</span>
+                      <span>{noteMaturityDate}</span>
+                      <span>{price}</span>
+                    </Row>
+                  );
+                })}
+              </Right>
+            </Group>
+          );
+        })}
+
+        <Group className={scss['total']}>
+          <Left>
+            <Row>
+              <span></span>
+              <span>合計</span>
+              <span>20000</span>
+            </Row>
+          </Left>
+          <Right>
+            <Row>
+              <span></span>
+              <span></span>
+              <span>合計</span>
+              <span>50000</span>
+            </Row>
+          </Right>
+        </Group>
+
+        <div className={scss.footCaption}>已開立發票未收款項</div>
+
+        <Group className={scss['total']}>
+          <Left>
+            <Row>
+              <span></span>
+              <span>合計</span>
+              <span>20000</span>
+            </Row>
+          </Left>
+          <Right>
+            <Row>
+              <span></span>
+              <span></span>
+              <span>合計</span>
+              <span>50000</span>
+            </Row>
+          </Right>
+        </Group>
       </div>
     </div>
   );
@@ -70,3 +170,83 @@ const Right = ({ className, children }: { className?: string; children?: React.R
 const Row = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
   return <div className={classNames(scss.row, className)}>{children}</div>;
 };
+
+// ================================================================================
+
+// region fake data
+// length ===3
+// id is random
+const fakeStateArr: Tstate[] = [
+  {
+    invoice: {
+      id: 'i-fdsagasdgas',
+      invoiceNumber: 'A123456',
+      invoiceDate: '2021/01/01',
+      price: '1000',
+    },
+    accountantArr: [
+      {
+        id: 'a-sdfasfasd',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+    ],
+  },
+  {
+    invoice: {
+      id: 'i-fsdfsdbdgefd',
+      invoiceNumber: 'A123456',
+      invoiceDate: '2021/01/01',
+      price: '1000',
+    },
+    accountantArr: [
+      {
+        id: 'a-ghhghsd',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+      {
+        id: 'a-dsdghsdf',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+    ],
+  },
+  {
+    invoice: {
+      id: 'i-gjdfgbndfghtr',
+      invoiceNumber: 'A123456',
+      invoiceDate: '2021/01/01',
+      price: '1000',
+    },
+    accountantArr: [
+      {
+        id: 'a-gsdfhfghe',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+      {
+        id: 'a-gdfgsdfgsdjfyjkdfg',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+      {
+        id: 'a-gdsfytyiygds',
+        insertDate: '2021/01/01',
+        importAccountingNumber: 'A123456',
+        noteMaturityDate: '2021/01/01',
+        price: '1000',
+      },
+    ],
+  },
+];
