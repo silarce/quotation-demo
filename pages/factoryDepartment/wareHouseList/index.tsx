576c20c8cef994e57178ca78f6395068f6103db3
LarryLai <larrylai0306@gmail.com>
2024-06-19 17:39:05 +0800

    202406191738

--- pages/factoryDepartment/wareHouseList/index.tsx
+++ pages/factoryDepartment/wareHouseList/index.tsx
@@ -25,7 +25,7 @@ type Tquery = {
 //     type: string;
 // }
 export const setting = {
-    apipath: 'http://new-erp-api.beta.san-jeou.com.tw/', // 確保這裡包含正確的 API 路徑
+    apipath: 'http://new-erp-api.beta.san-jeou.com.tw/WareHouse', // 確保這裡包含正確的 API 路徑
 };
 
 
