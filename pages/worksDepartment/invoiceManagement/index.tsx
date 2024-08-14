52bc30d34988dd9c542c576a9cd0887d1a828928
Your Name <you@example.com>
2024-08-14 14:13:25 +0800

    feat: 工務部 建立開立發票管理 page，內容與會計部的建立開立發票管理相同

---
+++ pages/worksDepartment/invoiceManagement/index.tsx
@@ -0,0 +1,7 @@
+import InvoiceManagement_com from 'components/wholePage/invoiceManagement';
+
+import type { TmyPageProps } from 'pages/_app';
+
+export default function InvoiceManagement(props: TmyPageProps) {
+  return <InvoiceManagement_com {...props} />;
+}
