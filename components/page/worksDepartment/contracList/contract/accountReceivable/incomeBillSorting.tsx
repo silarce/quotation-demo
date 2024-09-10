70a3066fc5a01609bd1c6016af493778c4f84c89
Your Name <you@example.com>
2024-09-10 14:25:33 +0800

    feat:工務部 應收帳款明細 應收帳款管理 修改發票合計的計算公式

--- components/page/worksDepartment/contracList/contract/accountReceivable/incomeBillSorting.tsx
+++ components/page/worksDepartment/contracList/contract/accountReceivable/incomeBillSorting.tsx
@@ -187,9 +187,12 @@ export default function IncomeBillSorting({
       const incomeBillList = invoice.incomeBillSerialList;
 
       // const price = period.price || 0;
-      const price = invoice.actualPrice || 0;
+      // const price = invoice.actualPrice || 0;
+      const { actualPrice, allowance } = invoice;
 
-      total_invoice_d = total_invoice_d.add(price || 0);
+      console.log(invoice);
+
+      total_invoice_d = total_invoice_d.add(actualPrice || 0).minus(allowance || 0);
 
       incomeBillList?.forEach((incomeBill) => {
         total_incomeBill_d = total_incomeBill_d.add(incomeBill.receivablePayment || 0);
