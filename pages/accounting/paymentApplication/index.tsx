e08da01077e84cdae0614fab8bdc76110b1891ab
Your Name <you@example.com>
2024-09-23 15:13:10 +0800

    feat: 會計部 付款申請 製作狀態管理

--- pages/accounting/paymentApplication/index.tsx
+++ pages/accounting/paymentApplication/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useMemo } from 'react';
+
 import classNames from 'classnames';
 
 import SubLayer from 'components/Layer/SubLayer/SubLayer';
@@ -5,23 +7,84 @@ import PageHeader02 from 'components/PageHeader/PageHeader02/PageHeader02';
 
 // component
 import Table_paymentApplication from 'components/page/accounting/paymentApplication/table_paymentApplication';
+import { SearchModal_customer } from 'components/composition/searchModal/useSearchModal/useSearchModal_customer';
+import { SearchModal_invoice } from 'components/composition/searchModal/useSearchModal/useSearchModal_invoice';
 
 // gear
 import SquareBtn from 'components/global/gear/button/larrysBtn/squarebtn';
 import InputSel, { TinputSelProps } from 'components/global/gear/inputAndSel_v2/inputSel';
-import MyButton_v2 from 'components/global/gear/button/myButton_v2';
+import DragableModal from 'components/global/gear/dragableModal/dragableModal';
+
+// utils
+import { getTaiwanDateStr } from 'js/utils/helpers/date/convertDate';
 
 import scss from './index.module.scss';
 
 // =========================================================================
+
+interface TfakeDto {
+  id: string;
+
+  付款申請單號: string;
+  申請日期: string;
+  經辦人員: string;
+  廠商代號: string;
+  扣款類別: string;
+  立帳方式: string;
+  發票號碼: string;
+}
+
+interface state {
+  id?: string;
+
+  付款申請單號: string;
+  申請日期: string;
+  經辦人員: string;
+  廠商代號: string;
+  扣款類別: string;
+  立帳方式: string;
+  發票號碼: string;
+}
+
+// interface TinputSelPr
+
+// =========================================================================
 export default function PaymentApplication() {
+  const defaultState = useDefaultState(fakeData);
+
+  const [disabled, setDisabled] = useState(true);
+  const [state, setState] = useState<state>(defaultState);
+
+  // 主表資料要放在這一層，資料同時要送到Profile與Table_paymentApplication
+
+  // --------------------------------------------------------------------------
+
+  const onEdit = () => {
+    setDisabled(false);
+  };
+
+  const onCancel = () => {
+    setDisabled(true);
+  };
+
+  const onConfirm = () => {
+    setDisabled(true);
+  };
+
+  // --------------------------------------------------------------------------
+
+  useEffect(() => {
+    setState(defaultState);
+  }, [defaultState, disabled]);
+
+  // --------------------------------------------------------------------------
   return (
     <SubLayer bodyPreStyle="style01">
       <PageHeader02 tag="付款申請" />
 
       <div>
-        <BtnBar className="mb-5" />
-        <Profile className="mb-5" />
+        <BtnBar className="mb-5" disabled={disabled} onEdit={onEdit} onCancel={onCancel} onConfirm={onConfirm} />
+        <Profile className="mb-5" disabled={disabled} state={state} setState={setState} />
         <Table_paymentApplication />
       </div>
     </SubLayer>
@@ -30,61 +93,191 @@ export default function PaymentApplication() {
 
 // =========================================================================
 
-const BtnBar = ({ className }: { className?: string }) => {
+const BtnBar = ({
+  disabled,
+  onEdit,
+  onCancel,
+  onConfirm,
+  className,
+}: {
+  disabled: boolean;
+
+  onEdit: () => void;
+  onCancel: () => void;
+  onConfirm: () => void;
+  className?: string;
+}) => {
   return (
     <div className={classNames(scss.btnBar, className)}>
       <div>
         <SquareBtn content="search" />
       </div>
       <div className="flex gap-1">
-        <SquareBtn content="save" />
-        <SquareBtn content="cancel" />
+        {disabled && (
+          <>
+            <SquareBtn className="invisible" />
+            <SquareBtn content="edit" onClick={onEdit} />
+          </>
+        )}
+        {!disabled && (
+          <>
+            <SquareBtn content="save" onClick={onConfirm} />
+            <SquareBtn content="cancel" theme="danger" onClick={onCancel} />
+          </>
+        )}
       </div>
       <div>
-        <SquareBtn content="delete" />
+        <SquareBtn content="delete" theme="danger" />
       </div>
     </div>
   );
 };
 
-const inputSelConfig_profile: TinputSelProps = {
-  captionSize: '18',
-  fontSize: '18',
-  captionStyle: { width: 120 },
-  // wrapperStyle: { width: 250 },
-};
+const Profile = ({
+  //
+  disabled,
+  state,
+  setState,
+  className,
+}: {
+  disabled: boolean;
+  className?: string;
+  state: state;
+  setState: React.Dispatch<React.SetStateAction<state>>;
+}) => {
+  const inputSelConfig_profile: TinputSelProps = {
+    disabled,
+    showBaseline: 'auto',
+    captionSize: '18',
+    fontSize: '18',
+    captionStyle: { width: 120 },
+    // wrapperStyle: { width: 250 },
+  };
 
-const Profile = ({ className }: { className?: string }) => {
+  const selectCustomer = () => {
+    const { unmount } = DragableModal.create({
+      children: (
+        <SearchModal_customer
+          onRowClick={(customer) => {
+            setState((prev) => {
+              return { ...prev, 廠商代號: customer.customerNumber };
+            });
+            unmount();
+          }}
+        />
+      ),
+    });
+  };
+
+  const selectInvoice = () => {
+    const { unmount } = DragableModal.create({
+      children: (
+        <SearchModal_invoice
+          onRowClick={(invoice) => {
+            setState((prev) => {
+              return { ...prev, 發票號碼: invoice.invoiceNumber };
+            });
+            unmount();
+          }}
+        />
+      ),
+    });
+  };
+
   return (
     <div className={classNames(scss.profile, className)}>
-      <InputSel {...inputSelConfig_profile} caption="付款申請單號" />
-      <InputSel {...inputSelConfig_profile} caption="申請日期" />
-      <InputSel {...inputSelConfig_profile} caption="經辦人員" />
+      <InputSel {...inputSelConfig_profile} caption="付款申請單號" showBaseline="invisible" node={state.付款申請單號} />
+      <InputSel
+        {...inputSelConfig_profile}
+        caption="申請日期"
+        showBaseline="invisible"
+        node={getTaiwanDateStr(state.申請日期)}
+      />
+      <InputSel {...inputSelConfig_profile} caption="經辦人員" showBaseline="invisible" node={state.經辦人員} />
 
       <InputSel
         {...inputSelConfig_profile}
         caption="廠商代號"
-        suffix={<SquareBtn className="mr-2" sharp="mini" label="選擇廠商" onClick={() => {}} />}
-        inputProps={{}}
+        htmlFor=""
+        showBaseline="invisible"
+        suffix={
+          <SquareBtn
+            className={classNames('mr-2', disabled && 'invisible')}
+            sharp="mini"
+            label="選擇廠商"
+            onClick={selectCustomer}
+          />
+        }
+        node={state.廠商代號}
       />
-      <InputSel {...inputSelConfig_profile} caption="扣稅類別" />
-      <InputSel {...inputSelConfig_profile} caption="立帳方式" />
+      <InputSel
+        {...inputSelConfig_profile}
+        caption="扣稅類別"
+        inputProps={{
+          props: {
+            value: state.扣款類別,
+            onChange: (e) => {
+              setState((prev) => {
+                return { ...prev, 扣款類別: e.target.value };
+              });
+            },
+          },
+        }}
+      />
+      <InputSel
+        {...inputSelConfig_profile}
+        caption="立帳方式"
+        inputProps={{
+          props: {
+            value: state.立帳方式,
+            onChange: (e) => {
+              setState((prev) => {
+                return { ...prev, 立帳方式: e.target.value };
+              });
+            },
+          },
+        }}
+      />
 
       <InputSel
         {...inputSelConfig_profile}
         caption="發票號碼"
+        htmlFor=""
+        showBaseline="invisible"
         suffix={
           <SquareBtn
-            className="mr-2"
+            className={classNames('mr-2', disabled && 'invisible')}
             sharp="mini"
             label="選擇發票"
-            onClick={() => {
-              console.log('fooo');
-            }}
+            onClick={selectInvoice}
           />
         }
-        inputProps={{}}
+        node={state.發票號碼}
       />
     </div>
   );
+};
+
+// =========================================================================
+
+const useDefaultState = (fakeData: TfakeDto) => {
+  const defaultState: state = useMemo(() => {
+    return { ...fakeData };
+  }, [fakeData]);
+
+  return defaultState;
+};
+
+// =========================================================================
+
+const fakeData: TfakeDto = {
+  id: '1',
+
+  付款申請單號: '123',
+  申請日期: '2021/01/01',
+  經辦人員: 'larry',
+  廠商代號: '456',
+  扣款類別: '789',
+  立帳方式: '101112',
+  發票號碼: '131415',
 };
