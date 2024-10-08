07426ae0349eefa644a37e1c3c459179c738bd58
Your Name <you@example.com>
2024-10-08 10:44:30 +0800

    fix:報價單 修正匯率或外幣計價可能送空字串導致的500錯誤

--- pages/domestic/quotationList/attachQuotation/index.tsx
+++ pages/domestic/quotationList/attachQuotation/index.tsx
@@ -747,7 +747,7 @@ function TheQuotation({ router }: { router: NextRouter }) {
       trackProgress: state_profile.trackProgress ?? '',
       projectProgress: state_profile.projectProgress ?? '',
 
-      discount: `${Number(state_summary.discountRate ?? 0)}` ?? '100',
+      discount: `${Number(state_summary.discountRate ?? 100)}`,
       tuneTotal: state_summary.tuneTotal ?? '0',
       subTotal: Number(state_summary.subTotal.replaceAll(',', '')),
       salesTax: Number(state_summary.salesTax.replaceAll(',', '')),
@@ -755,8 +755,8 @@ function TheQuotation({ router }: { router: NextRouter }) {
       deliveryLocation: state_summary.deliveryLocation,
       deliveryDate: state_summary.deliveryDate,
       paymentMethods: paymentMethod,
-      exchangeRate: state_summary.exchangeRate,
-      foreignTotal: state_summary.foreignTotal.replaceAll(',', ''),
+      exchangeRate: state_summary.exchangeRate || null,
+      foreignTotal: state_summary.foreignTotal.replaceAll(',', '') || null,
       currency: state_summary.currency,
       //
       //
