5b779f3e1b48e0e01de3aeae5a2ebe30b0b58b1a
Your Name <you@example.com>
2024-10-24 09:51:46 +0800

    feat:會計部/應付帳款明細表-建立header

--- pages/accounting/accountsPayableDetailList/index.tsx
+++ pages/accounting/accountsPayableDetailList/index.tsx
@@ -3,13 +3,14 @@ import classNames from 'classnames';
 import { nanoid } from 'nanoid';
 import moment, { Moment } from 'moment';
 import Decimal from 'decimal.js';
+import { useRouter } from 'next/router';
 
 // antd
 import { Spin } from 'antd';
 
 // layer
 import SubLayer from 'components/Layer/SubLayer/SubLayer';
-import PageHeader02 from 'components/PageHeader/PageHeader02/PageHeader02';
+import PageHeader02, { TpanelList } from 'components/PageHeader/PageHeader02/PageHeader02';
 
 // gear
 import SquareBtn from 'components/global/gear/button/larrysBtn/squarebtn';
@@ -17,6 +18,7 @@ import InputSel from 'components/global/gear/inputAndSel_v2/inputSel';
 import DragableModal from 'components/global/gear/dragableModal/dragableModal';
 import ThreePartBar from 'components/global/container/bar/threePartBar';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
+import { useYearMonth_options, useYearMonth_selectBar_query, SelectBar } from 'js/utils/helpers/hook/useYearMonth';
 
 import type { TuserDto } from 'js/api/dtoTypes';
 
@@ -25,6 +27,10 @@ import { useTranslation } from 'react-i18next';
 import scss from './index.module.scss';
 
 // ================================================================================
+type Tquery = {
+  year?: string;
+  month?: string;
+};
 
 // ================================================================================
 
@@ -38,7 +44,11 @@ export default function AccountsPayableDetailList() {
   // MARK: RENDER
   return (
     <SubLayer>
-      <PageHeader02 tag="應收帳款明細" />
+      <PageHeader02
+        tag="應付帳款明細表"
+        customeLeft={[<DateSelector key="DateSelector" />]}
+        panelList={createPanelList()}
+      />
 
       <div>
         <Spin spinning={false} delay={300}>
@@ -54,3 +64,38 @@ export default function AccountsPayableDetailList() {
 // ==============================================================================
 // ==============================================================================
 // ==============================================================================
+
+const createPanelList = () => {
+  const panelList: TpanelList = [
+    {
+      type: 'myButton',
+      label: '產生當月應付帳款',
+      onClick: () => {
+        console.log('產生當月應付帳款');
+      },
+    },
+  ];
+
+  return panelList;
+};
+
+const DateSelector = () => {
+  const router = useRouter();
+  const query = router.query as Tquery;
+
+  const { yearOptionArr, monthOptionArr, thisYear, thisMonth } = useYearMonth_options();
+  const {
+    //
+    year = thisYear.toString(),
+    month = thisMonth.toString(),
+  } = query;
+
+  const selectPropsArr = useYearMonth_selectBar_query({
+    year: year,
+    month: month,
+    yearOptionArr,
+    monthOptionArr,
+  });
+
+  return <SelectBar key="selectBar" className="ml-10" selectPropsArr={selectPropsArr} />;
+};
