9b250ef84948166b2c4348e0bd7f270fed081fde
Your Name <you@example.com>
2024-10-28 09:45:14 +0800

    feat:會計部/應付帳款明細表-放上搜尋UI。暫時停工，待UI與功能確認後再繼續。

--- pages/accounting/accountsPayableDetailList/index.tsx
+++ pages/accounting/accountsPayableDetailList/index.tsx
@@ -10,7 +10,7 @@ import { Spin } from 'antd';
 
 // layer
 import SubLayer from 'components/Layer/SubLayer/SubLayer';
-import PageHeader02, { TpanelList } from 'components/PageHeader/PageHeader02/PageHeader02';
+import PageHeader02, { TpanelList, TsearchGroup } from 'components/PageHeader/PageHeader02/PageHeader02';
 
 // gear
 import SquareBtn from 'components/global/gear/button/larrysBtn/squarebtn';
@@ -19,6 +19,7 @@ import DragableModal from 'components/global/gear/dragableModal/dragableModal';
 import ThreePartBar from 'components/global/container/bar/threePartBar';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
 import { useYearMonth_options, useYearMonth_selectBar_query, SelectBar } from 'js/utils/helpers/hook/useYearMonth';
+import Row, { Cell } from 'components/global/gear/table/row';
 
 import type { TuserDto } from 'js/api/dtoTypes';
 
@@ -27,9 +28,25 @@ import { useTranslation } from 'react-i18next';
 import scss from './index.module.scss';
 
 // ================================================================================
+
+// interface Tdata {
+//   應付帳款單號: string | null;
+//   廠商編號: string | null;
+//   發票廠商: string | null;
+//   付款帳號: string | null;
+//   支票號碼: string | null;
+//   發票廠商: string | null;
+//   發票號碼: string | null;
+//   發票金額: string | null;
+//   付款狀態: string | null;
+//   發票日期: string | null;
+// }
+
+// ================================================================================
 type Tquery = {
   year?: string;
   month?: string;
+  invoiceNumber?: string;
 };
 
 // ================================================================================
@@ -47,7 +64,7 @@ export default function AccountsPayableDetailList() {
       <PageHeader02
         tag="應付帳款明細表"
         customeLeft={[<DateSelector key="DateSelector" />]}
-        panelList={createPanelList()}
+        panelList={usePanelList()}
       />
 
       <div>
@@ -65,8 +82,41 @@ export default function AccountsPayableDetailList() {
 // ==============================================================================
 // ==============================================================================
 
-const createPanelList = () => {
+// MARK:usePanelList
+
+const usePanelList = () => {
+  const router = useRouter();
+  const query = router.query as Tquery;
+  const { invoiceNumber } = query;
+
+  const searchGroup: TsearchGroup = {
+    searchTargetList: [
+      {
+        defaultValue: invoiceNumber,
+        placeholder: '以發票號碼搜尋',
+      },
+    ],
+
+    doSearch: (searchValueArr) => {
+      const invoiceNumber = searchValueArr[0] as string;
+
+      const newQuery: Tquery = {
+        ...query,
+        invoiceNumber,
+      };
+
+      if (!newQuery.invoiceNumber) {
+        delete newQuery.invoiceNumber;
+      }
+
+      router.replace({
+        query: newQuery,
+      });
+    },
+  };
+
   const panelList: TpanelList = [
+    { searchGroup },
     {
       type: 'myButton',
       label: '產生當月應付帳款',
@@ -79,6 +129,7 @@ const createPanelList = () => {
   return panelList;
 };
 
+// MARK: DateSelector
 const DateSelector = () => {
   const router = useRouter();
   const query = router.query as Tquery;
@@ -99,3 +150,5 @@ const DateSelector = () => {
 
   return <SelectBar key="selectBar" className="ml-10" selectPropsArr={selectPropsArr} />;
 };
+
+// ==========================================================================
