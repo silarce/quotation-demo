c0223e9157cb1803a050487eddaa59db11f77381
Your Name <you@example.com>
2024-11-22 09:31:18 +0800

    feat:工務部/收入傳票管理-修改合約編號欄位的樣式

--- components/page/worksDepartment/incomeSummons/summon.tsx
+++ components/page/worksDepartment/incomeSummons/summon.tsx
@@ -28,8 +28,6 @@ import { TreqPatch } from 'pages/worksDepartment/incomeSummons';
 import { IconCheck02, IconEdit } from 'public/image/icon/svgComponent/svgIcons';
 // import { UpDownArrow } from 'components/global/myAntd/collapse';
 
-import scss from './summon.module.scss';
-
 // untils
 import { getTaiwanDateStr } from 'js/utils/helpers/date/convertDate';
 import { calcIncomeBillUnpaidPayment, calcIncomeBillExchangeBenefits } from 'js/utils/calc/calcIncomeBill';
@@ -43,6 +41,9 @@ import { AppContext } from 'pages/_app';
 import { optionsCreator_currency } from 'js/utils/options/options';
 
 import { apiGetContract } from 'js/api/api_quotation';
+
+// css
+import scss from './summon.module.scss';
 
 // ============================================================================
 
@@ -475,21 +476,6 @@ const cellPropsList_summon: TcellPropsList_summon = {
       setState_incomeBillSerial: setState_incomeBillSerial,
       router,
     }) => {
-      const inputProps: TinputSelProps['inputProps'] = {
-        props: {
-          className: 'text-center',
-          value: state_incomeBillSerial.contractNumber,
-          onChange: (e) => {
-            setState_incomeBillSerial((prev) => {
-              return {
-                ...prev,
-                contractNumber: e.target.value,
-              };
-            });
-          },
-        },
-      };
-
       const pushToAccountReceivable = async () => {
         await apiGetContract({
           filter: {
@@ -516,15 +502,33 @@ const cellPropsList_summon: TcellPropsList_summon = {
           });
       };
 
-      const node = (
-        <span className={'cursor-pointer'} onClick={() => pushToAccountReceivable()}>
-          {state_incomeBillSerial.contractNumber}
-        </span>
-      );
+      const inputSelProps: TinputSelProps = {
+        inputProps: {
+          props: {
+            value: state_incomeBillSerial.contractNumber,
+            onChange: (e) => {
+              setState_incomeBillSerial((prev) => {
+                return {
+                  ...prev,
+                  contractNumber: e.target.value,
+                };
+              });
+            },
+          },
+        },
+      };
 
-      const props = disabled ? { node } : { inputProps };
+      if (disabled && !!state_incomeBillSerial.contractNumber) {
+        inputSelProps.onClick = pushToAccountReceivable;
+        inputSelProps.disabled = false;
+        inputSelProps.showBaseline = 'invisible';
 
-      return props;
+        const inputPropsProps = inputSelProps.inputProps!.props!;
+        inputPropsProps.readOnly = true;
+        inputPropsProps.className = scss.inputLink;
+      }
+
+      return inputSelProps;
     },
   },
   projectName: {
