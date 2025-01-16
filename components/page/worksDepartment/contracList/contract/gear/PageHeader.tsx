e90bb2132cdf4ce926a78683c14dc3126c94421c
Your Name <you@example.com>
2025-01-16 10:45:23 +0800

    feat:工務部-PageHeader新增參數linkForbidden，使可以不渲染頁籤超連結

--- components/page/worksDepartment/contracList/contract/gear/PageHeader.tsx
+++ components/page/worksDepartment/contracList/contract/gear/PageHeader.tsx
@@ -42,11 +42,13 @@ export default function PageHeader({
   panelList = [],
   contractNumber = '未取得',
   returnBtn = true,
+  linkForbidden,
 }: {
   tagCallback?: (contractId: string) => string;
   panelList?: TpanelList;
   contractNumber?: string;
   returnBtn?: boolean;
+  linkForbidden?: boolean;
 }) {
   const { erpFeature } = useContext(AppContext);
   const history_contractList = useUrlHistory((state) => state.contractList);
@@ -285,7 +287,7 @@ export default function PageHeader({
       {/* 上面的 */}
       <PageHeader02 tag={tag} panelList={panelList} />
       {/* 下面的 */}
-      <PageHeaderFlex01 linkList={linkList} />
+      {!linkForbidden && <PageHeaderFlex01 linkList={linkList} />}
     </div>
   );
 }
