becae67a73ff55dc0f1f04a603e5bd11a26f4f18
Your Name <you@example.com>
2025-01-16 17:06:58 +0800

    feat:調貨單-材質規格欄位現在可以key in輸入

--- components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/editTransfer.tsx
+++ components/page/worksDepartment/contracList/contract/listOfDeliveryOrders/editTransfer.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
 
 // global gear
-import InputSel from 'components/global/gear/inputAndSel/inputSel';
+// import InputSel from 'components/global/gear/inputAndSel/inputSel';
+import InputSel from 'components/global/gear/inputAndSel_v2/inputSel';
 import CellWithBar from 'components/global/gear/cell/cellWithBar';
 
 // icon
@@ -106,38 +107,49 @@ export default function EditTransfer({ disabled, controll }: { disabled: boolean
                     <div className={className} key={index} style={theStyle}>
                       {type === 'textarea' && (
                         <InputSel
-                          textareaProps={{
-                            value,
-                            onChange: onChangeInput,
-                            className: style.input,
-                          }}
-                          placeholder=""
                           disabled={disabled}
+                          textareaProps={{
+                            props: {
+                              placeholder: '',
+                              value,
+                              onChange: (e) => {
+                                onChangeInput(e.target.value);
+                              },
+                              className: style.input,
+                            },
+                          }}
                         />
                       )}
                       {type === 'input' && (
                         <InputSel
-                          inputProps={{
-                            value,
-                            onChange: onChangeInput,
-                            className: style.input,
-                            inputType: inputType,
-                          }}
-                          placeholder=""
                           disabled={disabled}
+                          inputProps={{
+                            props: {
+                              type: inputType,
+                              placeholder: '',
+                              value,
+                              onChange: (e) => {
+                                onChangeInput(e.target.value);
+                              },
+                              className: style.input,
+                            },
+                          }}
                         />
                       )}
                       {type === 'select' && (
                         <InputSel
+                          disabled={disabled}
+                          fontSize="16"
                           selectProps={{
-                            value,
-                            onChange: onChangeSel,
-                            options: options ?? [],
-                            fontSize: '16px',
+                            props: {
+                              isSearchable: true,
+                              placeholder: '',
+                              value: !value ? null : { value: value, label: value },
+                              onChange: onChangeSel,
+                              options: options ?? [],
+                            },
                             arrowType: 'black',
                           }}
-                          placeholder=""
-                          disabled={disabled}
                         />
                       )}
                     </div>
