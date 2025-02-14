7bddd343d8548653aa8f60084f93571829e4fa61
Your Name <you@example.com>
2025-02-14 16:52:55 +0800

    feat:外包計價-移除註解，刪除不再使用的元件

--- pages/worksDepartment/outsourcingPricing/edit.tsx
+++ pages/worksDepartment/outsourcingPricing/edit.tsx
@@ -2,7 +2,6 @@ import { useState, useMemo, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import Decimal from 'decimal.js';
-import _ from 'lodash';
 import moment from 'moment';
 import Link from 'next/link';
 
@@ -22,7 +21,7 @@ import { Upload } from 'antd';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
 import SquareBtn from 'components/global/gear/button/larrysBtn/squarebtn';
 import ReviewFlowSelector from 'components/composition/review/reviewFlowSelector';
-import { Ttable, Tcell } from 'components/global/gear/table/table01';
+
 import Row, { Cell } from 'components/global/gear/table/row';
 
 // icon
@@ -34,9 +33,6 @@ import scss from './edit.module.scss';
 // api
 import {
   Tparams,
-  ToutsourcingPaymentDetailDto,
-  ToutsourcingPaymentDto,
-  TupdateOutsourcingPaymentDto,
   useGetOutsourcingPayment_id_kit,
   useGetOutsourcingPaymentDetail,
   apiClearDebt,
@@ -46,7 +42,6 @@ import {
 import { TuserDto, TdeductionDto } from 'js/api/dtoTypes';
 
 import PaymentSelectSlideBar from 'components/page/worksDepartment/outsourcingPricing/edti/paymentSelectSlideBar';
-import Table from 'components/page/worksDepartment/outsourcingPricing/edti/table';
 
 import { useAttachment } from 'hooks/attachment/useAttachment';
 import { useReviewFlow } from 'components/composition/review/reviewFlow';
@@ -211,20 +206,6 @@ export default function OutsourcingPricingEdit({
     });
   };
 
-  const editDeduction = ({ index, key, value }: { index: number; key: keyof TdeductionDto; value: string }) => {
-    setState_deductionArr((prev) => {
-      const newArr = [...prev];
-
-      if (key === 'price') {
-        newArr[index][key] = Number(value);
-      } else {
-        newArr[index][key] = value;
-      }
-
-      return newArr;
-    });
-  };
-
   const createSetDeduction = (index: number) => {
     const func: React.Dispatch<React.SetStateAction<TdeductionDto>> = (setStateAction) => {
       setState_deductionArr((prev) => {
@@ -276,26 +257,6 @@ export default function OutsourcingPricingEdit({
       setIsLoading(false);
     }
   };
-
-  // -------------------------------------------------------------------------
-
-  // const { control_table_project, subTotal_project } = useProject({ paymentDetail });
-
-  // const {
-  //   control_table_deduction,
-  //   // subTotal_deduction
-  // } = useDeduction({
-  //   disabled,
-  //   state_deduction: state_deductionArr,
-  //   editDeduction: editDeduction,
-  //   deleteAnmountToBeDeducted: deleteDeduction,
-  // });
-
-  // const { control_table_actualAmountReceived, result } = useTable({
-  //   subTotal_project: subTotal_detail,
-  //   data_payment,
-  //   subTotal_deduction,
-  // });
 
   // -------------------------------------------------------------------------
 
@@ -458,7 +419,6 @@ export default function OutsourcingPricingEdit({
   // MARK: useEffect
 
   useEffect(() => {
-    // setPayment(paymentOri ?? create_emptyPayment());
     setState_deductionArr(paymentOri?.deduction ?? []);
   }, [paymentOri, disabled]);
 
@@ -510,16 +470,6 @@ export default function OutsourcingPricingEdit({
             }}
           />
         )}
-
-        {/* <div>
-          <Row thead={true}>
-            <Cell>序號</Cell>
-            <Cell>工程編號</Cell>
-            <Cell>工程名稱</Cell>
-            <Cell>請款</Cell>
-            <Cell></Cell>
-          </Row>
-        </div> */}
 
         <div className="w-[1100px] m-auto mt-[96px]">
           <div className="mb-2">
@@ -583,31 +533,6 @@ export default function OutsourcingPricingEdit({
           </div>
         </div>
 
-        {/* <Table
-          caption={
-            <>
-              工程列表{' '}
-              <Link
-                href={{
-                  pathname: './detail',
-                  query: {
-                    outsourcingId,
-                    paymentId,
-                    isNew: 'true',
-                  },
-                }}
-              >
-                <SquareBtn sharp="mini" className="ml-3">
-                  新建明細
-                </SquareBtn>
-              </Link>
-            </>
-          }
-          disabled={disabled}
-          className="w-fit m-auto mt-[96px]"
-          control={control_table_project}
-        /> */}
-
         <div className="w-[1100px] m-auto mt-[96px]">
           <div className="mb-2 flex gap-2 items-center">
             <span className="text-main text-lg">應扣明細</span>
@@ -658,14 +583,6 @@ export default function OutsourcingPricingEdit({
             </Row>
           </div>
         </div>
-
-        {/* <Table
-          caption="應扣明細"
-          disabled={disabled}
-          className="w-fit m-auto mt-[96px]"
-          control={control_table_deduction}
-          onAddClick={addDeduction}
-        /> */}
 
         <div className="w-[500px] m-auto mt-[96px] ml-[277.5px]">
           <div className="mb-2 ">
@@ -722,7 +639,6 @@ export default function OutsourcingPricingEdit({
           </div>
         </div>
 
-        {/* <Table caption="實領金額" className="w-fit m-auto mt-[96px]" control={control_table_actualAmountReceived} /> */}
         {/*  */}
 
         <div className="w-fit m-auto ml-[275px] mt-[96px]">
@@ -964,568 +880,11 @@ const config_settlement: Tconfig_settlement = {
   },
 };
 
-// =============================================================================
-
-type Tconfig_legency<keys extends string = string> = {
-  [key in keys]: {
-    width?: React.CSSProperties['width'];
-    flex?: React.CSSProperties['flex'];
-    justifyContent?: React.CSSProperties['justifyContent'];
-    className?: string;
-    style?: React.CSSProperties;
-    tbody?: {
-      width?: React.CSSProperties['width'];
-      flex?: React.CSSProperties['flex'];
-      justifyContent?: React.CSSProperties['justifyContent'];
-      className?: string;
-      style?: React.CSSProperties;
-    };
-    inputWidth?: React.CSSProperties['width'];
-  };
-};
-
-const config_public: Tconfig_legency = {
-  left: {
-    tbody: {
-      flex: 'auto',
-      justifyContent: 'flex-end',
-    },
-  },
-  right: {
-    tbody: {
-      width: '270px',
-      justifyContent: 'flex-end',
-    },
-  },
-};
-
-const config_projectTable: Tconfig_legency = {
-  indexNumber: {
-    width: '60px',
-    justifyContent: 'center',
-  },
-  projectNumber: {
-    flex: '1 0',
-    justifyContent: 'center',
-  },
-  projectName: {
-    flex: '1 0',
-    justifyContent: 'center',
-  },
-  subTotal_invoice: {
-    width: '270px',
-    justifyContent: 'center',
-    tbody: {
-      width: '200px',
-      justifyContent: 'flex-end',
-    },
-  },
-  btn_info: {
-    tbody: {
-      width: '70px',
-      justifyContent: 'center',
-    },
-  },
-  label_subTotal: config_public.left,
-  subtotal: config_public.right,
-};
-
-const thead_projectTable: Ttable['thead'] = {
-  cellArr: [
-    {
-      children: '序號',
-      ...config_projectTable.indexNumber,
-    },
-    {
-      children: '工程編號',
-      ...config_projectTable.projectNumber,
-    },
-    {
-      children: '工程名稱',
-      ...config_projectTable.projectName,
-    },
-    {
-      children: '請款',
-      ...config_projectTable.subTotal_invoice,
-    },
-  ],
-};
-// ---------------------
-
-const config_deduction_legacy: Tconfig_legency<
-  'type' | 'itemName' | 'price' | 'price_enabled' | 'btn_delete' | 'label_subTotal' | 'subtotal'
-> = {
-  type: {
-    width: 414,
-    // flex: '1 0',
-    justifyContent: 'center',
-    inputWidth: 394,
-  },
-  itemName: {
-    width: 414,
-    // flex: '1 0',
-    justifyContent: 'center',
-    inputWidth: 394,
-  },
-  price: {
-    width: '270px',
-    justifyContent: 'center',
-    tbody: {
-      width: '270px',
-      justifyContent: 'flex-end',
-    },
-  },
-  price_enabled: {
-    inputWidth: 180,
-    tbody: {
-      width: 200,
-      justifyContent: 'flex-end',
-    },
-  },
-  btn_delete: {
-    tbody: {
-      width: '70px',
-      justifyContent: 'center',
-    },
-  },
-  label_subTotal: config_public.left,
-  subtotal: config_public.right,
-};
-
-const thead_amountToBeDeducted: Ttable['thead'] = {
-  cellArr: [
-    {
-      children: '類別',
-      ...config_deduction_legacy.type,
-    },
-    {
-      children: '項目',
-      ...config_deduction_legacy.itemName,
-    },
-    {
-      children: '應扣額',
-      ...config_deduction_legacy.price,
-    },
-  ],
-};
-
-// ======================================================================
-
-const config_actualAmountReceived: Tconfig_legency = {
-  caption: {
-    flex: '1',
-    justifyContent: 'center',
-    tbody: {
-      flex: '1',
-      justifyContent: 'flex-end',
-    },
-  },
-  subTotal_invoice: {
-    width: '270px',
-    justifyContent: 'center',
-    tbody: {
-      width: '270px',
-      justifyContent: 'flex-end',
-    },
-  },
-};
-
-const thead_actualAmountReceived: Ttable['thead'] = {
-  cellArr: [
-    {
-      children: '金額名稱',
-      ...config_actualAmountReceived.caption,
-    },
-    {
-      children: '金額',
-      ...config_actualAmountReceived.subTotal_invoice,
-    },
-  ],
-};
-
-// ======================================================================
-
-// MARK: useProject
-const useProject = ({ paymentDetail }: { paymentDetail: ToutsourcingPaymentDetailDto[] | undefined }) => {
-  const router = useRouter();
-
-  return useMemo(() => {
-    let decimal_subTotal = new Decimal(0);
-    const control_tbody: Ttable['tbody'] = (() => {
-      const rowArr: Ttable['tbody']['rowArr'] = (paymentDetail ?? []).map((data, index) => {
-        const { outsourcingTotal, engineeringContact } = data;
-
-        const { projectName = '', projectNumber = '' } = engineeringContact ?? {};
-
-        decimal_subTotal = decimal_subTotal.add(outsourcingTotal);
-
-        const href = {
-          pathname: './detail',
-          query: {
-            paymentDetailId: data.id,
-          },
-        };
-
-        const cellArr: Tcell[] = [
-          {
-            children: index + 1,
-            ...config_projectTable.indexNumber,
-          },
-          {
-            children: projectNumber,
-            ...config_projectTable.projectNumber,
-          },
-          {
-            children: projectName,
-            ...config_projectTable.projectName,
-          },
-          {
-            children: outsourcingTotal.toLocaleString(),
-            ...config_projectTable.subTotal_invoice.tbody,
-          },
-          {
-            children: <IconDetail onClick={() => router.push(href)} />,
-            ...config_projectTable.btn_info.tbody,
-          },
-        ];
-
-        return {
-          cellArr,
-        };
-      });
-
-      rowArr.push({
-        cellArr: [
-          {
-            children: '小計',
-            ...config_projectTable.label_subTotal.tbody,
-          },
-          {
-            children: decimal_subTotal.toNumber().toLocaleString(),
-            ...config_projectTable.subtotal.tbody,
-          },
-        ],
-      });
-
-      return {
-        rowArr,
-      };
-    })();
-
-    const control_table: Ttable = {
-      thead: thead_projectTable,
-      tbody: control_tbody,
-      haveBorder: true,
-    };
-
-    return {
-      //
-      control_table_project: control_table,
-      subTotal_project: decimal_subTotal.toNumber(),
-    };
-  }, [paymentDetail]);
-};
-
-// MARK:useDeduction
-const useDeduction = ({
-  disabled,
-  state_deduction,
-  editDeduction,
-  deleteAnmountToBeDeducted,
-}: {
-  disabled: boolean;
-  state_deduction: TdeductionDto[];
-  editDeduction: ({ index, key, value }: { index: number; key: keyof TdeductionDto; value: string }) => void;
-  deleteAnmountToBeDeducted: (index: number) => void;
-}) => {
-  return useMemo(() => {
-    //
-    let decimal_subTotal = new Decimal(0);
-    //
-    const control_tbody: Ttable['tbody'] = (() => {
-      //
-      const rowArr: Ttable['tbody']['rowArr'] = state_deduction.map((data, index) => {
-        const { type, itemName, price } = data;
-
-        decimal_subTotal = decimal_subTotal.add(price);
-
-        const inputWidth_type = config_deduction_legacy.type.inputWidth;
-        const inputWidth_item = config_deduction_legacy.itemName.inputWidth;
-        const inputWidth_price = config_deduction_legacy.price_enabled.inputWidth;
-
-        const typeChildren = (
-          <input
-            value={type}
-            onChange={(e) => {
-              editDeduction({ index, key: 'type', value: e.target.value });
-            }}
-            className={classNames(scss.inputInTable, !disabled && scss.enabled)}
-            style={{ width: inputWidth_type }}
-            readOnly={disabled}
-          />
-        );
-
-        const itemChildren = (
-          <input
-            value={itemName}
-            onChange={(e) => {
-              editDeduction({ index, key: 'itemName', value: e.target.value });
-            }}
-            className={classNames(scss.inputInTable, !disabled && scss.enabled)}
-            style={{ width: inputWidth_item }}
-            readOnly={disabled}
-          />
-        );
-
-        const subTotal_invoiceChildren = (
-          <input
-            value={disabled ? price.toLocaleString() : price}
-            onChange={(e) => {
-              editDeduction({ index, key: 'price', value: e.target.value });
-            }}
-            type={disabled ? 'text' : 'number'}
-            className={classNames(scss.inputInTable, !disabled && scss.enabled)}
-            style={{ width: inputWidth_price }}
-            readOnly={disabled}
-          />
-        );
-
-        const cellArr: Tcell[] = [
-          {
-            children: typeChildren,
-            ...config_deduction_legacy.type,
-          },
-          {
-            children: itemChildren,
-            ...config_deduction_legacy.itemName,
-          },
-          {
-            children: subTotal_invoiceChildren,
-            ...(disabled ? config_deduction_legacy.price.tbody : config_deduction_legacy.price_enabled.tbody),
-          },
-        ];
-
-        if (!disabled) {
-          cellArr.push({
-            children: (
-              <IconDelete01
-                onClick={() => {
-                  myAlert.confirm({
-                    title: '確定要刪除嗎?',
-                    props: {
-                      onOk: () => {
-                        deleteAnmountToBeDeducted(index);
-                      },
-                    },
-                  });
-                }}
-              />
-            ),
-            ...config_deduction_legacy.btn_delete.tbody,
-          });
-        }
-
-        return {
-          cellArr,
-        };
-      });
-
-      rowArr.push({
-        cellArr: [
-          {
-            children: '小計',
-            ...config_projectTable.label_subTotal.tbody,
-          },
-          {
-            children: decimal_subTotal.toNumber().toLocaleString(),
-            ...config_projectTable.subtotal.tbody,
-          },
-        ],
-      });
-
-      return {
-        rowArr,
-      };
-    })();
-
-    const control_table: Ttable = {
-      thead: thead_amountToBeDeducted,
-      tbody: control_tbody,
-      haveBorder: true,
-    };
-
-    return {
-      control_table_deduction: control_table,
-      subTotal_deduction: decimal_subTotal.toNumber(),
-    };
-  }, [state_deduction, disabled]);
-};
-
-// MARK: useTable
-const useTable = ({
-  subTotal_project,
-  data_payment,
-  subTotal_deduction,
-}: {
-  subTotal_project: number;
-  data_payment: ToutsourcingPaymentDto | undefined | null;
-  subTotal_deduction: number;
-}) => {
-  return useMemo(() => {
-    //
-
-    // 本期保留10% // 本期保留款
-    const retainage = new Decimal(subTotal_project).mul(0.1).toNumber();
-    // '上期保留10%' // 上期保留款
-    const latestPeriodKeep = data_payment?.priorPeriodRetainage ?? 0;
-
-    const subTotal = new Decimal(subTotal_project)
-      .sub(retainage)
-      .add(latestPeriodKeep)
-      .sub(subTotal_deduction)
-      .toNumber();
-
-    const tax = new Decimal(subTotal).mul(0.05).toDecimalPlaces(0).toNumber();
-    const actualAmountReceived = new Decimal(subTotal).add(tax).toNumber();
-
-    const result = {
-      retainage: retainage, // 本期保留款項
-      subTotal: subTotal,
-      salesTax: tax,
-      total: actualAmountReceived, // 實領總計
-    };
-
-    //
-    const rowArr: Ttable['tbody']['rowArr'] = [
-      //
-      {
-        cellArr: [
-          {
-            children: '請款合計',
-            ...config_actualAmountReceived.caption,
-          },
-          {
-            children: subTotal_project.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-          },
-        ],
-      },
-      //
-      {
-        cellArr: [
-          {
-            children: '本期保留10%',
-            ...config_actualAmountReceived.caption,
-          },
-          {
-            children: retainage.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-            className: scss.textRed,
-          },
-        ],
-      },
-      //
-      {
-        cellArr: [
-          {
-            children: '上期保留10%',
-            ...config_actualAmountReceived.caption,
-          },
-          {
-            children: latestPeriodKeep.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-            className: scss.textGreen,
-          },
-        ],
-      },
-      //
-      {
-        cellArr: [
-          {
-            children: '應扣明細',
-            ...config_actualAmountReceived.caption,
-          },
-          {
-            children: subTotal_deduction.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-            className: scss.textRed,
-          },
-        ],
-      },
-      //
-      {
-        cellArr: [
-          {
-            children: '小計',
-            ...config_actualAmountReceived.caption.tbody,
-          },
-          {
-            children: subTotal.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-          },
-        ],
-      },
-      {
-        cellArr: [
-          {
-            children: '稅額5%',
-            ...config_actualAmountReceived.caption.tbody,
-          },
-          {
-            children: tax.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-          },
-        ],
-      },
-      {
-        cellArr: [
-          {
-            children: '實領金額',
-            ...config_actualAmountReceived.caption.tbody,
-          },
-          {
-            children: actualAmountReceived.toLocaleString(),
-            ...config_actualAmountReceived.subTotal_invoice.tbody,
-            className: scss.textBold,
-          },
-        ],
-      },
-    ]; // rowArr
-
-    const tbody = {
-      rowArr,
-    };
-
-    const control_table: Ttable = {
-      thead: thead_actualAmountReceived,
-      tbody,
-      haveBorder: true,
-    };
-
-    return {
-      control_table_actualAmountReceived: control_table,
-      result,
-    };
-    //
-  }, [subTotal_project, subTotal_deduction, data_payment?.priorPeriodRetainage]);
-};
-
 // ======================================================================
 // ======================================================================
 // ======================================================================
 // ======================================================================
 // ======================================================================
-
-const create_emptyPayment = (): TupdateOutsourcingPaymentDto => ({
-  date: '',
-  paymentSubTotal: 0,
-  deduction: [],
-  deductionTotal: 0,
-  retainage: 0,
-  subTotal: 0,
-  salesTax: 0,
-  total: 0,
-});
 
 const create_emptyDeduction = (): TdeductionDto => ({
   type: '',
