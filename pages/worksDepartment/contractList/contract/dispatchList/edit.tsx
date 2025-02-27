b99f71b658d9fc6ede655289b2be6e0c5885a889
Your Name <you@example.com>
2025-02-27 13:35:41 +0800

    fix:派工單-PDF 修正接洽人欄位串接的資料來源

--- pages/worksDepartment/contractList/contract/dispatchList/edit.tsx
+++ pages/worksDepartment/contractList/contract/dispatchList/edit.tsx
@@ -692,8 +692,9 @@ export default function EditDispatchList() {
       address = '',
       warrantyDate,
       tasks = '',
-      pointContactPerson = '',
-      pointContactNumber = '',
+      // pointContactPerson = '',
+      // pointContactNumber = '',
+      pointContact,
 
       projectSiteContactPerson = '',
       projectSiteContactPersonNumber = '',
@@ -701,11 +702,14 @@ export default function EditDispatchList() {
 
     const wholeAddress = `${county}${district}${address}`;
 
+    const contactPerson = pointContact?.[0]?.name + '\n' + pointContact?.[0]?.phone;
+
     const data_pdf: Tdata_pdf = {
       idNumber: dispatching?.idNumber ?? '',
       customerName: contract?.content.projectName ?? '',
       phoneNumber: (projectSiteContactPerson || '') + '\n' + (projectSiteContactPersonNumber || ''),
-      contactPerson: (pointContactPerson || '') + '\n' + (pointContactNumber || ''),
+      // contactPerson: (pointContactPerson || '') + '\n' + (pointContactNumber || ''),
+      contactPerson,
       address: wholeAddress,
       projectNumber: contract?.contractNumber ?? '',
       warrantyPeriod: (warrantyDate ? getTaiwanDateStr(warrantyDate) : '') || '',
