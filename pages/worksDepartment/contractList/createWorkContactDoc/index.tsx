c6e76d8c8636376b075499774c2be6a8bd3e2e21
Your Name <you@example.com>
2025-03-11 10:17:53 +0800

    feat:建立「建立工程聯絡單」頁

---
+++ pages/worksDepartment/contractList/createWorkContactDoc/index.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect, useMemo } from 'react';
+
+// layer
+import SubLayer from 'components/Layer/SubLayer/SubLayer';
+import PageHeader02, { TpanelList, TsearchGroup } from 'components/PageHeader/PageHeader02/PageHeader02';
+
+export default function CreateWorkContactDoc() {
+  return (
+    <SubLayer>
+      <PageHeader02 />
+
+      <div>fooo</div>
+    </SubLayer>
+  );
+}
