376c40ce5908aadc694cbeb2daca8f898151ab1f
Your Name <you@example.com>
2025-03-12 15:36:47 +0800

    feat:送電備品領取單列表-修正門型的格式，調整樣式

--- components/page/worksDepartment/electronicSupplies/pickupRecord.tsx
+++ components/page/worksDepartment/electronicSupplies/pickupRecord.tsx
@@ -12,7 +12,7 @@ import { IconDetail } from 'public/image/icon/svgComponent/svgIcons';
 // utils
 import { getTaiwanDateStr } from 'js/utils/helpers/date/convertDate';
 
-import type { TelectronicSuppliesPickupRecordDto } from 'js/api/dtoTypes';
+import type { TelectronicSuppliesPickupRecordDto_addition } from 'js/api/api_engineering';
 
 // ==================================================================
 
@@ -26,7 +26,7 @@ export default function PickupRecord({
   pickupRecords,
 }: {
   className?: string;
-  pickupRecords: TelectronicSuppliesPickupRecordDto[];
+  pickupRecords: TelectronicSuppliesPickupRecordDto_addition[];
 }) {
   const router = useRouter();
   const query = router.query as Tquery;
@@ -59,6 +59,7 @@ export default function PickupRecord({
         doorModel,
         preparationEmployee,
         number: idNumber,
+        addition: { doorTypeArr },
       } = item;
 
       return {
@@ -77,7 +78,8 @@ export default function PickupRecord({
           },
           {
             ...configList.doorModelName,
-            children: doorModel,
+            // children: doorModel,
+            children: doorTypeArr?.join('、'),
           },
           {
             ...configList.qty,
@@ -139,23 +141,28 @@ const configList: { [key: string]: Tconfig_table } = {
   },
   date: {
     label: '領料日期',
-    flex: '20%',
+    // flex: '20%',
+    width: 150,
   },
   ingredientTechnician: {
     label: '領料人員',
-    flex: '20%',
+    // flex: '20%',
+    width: 150,
   },
   doorModelName: {
     label: '門型',
-    flex: '20%',
+    flex: 'auto',
+    justifyContent: 'flex-start',
   },
   qty: {
     label: '樘數',
-    flex: '20%',
+    // flex: '20%',
+    width: 80,
   },
   materialHandler: {
     label: '備料人員',
-    flex: '20%',
+    // flex: '20%',
+    width: 150,
   },
   btn: {
     width: 100,
