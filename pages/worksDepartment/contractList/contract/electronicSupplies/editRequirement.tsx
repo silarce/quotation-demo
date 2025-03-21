e08c3e233c68553384be6796c8b45f1db0bd572d
Your Name <you@example.com>
2025-03-21 17:18:07 +0800

    feat:送電備品需求單-將DefaultItemSelector抽出為獨立檔案

--- pages/worksDepartment/contractList/contract/electronicSupplies/editRequirement.tsx
+++ pages/worksDepartment/contractList/contract/electronicSupplies/editRequirement.tsx
@@ -10,6 +10,7 @@ import PageHeader, { TpanelList } from 'components/page/worksDepartment/contracL
 
 // component
 import SupplyTable, { useStateToGroup } from 'components/page/worksDepartment/electronicSupplies/ui/supplyTable';
+import DefaultItemSelector from 'components/page/worksDepartment/electronicSupplies/defaultItemSelector';
 
 // antd
 import { Select as AntdSelect } from 'antd';
@@ -19,7 +20,6 @@ import InputSel, { TinputSelProps } from 'components/global/gear/inputAndSel_v2/
 import { selectModalCreator_multi } from 'components/global/gear/modal/selectorModalCreator_multi/selectorModalCreator_multi';
 import myAlert from 'components/global/gear/modal/simpleModal/alertModals';
 import SquareBtn from 'components/global/gear/button/larrysBtn/squarebtn';
-import DataEntry from 'components/global/gear/dataEntry';
 
 // css
 import scss from './editRequirement.module.scss';
@@ -60,29 +60,6 @@ type Tquery = {
   contractId: string | undefined;
   requirementRecordId: string | undefined;
 };
-
-type Taction =
-  | {
-      type: 'checked';
-      payload: {
-        index: number;
-        checked: boolean;
-      };
-    }
-  | {
-      type: 'category';
-      payload: {
-        index: number;
-        category: string;
-      };
-    }
-  | {
-      type: 'qty';
-      payload: {
-        index: number;
-        qty: number | `${number}` | '';
-      };
-    };
 
 // ==================================================================
 
@@ -544,114 +521,5 @@ export default function EditRequirementRecord() {
 }
 
 // MARK: END
-
-// ==================================================================
-
-const reducer = (state: (Tstate_electronicItem & { checked: boolean })[], action: Taction) => {
-  const copy = [...state];
-  const { type, payload } = action;
-  const index = payload.index;
-
-  switch (type) {
-    case 'checked':
-      copy[index].checked = payload.checked;
-
-      return copy;
-    case 'category':
-      copy[index].category = payload.category;
-
-      return copy;
-
-    case 'qty':
-      copy[index].quantity = payload.qty === '' ? null : Number(payload.qty);
-
-      return copy;
-
-    default:
-      return state;
-  }
-};
-
-// MARK:DefaultItemSelector
-const DefaultItemSelector = ({
-  state_electronicItemArr,
-  onConfirm: _onConfirm,
-}: {
-  state_electronicItemArr: Tstate_electronicItem[];
-  onConfirm: (arr: Tstate_electronicItem[]) => void;
-}) => {
-  const arr = state_electronicItemArr.map((item) => ({ ...item, checked: false }));
-
-  const [stateArr, dispatch] = useReducer(reducer, arr);
-
-  const onConfirm = () => {
-    const arr = stateArr
-      .filter((state) => state.checked)
-      .map((_item) => {
-        const { checked, ...item } = _item;
-
-        return {
-          ...item,
-          category: item.category.trim(),
-        };
-      });
-
-    _onConfirm(arr);
-  };
-
-  return (
-    <div className="p-5">
-      <div className="text-main text-bold text-xl">建議送電備品</div>
-      <br />
-      <div className={scss.row}>
-        <br />
-        <span className="text-bold text-lg">品名</span>
-        <span className="text-bold text-lg">種類</span>
-        <span className="text-bold text-lg">數量</span>
-        {/*  */}
-        {stateArr.map((state, index) => {
-          return (
-            <Fragment key={index}>
-              <DataEntry.Checkbox
-                value={state.checked}
-                onChange={(e) =>
-                  dispatch({
-                    type: 'checked',
-                    payload: {
-                      index,
-                      checked: e.target.checked,
-                    },
-                  })
-                }
-              />
-              <span>{state.itemName}</span>
-              <DataEntry.Input
-                value={state.category}
-                onChange={(e) => dispatch({ type: 'category', payload: { index, category: e.target.value } })}
-              />
-              <DataEntry.Input
-                type="number"
-                min={0}
-                step={0}
-                value={state.quantity ?? ''}
-                onChange={(e) => {
-                  if (e.target.validity.valid) {
-                    dispatch({ type: 'qty', payload: { index, qty: e.target.value as `${number}` } });
-                  }
-                }}
-              />
-            </Fragment>
-          );
-        })}
-
-        {/*  */}
-      </div>
-      <br />
-      <SquareBtn sharp="long" onClick={onConfirm}>
-        確認
-      </SquareBtn>
-    </div>
-  );
-};
 
 // ==================================================================
