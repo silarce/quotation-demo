74d810183d4c5dd4e4d153acdff6ac5b6f967b59
Your Name <you@example.com>
2025-03-27 13:41:06 +0800

    refactor:外包計價-將MonthVendorPanel抽出為獨立檔案

--- pages/worksDepartment/outsourcingPricing/index.tsx
+++ pages/worksDepartment/outsourcingPricing/index.tsx
@@ -26,6 +26,7 @@ import TabCarousel02, {
 import VendorMonthPanel from 'components/page/worksDepartment/outsourcingPricing/vendorMonthPanel';
 import OutsourcingList from 'components/page/worksDepartment/outsourcingPricing/outsourcingList';
 import DateList from 'components/page/worksDepartment/outsourcingPricing/dateList';
+import MonthVendorPanel from 'components/page/worksDepartment/outsourcingPricing/monthVendorPanel';
 
 // css
 import scss from './index.module.scss';
@@ -60,14 +61,19 @@ export default function OutsourcingPricing() {
   // ------------------------------------------------------------------------
 
   const isShowVendorList = showListBy === 'vendor' && !targetOutsourcingId;
-  const isShowDateList = showListBy === 'month' && !targetIsoDate;
   const isShowVendorMonthList = showListBy === 'vendor' && targetOutsourcingId;
+
+  const isShowDateList = showListBy === 'month' && !targetIsoDate;
   const isShowMonthVendorList = showListBy === 'month' && targetIsoDate;
+
+  const showComponent = {
+    vendor: targetOutsourcingId ? 'showVendorMonthList' : 'showVendorList',
+    month: targetOutsourcingId ? 'showMonthVendorList' : 'showDateList',
+  }[showListBy];
 
   // ------------------------------------------------------------------------
 
   const params: Tparams = {
-    // filter,
     sort: 'createdAt',
     order: 'DESC',
   };
@@ -76,11 +82,6 @@ export default function OutsourcingPricing() {
 
   // ------------------------------------------------------------------------
 
-  useEffect(() => {
-    reset();
-  }, []);
-
-  // ------------------------------------------------------------------------
   const tagList: TtagList = [
     {
       label: '外包廠商',
@@ -99,6 +100,15 @@ export default function OutsourcingPricing() {
       },
     },
   ];
+
+  // ------------------------------------------------------------------------
+  // MARK: useEffect
+
+  useEffect(() => {
+    reset();
+  }, []);
+
+  // ------------------------------------------------------------------------
 
   // MARK: RENDER
 
@@ -156,185 +166,6 @@ export default function OutsourcingPricing() {
 // MARK: END
 
 // ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-
-// ====================================================================
-
-const MonthVendorPanel = ({
-  //
-  targetDate,
-  className,
-  onDateTabClick,
-}: {
-  targetDate: string | undefined;
-  className?: string;
-  onDateTabClick: (isoString: string) => void;
-}) => {
-  const router = useRouter();
-
-  // ----------------------------------------------------------------------
-  const [activeTab_date, setActiveTab_date] = useState<number>(0);
-
-  const ref_carousel = useRef<TimperativeHandle>(null!);
-
-  // ----------------------------------------------------------------------
-
-  const monthStart = moment(targetDate).startOf('month').toISOString();
-  const monthEnd = moment(targetDate).endOf('month').toISOString();
-
-  const filter = {
-    date: {
-      $gte: monthStart,
-      $lte: monthEnd,
-    },
-  };
-
-  const params: Tparams = {
-    pageSize: 99999,
-    filter,
-  };
-
-  const { dataArr: paymentArr, reset: reset_payment } = useGetOutsourcingPayment({ customParams: params });
-
-  // ----------------------------------------------------------------------
-
-  useEffect(() => {
-    reset_payment();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [targetDate]);
-
-  // ----------------------------------------------------------------------
-
-  const { tabArr, defaultCarouselIndex } = useMemo(() => {
-    //
-    const dateArr = getAllMonthByRange({
-      start: 2022,
-      end: undefined,
-    }).reverse();
-
-    let defaultCarouselIndex = -1;
-
-    const tabArr: Tcontrol_tabCarousel['tabArr'] = dateArr.map((date, index) => {
-      const twDate = moment(date).subtract(1911, 'years');
-
-      if (date === moment(targetDate).format('yy-MM')) {
-        defaultCarouselIndex = index;
-        setActiveTab_date(index);
-      }
-
-      return {
-        label: twDate.format('yy-MM'),
-        onClick: ({ ref_slider }) => {
-          setActiveTab_date(index);
-          ref_slider.current.slickGoTo(index);
-          const theDate = moment(date);
-          onDateTabClick(theDate.toISOString());
-        },
-      };
-    });
-
-    return { tabArr, defaultCarouselIndex };
-  }, []);
-
-  const control_tabCarousel: Tcontrol_tabCarousel = {
-    activeIndex: activeTab_date,
-    tabArr,
-  };
-
-  // ----------------------------------------------------------------------
-
-  const thead: Ttable['thead'] = {
-    stickyTop: {
-      top: '40px',
-    },
-    rowProps: {
-      minHeight: tableConfig.row.minHeight,
-    },
-    cellArr: [
-      {
-        children: cellCofig.vendor.label,
-        width: cellCofig.vendor.width,
-      },
-      {
-        children: cellCofig.phoneNumber.label,
-        width: cellCofig.phoneNumber.width,
-      },
-    ],
-  };
-
-  const control_rowArr: Ttable['tbody']['rowArr'] = useMemo(() => {
-    return paymentArr.map((paymentInfo) => {
-      const { outsourcing } = paymentInfo;
-
-      return {
-        minHeight: tableConfig.row.minHeight,
-        onClick: () => {
-          router.push({
-            pathname: router.pathname + '/edit',
-            query: {
-              paymentId: paymentInfo?.id,
-            },
-          });
-        },
-        cellArr: [
-          {
-            children: outsourcing.name,
-            width: cellCofig.vendor.width,
-          },
-          {
-            children: outsourcing.contactNumber,
-            width: cellCofig.phoneNumber.width,
-          },
-        ],
-      };
-    });
-  }, [paymentArr]);
-
-  const control_table: Ttable = {
-    thead: thead,
-    tbody: {
-      rowArr: control_rowArr,
-    },
-    haveBorder: false,
-  };
-
-  // ----------------------------------------------------------------------
-
-  useEffect(() => {
-    ref_carousel.current.slideToIndex(defaultCarouselIndex);
-  }, []);
-
-  // ----------------------------------------------------------------------
-  return (
-    <div className={classNames(className)}>
-      <TabCarousel02 ref={ref_carousel} control={control_tabCarousel} />
-      <Wrapper_tab
-        className={classNames('m-auto')}
-        childrenOption={{
-          noBorderTop: true,
-        }}
-        stickyTop={{
-          top: 40,
-        }}
-      >
-        <Table01 {...control_table} />
-      </Wrapper_tab>
-    </div>
-  );
-};
-
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-
-// ====================================================================
 
 const tableConfig = {
   row: {
@@ -355,23 +186,6 @@ const cellCofig: TcellConfig = {
 };
 
 // ====================================================================
-
-function generateMonthsSinceNow(): { [key: `${number}`]: number[] } {
-  const currentYear = new Date().getFullYear();
-  const result: { [key: string]: number[] } = {};
-
-  for (let year = 2020; year <= currentYear; year++) {
-    result[year.toString()] = Array.from({ length: 12 }, (_, i) => i + 1);
-  }
-
-  return result;
-}
-
-//
-//
-//
-//
-export { generateMonthsSinceNow };
 
 export type { TcellConfig };
 
