053b86e3fb3b82d22bb291660bcc4b7d9204711e
Your Name <you@example.com>
2025-04-22 14:34:52 +0800

    task:研發部報價單-初步製作summary

--- pages/researchDepartment/quotation/edit/index.tsx
+++ pages/researchDepartment/quotation/edit/index.tsx
@@ -3,7 +3,11 @@ import useRouter from 'next/router';
 import SubLayer from 'components/Layer/SubLayer/SubLayer';
 import PageHeader02, { TpanelList } from 'components/PageHeader/PageHeader02/PageHeader02';
 
+// component
 import Profile from 'components/page/researchDepartment/quotation/profile';
+import QuotationRemark, { Tprops_quotationRemark } from 'components/page/domestic/quotation_v2/QuotationRemark';
+
+import scss from './index.module.scss';
 
 export default function Edit() {
   return (
@@ -11,7 +15,32 @@ export default function Edit() {
       <PageHeader02 tag="新增報價單" />
       <div>
         <Profile />
+
+        <div className={scss.summary}>
+          <div className={scss.left}>
+            <QuotationRemark {...propsForTest_quotationRemark} label="備註" />
+            <QuotationRemark {...propsForTest_quotationRemark} label="報價範圍" />
+          </div>
+          {/*  */}
+          <div className={scss.right}>445456d</div>
+        </div>
       </div>
     </SubLayer>
   );
 }
+
+// =============================================================
+
+const propsForTest_quotationRemark: Tprops_quotationRemark = {
+  disabled: false,
+  label: '備註',
+  onAddClick: () => {},
+  onUpponAddClick: () => {},
+  remarkArr: [
+    {
+      value: 'test',
+      onChange: (v: string) => {},
+      onDelete: () => {},
+    },
+  ],
+};
