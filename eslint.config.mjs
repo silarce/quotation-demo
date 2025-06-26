8acd0012953e496ccafb38e9d494a247691ddb3f
Silarce <65767828+silarce@users.noreply.github.com>
2025-06-26 10:36:34 +0800

    chore:修改ESLlint規則

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,8 +65,8 @@ export default defineConfig([
 
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
-      // 'no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
     },
   },
 ]);
