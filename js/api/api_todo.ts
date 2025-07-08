cda1ff187fec972aef8d07016daa762ff6bf1dde
Silarce <65767828+silarce@users.noreply.github.com>
2025-07-08 16:15:11 +0800

    fix: 修正apiPostTodo中日期格式的錯誤，將'yyyy-MM-DD'改為'YYYY-MM-DD'

--- js/api/api_todo.ts
+++ js/api/api_todo.ts
@@ -92,8 +92,8 @@ export const apiPostTodo = async (body: TcreateTodoDto[], { callAlert = true }: 
   const api = '/todo';
 
   // 後端收的不是ISOstring，送ISOstring的話會因為時區的問題而get錯誤的日期
-  body[0].notificationDate = dayjs(body[0].notificationDate).format('yyyy-MM-DD');
-  body[0].entryDate = dayjs(body[0].entryDate).format('yyyy-MM-DD');
+  body[0].notificationDate = dayjs(body[0].notificationDate).format('YYYY-MM-DD');
+  body[0].entryDate = dayjs(body[0].entryDate).format('YYYY-MM-DD');
 
   return axi
     .post(api, body)
