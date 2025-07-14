daa8c3ec723de260b3151746c658f99fc595caef
Silarce <65767828+silarce@users.noreply.github.com>
2025-07-14 13:43:07 +0800

    chore: Btn_fong完成

--- components/global/gear/button/btn_fong/index.tsx
+++ components/global/gear/button/btn_fong/index.tsx
@@ -1,28 +1,42 @@
-import dynamic from 'next/dynamic';
-
 import classNames from 'classnames';
 
 import scss from './index.module.scss';
 
+import Icon_add from 'public/image/icon/fong/add.svg';
+import Icon_arrowDown from 'public/image/icon/fong/arrowDown.svg';
+import Icon_arrowUp from 'public/image/icon/fong/arrowUp.svg';
+import Icon_cross from 'public/image/icon/fong/cross.svg';
+import Icon_import from 'public/image/icon/fong/import.svg';
+import Icon_query from 'public/image/icon/fong/query.svg';
+import Icon_save from 'public/image/icon/fong/save.svg';
+import Icon_saveAs from 'public/image/icon/fong/saveAs.svg';
+import Icon_tempIcon from 'public/image/icon/fong/tempIcon.svg';
+import Icon_trash from 'public/image/icon/fong/trash.svg';
+import Icon_send from 'public/image/icon/fong/send.svg';
+
 // ================================================================================
 
-function Btn({
-  theme = 'basic',
+function Btn_fong({
   className,
   children,
+  //
+  theme = 'basic',
   props_icon,
   props_iconAfter,
   icon,
   iconAfter,
+  themeColor: customThemeColor,
   ...props
 }: Tprops_btn) {
-  const { className: themeClassName } = theme && lookup_theme[theme];
-  const Icon = icon !== undefined ? icon : lookup_theme[theme].Icon;
-  const IconAfter = iconAfter !== undefined ? iconAfter : lookup_theme[theme].IconAfter;
+  const { themeColor } = theme && lookup_theme[theme];
+
+  const Icon = typeof icon === 'string' ? lookup_icon[icon] : icon || lookup_theme[theme].Icon;
+  const IconAfter = typeof iconAfter === 'string' ? lookup_icon[iconAfter] : iconAfter || lookup_theme[theme].IconAfter;
+
+  const themeColorName = customThemeColor || themeColor;
 
   return (
-    <button className={classNames(scss.btn, themeClassName, className)} {...props}>
-      {/*  */}
+    <button className={classNames(scss.btn, themeColorName && scss[themeColorName], className)} {...props}>
       {Icon && (
         <div className={scss.iconContainer}>
           <Icon {...props_icon} />
@@ -36,13 +50,13 @@ function Btn({
           <IconAfter {...props_iconAfter} />
         </div>
       )}
-
-      {/*  */}
     </button>
   );
 }
 
 // ================================================================================
+
+type Tcolors = 'blue_I' | 'green_I' | 'green_II' | 'red_I' | 'brown_I';
 
 type TthemeName =
   | 'basic'
@@ -63,63 +77,80 @@ type Tprops_btn = {
   theme?: TthemeName;
   props_icon?: React.SVGProps<SVGElement>;
   props_iconAfter?: React.SVGProps<SVGElement>;
-  icon?: React.ComponentType<React.SVGProps<SVGElement>> | null;
-  iconAfter?: React.ComponentType<React.SVGProps<SVGElement>> | null;
+  icon?: React.ComponentType<React.SVGProps<SVGElement>> | null | TiconName;
+  iconAfter?: React.ComponentType<React.SVGProps<SVGElement>> | null | TiconName;
+  themeColor?: Tcolors;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 interface Ttheme {
-  className?: string;
+  themeColor?: Tcolors;
   Icon?: React.ComponentType<React.SVGProps<SVGElement>>;
-  IconAfter?: React.ComponentType<React.SVGProps<SVGElement>>; // SVG component or null
+  IconAfter?: React.ComponentType<React.SVGProps<SVGElement>>;
 }
 
+type TiconName = keyof typeof lookup_icon;
+
 // ================================================================================
+
+const lookup_icon = {
+  add: Icon_add,
+  arrowDown: Icon_arrowDown,
+  arrowUp: Icon_arrowUp,
+  cross: Icon_cross,
+  import: Icon_import,
+  query: Icon_query,
+  save: Icon_save,
+  saveAs: Icon_saveAs,
+  tempIcon: Icon_tempIcon,
+  trash: Icon_trash,
+  send: Icon_send,
+} as const;
 
 const lookup_theme: Record<TthemeName, Ttheme> = {
   basic: {},
   save: {
-    className: scss.blue_I,
-    Icon: dynamic(() => import('public/image/icon/fong/save.svg')),
+    themeColor: 'blue_I',
+    Icon: Icon_save,
   },
   add: {
-    className: scss.green_I,
-    Icon: dynamic(() => import('public/image/icon/fong/add.svg')),
+    themeColor: 'green_I',
+    Icon: Icon_add,
   },
   arrowDown: {
-    IconAfter: dynamic(() => import('public/image/icon/fong/arrowDown.svg')),
+    IconAfter: Icon_arrowDown,
   },
   arrowUp: {
-    IconAfter: dynamic(() => import('public/image/icon/fong/arrowUp.svg')),
+    IconAfter: Icon_arrowUp,
   },
   cross: {
-    className: scss.green_II,
-    Icon: dynamic(() => import('public/image/icon/fong/cross.svg')),
+    themeColor: 'green_II',
+    Icon: Icon_cross,
   },
   import: {
-    Icon: dynamic(() => import('public/image/icon/fong/import.svg')),
+    Icon: Icon_import,
   },
   query: {
-    Icon: dynamic(() => import('public/image/icon/fong/query.svg')),
+    Icon: Icon_query,
   },
 
   saveAs: {
-    Icon: dynamic(() => import('public/image/icon/fong/saveAs.svg')),
+    Icon: Icon_saveAs,
   },
   tempIcon: {
-    Icon: dynamic(() => import('public/image/icon/fong/tempIcon.svg')),
+    Icon: Icon_tempIcon,
   },
   trash: {
-    className: scss.red_I,
-    Icon: dynamic(() => import('public/image/icon/fong/trash.svg')),
+    themeColor: 'red_I',
+    Icon: Icon_trash,
   },
   send: {
-    className: scss.blue_I,
-    Icon: dynamic(() => import('public/image/icon/fong/send.svg')),
+    themeColor: 'blue_I',
+    Icon: Icon_send,
   },
   brown: {
-    className: scss.brown,
+    themeColor: 'brown_I',
   },
 };
 
-// ================================================================================
+const Btn = Btn_fong;
 export default Btn;
