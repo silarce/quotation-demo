04450123af445becbfab331fa5500f88c08d8912
Silarce <65767828+silarce@users.noreply.github.com>
2025-07-23 14:20:05 +0800

    task:應收帳款查詢-串接api

--- pages/accounting/accountsReceivableInquiry/index.tsx
+++ pages/accounting/accountsReceivableInquiry/index.tsx
@@ -1,4 +1,7 @@
-import Router from 'next/router';
+import { useMemo } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+
+import Router, { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import classNames from 'classnames';
@@ -9,29 +12,66 @@ import Table_antd, { TableProps } from 'components/global/myAntd/table';
 import { DataEntry_fong, Input, DatePicker } from 'components/global/gear/dataEntry';
 import Btn from 'components/global/gear/button/btn_fong';
 
+import { Form } from 'antd';
+
+import { getTaiwanDateStr } from 'js/utils/helpers/date/convertDate';
+
 import Icon_note from 'public/image/icon/fong/note.svg';
 
+import {
+  useApiGetAccountsReceivablesList,
+  TaccountsReceivablesList_Dto,
+} from 'js/api/api_netCore/api_accountsReceivable';
+
 export default function AccountsReceivableInquiry() {
+  const router = useRouter();
+  const query = router.query as Tquery;
+  const { keyword, createdAt, page } = router.query as Tquery;
+
+  const apiParam = useMemo(() => {
+    return {
+      filter: keyword,
+      page: Number(page) || 1,
+    };
+  }, [keyword, createdAt, page]);
+
+  const { data: accountsReceivablesArr, meta } = useApiGetAccountsReceivablesList(apiParam);
+
+  const handel_search = ({ keyword, createdAt }: { keyword?: string; createdAt?: Dayjs }) => {
+    router.replace({
+      query: {
+        ...query,
+        keyword: keyword ?? undefined,
+        createdAt: createdAt ? createdAt.toISOString() : undefined,
+      },
+    });
+  };
+
   return (
     <div>
       <div className={'pageTop flex justify-between'}>
-        <div className={classNames('flex gap-4', scss.form)}>
-          <DataEntry_fong
-            childrenWrapperProps={{
-              className: scss.input,
-            }}
-          >
-            <Input placeholder="輸入合約編號 / 客戶姓名 / 案場名稱" />
-          </DataEntry_fong>
+        <Form className={classNames('flex gap-4', scss.form)} onFinish={handel_search}>
+          <Form.Item className={scss.formItem} name="keyword">
+            <DataEntry_fong
+              childrenWrapperProps={{
+                className: scss.input,
+              }}
+            >
+              <Input placeholder="輸入合約編號 / 客戶姓名 / 案場名稱" defaultValue={keyword} />
+            </DataEntry_fong>
+          </Form.Item>
+
           <DataEntry_fong
             childrenWrapperProps={{
               className: scss.datePicker,
             }}
           >
-            <DatePicker />
+            <Form.Item className={scss.formItem} name="createdAt">
+              <DatePicker defaultValue={createdAt ? dayjs(createdAt) : undefined} />
+            </Form.Item>
           </DataEntry_fong>
           <Btn theme="query">搜索資料</Btn>
-        </div>
+        </Form>
         <div>
           <Link href={Router.pathname + '/salesInformation'}>
             <Btn theme="add">新增資料</Btn>
@@ -41,9 +81,22 @@ export default function AccountsReceivableInquiry() {
 
       <Table_antd
         columns={columns}
-        dataSource={fakeData}
+        dataSource={accountsReceivablesArr ?? undefined}
         scroll={{
           y: '550px',
+        }}
+        pagination={{
+          current: Number(page || 1),
+          pageSize: meta?.pageSize,
+          total: meta?.itemCount,
+          onChange(page) {
+            router.replace({
+              query: {
+                ...query,
+                page: page,
+              },
+            });
+          },
         }}
       />
     </div>
@@ -52,29 +105,29 @@ export default function AccountsReceivableInquiry() {
 
 // ========================================================================
 
-interface Tdata {
-  idNumber: string;
-  createdAt: string;
-  customerName: string;
-  projectName: string;
-  contractPrice: number;
-  tax: number;
-  totalPrice: number;
-  amountOfPaymentRequested: number;
-  deduction: number;
-  status: string;
+// MARK: TYPE
+
+interface Tquery {
+  keyword?: string;
+  createdAt?: string;
+
+  page?: `${number}`;
 }
 
-const columns: TableProps<Tdata>['columns'] = [
+// ========================================================================
+
+// MARK: columns
+const columns: TableProps<TaccountsReceivablesList_Dto>['columns'] = [
   {
     title: '合約編號',
-    dataIndex: 'idNumber',
+    dataIndex: 'quotationContractNumber',
     width: 150,
   },
   {
     title: '建立日期',
     dataIndex: 'createdAt',
     width: 130,
+    render: (_, record) => getTaiwanDateStr(record.createdAt),
   },
   {
     title: '客戶姓名',
@@ -88,28 +141,38 @@ const columns: TableProps<Tdata>['columns'] = [
   },
   {
     title: '合約金額',
-    dataIndex: 'contractPrice',
+    dataIndex: 'salesAmount',
     width: 150,
+    align: 'right',
+    render: (_, record) => toLocaleString(record.salesAmount),
   },
   {
     title: '稅金',
-    dataIndex: 'tax',
+    dataIndex: 'taxes',
+    align: 'right',
     width: 150,
+    render: (_, record) => toLocaleString(record.taxes),
   },
   {
     title: '總金額',
-    dataIndex: 'totalPrice',
+    dataIndex: 'totalAmount',
     width: 150,
+    align: 'right',
+    render: (_, record) => toLocaleString(record.totalAmount),
   },
   {
     title: '應收款項金額',
-    dataIndex: 'amountOfPaymentRequested',
+    dataIndex: 'requestAmount',
     width: 150,
+    align: 'right',
+    render: (_, record) => toLocaleString(record.requestAmount),
   },
   {
     title: '扣款金額',
     dataIndex: 'deduction',
     width: 150,
+    align: 'right',
+    render: (_, record) => toLocaleString(record.deduction),
   },
   {
     title: '狀態',
@@ -120,14 +183,7 @@ const columns: TableProps<Tdata>['columns'] = [
     title: '操作',
     key: 'action',
     width: 80,
-    render(record) {
-      // const href = {
-      //   pathname: Router.pathname + '/salesInformation',
-      //   query: {
-      //     id: record.id,
-      //   },
-      // };
-
+    render(_, record) {
       const href = Router.pathname + '/salesInformation';
 
       return (
@@ -141,19 +197,6 @@ const columns: TableProps<Tdata>['columns'] = [
   },
 ];
 
-const fakeData: Tdata[] = Array.from({ length: 100 }, (_, index) => {
-  const data: Tdata = {
-    idNumber: `ID-${index + 1}`,
-    createdAt: `2023/10/${(index % 30) + 1}`,
-    customerName: `客戶${index + 1}`,
-    projectName: `案場${index + 1}`,
-    contractPrice: 999999999999,
-    tax: 999999999999,
-    totalPrice: 999999999999,
-    amountOfPaymentRequested: 999999999999,
-    deduction: 999999999999,
-    status: index % 2 === 0 ? '待處理' : '已完成',
-  };
-
-  return data;
-});
+const toLocaleString = (value: number | null) => {
+  return value !== null ? '$' + value.toLocaleString() : '';
+};
