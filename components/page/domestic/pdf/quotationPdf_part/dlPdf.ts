5c42634f904254415849ad252f52ab9304def5d3
Silarce <65767828+silarce@users.noreply.github.com>
2025-07-29 09:31:49 +0800

    fix:修復報價單與派工單下載PDF功能-tailwind與html2canvas樣式衝突的問題

--- components/page/domestic/pdf/quotationPdf_part/dlPdf.ts
+++ components/page/domestic/pdf/quotationPdf_part/dlPdf.ts
@@ -25,6 +25,20 @@ const dlPdf = async ({
   let isFirst = true;
   let item;
 
+  // ----------------------------------------------------------
+  // html2canvas 和 Tailwind CSS 在 CSS 上會有部分衝突，
+  // 只要在專案中使用 Tailwind CSS，Tailwind CSS 在 preflight 時會將 img 標籤的 display 屬性設置為 display: block，
+  // 這會在 element 中引入一個換行。當然，在 頁面 render 時不會顯示出來。但當使用 html2canvas 將相同的 HTML 轉換為圖片時，這個換行就會會被渲染出來。
+
+  // 解法
+  // https://blog.subarya.me/2024/10/18/[html2canvas]%20%E4%BD%BF%E7%94%A8%20html%20%E8%BD%89%E6%88%90%20pdf%20%E6%99%82%E6%8E%A1%E7%9A%84%E5%9D%91/
+  // https://github.com/niklasvh/html2canvas/issues/2775#issuecomment-1316356991
+
+  const style = document.createElement('style');
+  document.head.appendChild(style);
+  style.sheet?.insertRule('body > div:last-child img { display: inline-block; }');
+  // ----------------------------------------------------------
+
   for (item of refPdf.current) {
     if (!item) {
       continue;
@@ -48,6 +62,11 @@ const dlPdf = async ({
     // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
     doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
   }
+
+  // -------------------------------------------------------------
+  // 接上面的 tailwind CSS 衝突問題處理
+  style.remove();
+  // -------------------------------------------------------------
 
   doc.save(`${quotationNumber}.pdf`);
   showRootLoading(false);
