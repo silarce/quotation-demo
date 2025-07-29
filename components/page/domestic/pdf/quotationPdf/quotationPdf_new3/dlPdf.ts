93f2bfd04e2df8b8030e8b978178761b03485dc9
Silarce <65767828+silarce@users.noreply.github.com>
2025-07-29 09:52:31 +0800

    fix:將所有下載PDF功能套用fixTailwindImgDisplay函式

--- components/page/domestic/pdf/quotationPdf/quotationPdf_new3/dlPdf.ts
+++ components/page/domestic/pdf/quotationPdf/quotationPdf_new3/dlPdf.ts
@@ -5,6 +5,8 @@ import dayjs from 'dayjs';
 // global gear
 import { showRootLoading } from 'components/global/gear/loadingCover/rootLoadingCover';
 
+import { fixTailwindImgDisplay } from 'js/utils/dlPdf';
+
 const dlPdf = async ({
   ref_pdf,
   fileName,
@@ -26,55 +28,38 @@ const dlPdf = async ({
   let isFirst = true;
   let item;
 
-  // ____________________________________________________________________
-  // html2canvas 和 Tailwind CSS 在 CSS 上會有部分衝突，
-  // 只要在專案中使用 Tailwind CSS，Tailwind CSS 在 preflight 時會將 img 標籤的 display 屬性設置為 display: block，
-  // 這會在 element 中引入一個換行。當然，在 頁面 render 時不會顯示出來。但當使用 html2canvas 將相同的 HTML 轉換為圖片時，這個換行就會會被渲染出來。
+  await fixTailwindImgDisplay(async () => {
+    for (item of ref_pdf.current) {
+      if (!item) {
+        continue;
+      }
 
-  // 解法
-  // https://blog.subarya.me/2024/10/18/[html2canvas]%20%E4%BD%BF%E7%94%A8%20html%20%E8%BD%89%E6%88%90%20pdf%20%E6%99%82%E6%8E%A1%E7%9A%84%E5%9D%91/
-  // https://github.com/niklasvh/html2canvas/issues/2775#issuecomment-1316356991
+      // =====================================================
 
-  const style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet?.insertRule('body > div:last-child img { display: inline-block; }');
-  // ____________________________________________________________________
+      const image = await html2canvas(item, {
+        scale: 3,
+        // 關於cookie的問題，這三個都沒用
+        // useCORS: true,
+        // allowTaint: true,
+        // credentials: true,
+      }).then((canvas) => {
+        const image = canvas.toDataURL('image/JPEG');
 
-  for (item of ref_pdf.current) {
-    if (!item) {
-      continue;
+        return image;
+      });
+
+      // =====================================================
+      if (!isFirst) {
+        doc.addPage();
+      }
+
+      isFirst = false;
+      // 留作參考
+      // doc.addImage(image, "JPEG", 0, 0, 595, 842);
+      // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
+      doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
     }
-
-    // =====================================================
-
-    const image = await html2canvas(item, {
-      scale: 3,
-      // 關於cookie的問題，這三個都沒用
-      // useCORS: true,
-      // allowTaint: true,
-      // credentials: true,
-    }).then((canvas) => {
-      const image = canvas.toDataURL('image/JPEG');
-
-      return image;
-    });
-
-    // =====================================================
-    if (!isFirst) {
-      doc.addPage();
-    }
-
-    isFirst = false;
-    // 留作參考
-    // doc.addImage(image, "JPEG", 0, 0, 595, 842);
-    // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
-    doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
-  }
-
-  // ____________________________________________________________________
-  // 接上面的 tailwind CSS 衝突問題處理
-  style.remove();
-  // ____________________________________________________________________
+  });
 
   const today = dayjs().format('YYYY-MM-DD');
   doc.save(`${fileName}_${today}.pdf`);
--- components/page/domestic/pdf/quotationPdf_part/dlPdf.ts
+++ components/page/domestic/pdf/quotationPdf_part/dlPdf.ts
@@ -3,6 +3,8 @@ import jsPDF from 'jspdf';
 
 import { showRootLoading } from 'components/global/gear/loadingCover/rootLoadingCover';
 
+import { fixTailwindImgDisplay } from 'js/utils/dlPdf';
+
 const dlPdf = async ({
   //
   refPdf,
@@ -25,48 +27,31 @@ const dlPdf = async ({
   let isFirst = true;
   let item;
 
-  // ----------------------------------------------------------
-  // html2canvas 和 Tailwind CSS 在 CSS 上會有部分衝突，
-  // 只要在專案中使用 Tailwind CSS，Tailwind CSS 在 preflight 時會將 img 標籤的 display 屬性設置為 display: block，
-  // 這會在 element 中引入一個換行。當然，在 頁面 render 時不會顯示出來。但當使用 html2canvas 將相同的 HTML 轉換為圖片時，這個換行就會會被渲染出來。
+  await fixTailwindImgDisplay(async () => {
+    for (item of refPdf.current) {
+      if (!item) {
+        continue;
+      }
 
-  // 解法
-  // https://blog.subarya.me/2024/10/18/[html2canvas]%20%E4%BD%BF%E7%94%A8%20html%20%E8%BD%89%E6%88%90%20pdf%20%E6%99%82%E6%8E%A1%E7%9A%84%E5%9D%91/
-  // https://github.com/niklasvh/html2canvas/issues/2775#issuecomment-1316356991
+      const image = await html2canvas(item, {
+        scale: 3,
+      }).then((canvas) => {
+        const image = canvas.toDataURL('image/JPEG');
 
-  const style = document.createElement('style');
-  document.head.appendChild(style);
-  style.sheet?.insertRule('body > div:last-child img { display: inline-block; }');
-  // ----------------------------------------------------------
+        return image;
+      });
 
-  for (item of refPdf.current) {
-    if (!item) {
-      continue;
+      if (!isFirst) {
+        doc.addPage();
+      }
+
+      isFirst = false;
+      // 留作參考
+      // doc.addImage(image, "JPEG", 0, 0, 595, 842);
+      // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
+      doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
     }
-
-    const image = await html2canvas(item, {
-      scale: 3,
-    }).then((canvas) => {
-      const image = canvas.toDataURL('image/JPEG');
-
-      return image;
-    });
-
-    if (!isFirst) {
-      doc.addPage();
-    }
-
-    isFirst = false;
-    // 留作參考
-    // doc.addImage(image, "JPEG", 0, 0, 595, 842);
-    // doc.addImage(image, "JPEG", 0, 0, canvas.width, canvas.height);
-    doc.addImage(image, 'JPEG', 0, 0, pageWidth, pageHeight);
-  }
-
-  // -------------------------------------------------------------
-  // 接上面的 tailwind CSS 衝突問題處理
-  style.remove();
-  // -------------------------------------------------------------
+  });
 
   doc.save(`${quotationNumber}.pdf`);
   showRootLoading(false);
