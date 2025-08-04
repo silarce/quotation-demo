a9f8eb1ef877b674f753debc922cf6795d5ba60e
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-04 13:04:59 +0800

    chore:next.config.js-移除沒有作用的property: swcMinify與future

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ dayjs.extend(timezone);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   images: {
     unoptimized: true,
@@ -29,9 +28,6 @@ const nextConfig = {
 
 module.exports = {
   ...nextConfig,
-  future: {
-    webpack5: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
