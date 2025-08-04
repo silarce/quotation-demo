838fb5a1e81e162b5fb099472caf403bfb9da6b3
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-04 14:13:34 +0800

    task:將更新Tres_apiGetARPaymentData與Tres_apiGetARPaymentDataInset並移到別的檔案

--- js/api/api_netCore/schemas/accountsReceivable.ts
+++ js/api/api_netCore/schemas/accountsReceivable.ts
@@ -222,6 +222,145 @@ interface TpaymentRequestType {
   name: string;
 }
 
+interface Tres_apiGetARPaymentData {
+  // 目前累計
+  accountsReceivables: {
+    id: string; //應收帳款id
+    accountsReceivableNumber: string; //應收帳款編號
+    sourceType: string; //來源類型
+    sourceId: string | null; //來源id
+    customerNumber: string; //客戶編號
+    customerName: string; //客戶名稱
+    companyPhone: string; //公司電話
+    companyFax: string; //公司傳真
+    salesAmount: number | null; // 銷售金額
+    taxes: number | null; //稅金
+    salesCurrency: string; //幣別
+    exchangeRate: number | null; //匯率
+    foreignCurrencyAmount: number | null; //外幣金額
+    prAmount: number | null; //已請款金額 總計
+    requestAmount: number | null; // 突然冒出來的 // 或許本來就有，只是給我文件時沒有這個?
+    collectAmount: number | null; //已收款項 總計
+    uncollectedPayment: number | null; //未收款項 (已請款未收款項)
+    totalAmount: number | null; //總金額
+    createdAt: string | null; //建立時間
+    updatedAt: string | null; //更新時間
+    createdBy: string | null; //建立人
+    updatedBy: string | null; //更新人
+    status: string | null; //狀態
+    deduction: number | null; //扣款金額
+    quotationContractNumber: string | null; //合約編號
+    projectName: string | null; //案場名稱
+    salesOrderNumber: string | null; //銷售訂單編號
+    taxId: string | null;
+    taxDeductionCategory: string | null;
+  };
+
+  // 本次請款明細
+  paymentRequest: {
+    id: string | null; //請款單Id
+
+    createdAt: string | null; //建立時間
+    createdBy: string | null; //建立人員
+    updatedAt: string | null; //更新時間
+    updatedBy: string | null; // 更新人員
+
+    accountsReceivableId: string | null; //應收帳款Id
+    paymentRequestNumber: string | null; //請款單編號
+    type: string; //請款單類型
+    period: string; //請款單期別
+
+    sourceFormType: string | null; //來源表單類型
+    sourceFormId: string | null; //來源表單Id
+
+    customerNumber: string; //客戶編號
+    customerName: string; //客戶名稱
+
+    paymentCurrency: string; //請款幣別
+    foreignCurrencyAmount: number | null; //外幣金額
+    paymentAmount: number | null; //請款金額
+    collect_amount: number | null; //已收金額,餘額在repo裡計算
+    receipt_balance: number | null; //收款餘額
+    deduction: number | null; //扣款金額
+
+    invoiceNumber: string | null; //發票號碼
+    invoiceAmount: number | null; //發票金額
+
+    //請款單沖銷明細 // 沖銷明細
+    prOffsetDetails: {
+      id: string | null; //沖銷明細Id
+      prOffsetNumber: string | null; //沖銷編號
+      accountantId: string | null; //會計收管管理Id
+      paymentRequestId: string | null; //請款單Id
+      prOffsetDate: string; //沖銷日期
+      paymentCurrency: string | null; //請款幣別
+      exchangeRate: number | null; //匯率
+      paymentAmount: number; //收款金額
+      settlementSerial: string | null; //結算序號
+      isCashierSeen: boolean | null; //出納是否已查看
+      isWorkSupervisorSeen: boolean | null; // 工作主管是否已查看
+      isManagerSeen: boolean | null; // 總經理是否已查看
+      declarationCurrency: string | null; // PostgreSQL enum 建議轉 string 處理
+      declarationExchangeRate: number | null; // 申報匯率
+      declarationCurrencyPayment: number | null; // 申報幣別收款金額
+      declarationPayment: number | null; // 申報收款金額
+      exchangeBenefits: number | null; // 匯兌利益
+      customerNumber: string | null; // 客戶編號
+      customerName: string | null; // 客戶名稱
+      fee: number | null; // 手續費
+      totalAmount: number | null; // 總金額
+      account: string | null; // 會計科目
+      createdAt: string | null; //建立時間
+      createdBy: string | null; //建立人員
+      updatedAt: string | null; //更新時間
+      updatedBy: string | null; // 更新人員
+    }[];
+  };
+  salesOrder: {
+    id: string; //銷售訂單id
+    salesOrderNumber: string; //銷售訂單編號
+    customerId: string; //客戶id
+    customerNumber: string; //客戶編號
+    customerName: string; //客戶名稱
+    constructionSite: string; //工地名稱
+    companyPhone: string; //公司電話
+    companyFax: string; //公司傳真
+    address: string; //地址
+    salesCurrency: string; //幣別
+    exchangeRate: number | null; //匯率
+    currencyAmount: number | null; //外幣金額
+    salesAmount: number | null; //銷售金額
+    taxes: number | null; //稅金
+    changedAmount: number | null; //追加減金額
+    changedTaxes: number | null; //追加減稅金
+    totalAmount: number | null; //總金額
+    createdAt: string; //建立時間
+    updatedAt: string; //更新時間
+    createdBy: string | null; //建立人員
+    updatedBy: string | null; //更新人員
+    status: number | null; //狀態
+    sourceType: string; //來源類型
+    sourceId: string | null; //來源id
+    quotationNumber: string | null; //報價單編號
+    //銷貨明細  // 工程項目明細
+    salesOrderItems: {
+      salesOrderItemId: string;
+      productName: string;
+      quantity: number | null;
+      unitPrice: number | null;
+
+      itemName: string | null;
+      sizeString: string | null;
+      prophaseCompletedQuantity: number | null; // 前期以完成
+      completedQuantity: number | null; // 本期完成
+      completedPayment: number | null; // 本期金額
+      totalCompletedQuantity: number | null; // 合計
+    }[];
+  };
+}
+
+type Tres_apiGetARPaymentDataInset = Omit<Tres_apiGetARPaymentData, 'paymentRequest'> & { paymentRequest: null };
+
 export type {
   TaccountsReceivablesList_Dto_depressed,
   TaccountsReceivablesList_Dto,
@@ -231,4 +370,6 @@ export type {
   TaccountsReceivable,
   TinsertpaymentRequest,
   TpaymentRequestType,
+  Tres_apiGetARPaymentData,
+  Tres_apiGetARPaymentDataInset,
 };
