dc7c782e12274012a62d09f77a3a52b690ae0d47
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-05 08:43:10 +0800

    task:請款單編輯/請款紀錄-修改欄位排序

--- components/page/accounting/accountsReceivableInquiry/paymentRequest/history.tsx
+++ components/page/accounting/accountsReceivableInquiry/paymentRequest/history.tsx
@@ -28,40 +28,40 @@ const HistoryDetail = () => {
   return (
     <div className={classNames('grid grid-cols-4 gap-fong ')}>
       <DataEntry_fong caption="本期請款金額" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="營業稅(5%)" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="本期合計" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
+      <div />
+      {/*  */}
       <DataEntry_fong caption="保留款(%)" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="稅" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="保留款金額" disabled={true}>
-        foooo
-      </DataEntry_fong>
-      <DataEntry_fong caption="金額總計" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="發票日期" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
+      {/*  */}
       <DataEntry_fong caption="發票號碼" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="發票金額" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="買受人" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
       <DataEntry_fong caption="統一編號" disabled={true}>
-        foooo
+        no property
       </DataEntry_fong>
     </div>
   );
