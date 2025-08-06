877371e8372ff9098124a686fcfaa637cfdf42c9
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-06 16:26:58 +0800

    task:請款單編輯/項目明細.本期金額與本次請款明細.請款金額連動

--- pages/accounting/accountsReceivableInquiry/paymentRequest.tsx
+++ pages/accounting/accountsReceivableInquiry/paymentRequest.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useMemo } from 'react';
-
 import { useRouter } from 'next/router';
+
+import Decimal from 'decimal.js';
 
 import { DeepNonNullable } from 'ts-essentials';
 
@@ -173,6 +174,22 @@ export default function PayentRequest() {
 
     // apiPostInsertPaymentRequest
   };
+
+  // ----------------------------------------------------------------------------
+
+  const completedPaymentTotal = useMemo(() => {
+    return instance_salesOrderItem.stateArr
+      .reduce((current, { completedPayment }) => {
+        return current.add(new Decimal(completedPayment || 0));
+      }, new Decimal(0))
+      .toNumber();
+  }, [instance_salesOrderItem.stateArr]);
+
+  useEffect(() => {
+    instance_paymentRequest.setPaymentAmount(`${completedPaymentTotal}`);
+  }, [completedPaymentTotal]);
+
+  // ----------------------------------------------------------------------------
 
   // MARK:RENDER
 
