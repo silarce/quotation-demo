e79828235f5bb3c3abdfd5eb2e9a92ab8af3e72d
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-12 16:14:54 +0800

    task:應收款編輯-新增欄位 外幣金額

--- pages/accounting/accountsReceivableInquiry/salesInformation.tsx
+++ pages/accounting/accountsReceivableInquiry/salesInformation.tsx
@@ -136,46 +136,73 @@ export default function SalesInformation() {
 
       <div className="border border-gray05 rounded-01 py-8 px-6">
         <div className="text-xl font-semibold mb-6">應收款</div>
-        <div className="grid grid-cols-4 gap-fong ">
+        <div className="grid grid-cols-8 gap-fong ">
           {/*  */}
 
-          <DataEntry_fong caption="合約編號" isMust={true} className="col-span-2">
+          <DataEntry_fong caption="合約編號" isMust={true} className="col-span-4">
             {quotationContractNumber}
           </DataEntry_fong>
-          <DataEntry_fong caption="案場名稱" className="col-span-2">
+          <DataEntry_fong caption="案場名稱" className="col-span-4">
             {projectName}
           </DataEntry_fong>
 
-          <DataEntry_fong caption="客戶編號" className="col-span-2">
+          <DataEntry_fong caption="客戶編號" className="col-span-4">
             {customerNumber}
           </DataEntry_fong>
-          <DataEntry_fong caption="客戶名稱" className="col-span-2">
+          <DataEntry_fong caption="客戶名稱" className="col-span-4">
             {customerName}
           </DataEntry_fong>
 
-          <DataEntry_fong caption="統一編號">{taxId}</DataEntry_fong>
-          <DataEntry_fong caption="稅別">{taxDeductionCategory}</DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="統一編號">
+            {taxId}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="稅別">
+            {taxDeductionCategory}
+          </DataEntry_fong>
           <DataEntry_fong caption="外幣">{toLocaleString(foreignCurrencyAmount)}</DataEntry_fong>
+
+          <DataEntry_fong caption="外幣金額">{'no property'}</DataEntry_fong>
+
           <DataEntry_fong caption="匯率">{exchangeRate}</DataEntry_fong>
 
-          <DataEntry_fong caption="銷售金額">
+          <DataEntry_fong className="col-span-2" caption="銷售金額">
             {<span className="text-right">{toLocaleString(salesAmount)}</span>}
           </DataEntry_fong>
-          <DataEntry_fong caption="銷售稅金">
+          <DataEntry_fong className="col-span-2" caption="銷售稅金">
             {<span className="text-right">{toLocaleString(taxes)}</span>}
           </DataEntry_fong>
-          <DataEntry_fong caption="追加減金額">{'no property'}</DataEntry_fong>
-          <DataEntry_fong caption="追加減金額稅金">{'no property'}</DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="追加減金額">
+            {'no property'}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="追加減金額稅金">
+            {'no property'}
+          </DataEntry_fong>
 
-          <DataEntry_fong caption="已收金額">{collectAmount}</DataEntry_fong>
-          <DataEntry_fong caption="扣款折讓">{deduction}</DataEntry_fong>
-          <DataEntry_fong caption="已請款總額">{prAmount}</DataEntry_fong>
-          <DataEntry_fong caption="銷售總額">{totalAmount}</DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="已收金額">
+            {collectAmount}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="扣款折讓">
+            {deduction}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="已請款總額">
+            {prAmount}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="銷售總額">
+            {totalAmount}
+          </DataEntry_fong>
 
-          <DataEntry_fong caption="合約保留款類型">{'no property'}</DataEntry_fong>
-          <DataEntry_fong caption="稅別">{'no property'}</DataEntry_fong>
-          <DataEntry_fong caption="百分比%">{'no property'}</DataEntry_fong>
-          <DataEntry_fong caption="保留款金額">{'no property'}</DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="合約保留款類型">
+            {'no property'}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="稅別">
+            {'no property'}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="百分比%">
+            {'no property'}
+          </DataEntry_fong>
+          <DataEntry_fong className="col-span-2" caption="保留款金額">
+            {'no property'}
+          </DataEntry_fong>
         </div>
         {/*  */}
         <div className="mt-10">
