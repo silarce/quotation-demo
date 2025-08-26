31baf880b748c9073f042e476c5d1ec007cc3f00
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-26 08:57:24 +0800

    task:Selector_customer變更api

--- components/composition/selectorModal/selector_customer/index.tsx
+++ components/composition/selectorModal/selector_customer/index.tsx
@@ -5,13 +5,14 @@ import Table_antd, { TableProps, metaToPageProps } from 'components/global/myAnt
 import Btn from 'components/global/gear/button/btn_fong';
 import { Container_confirm } from 'components/global/container/modal';
 import { modal_empty } from 'components/global/gear/modal/fongModal';
+import { DataEntry_fong, Input } from 'components/global/gear/dataEntry';
 
-import { TapiGetCustomersParams, useCustomers, TcustomerDto } from 'js/api/api_customer';
+import { TgetCustomerList_Dto, useApiGetCustomerList } from 'js/api/api_netCore/api_salesOrder';
 
 // ===========================================================================
 
 interface Tprops {
-  onConfirm?: (customer: TcustomerDto[]) => void;
+  onConfirm?: (customer: TgetCustomerList_Dto[]) => void;
   onCancel?: () => void;
   limit?: number;
 }
@@ -21,19 +22,21 @@ interface Tprops {
 function Selector_customer({ onConfirm, onCancel, limit = 1 }: Tprops) {
   const [page, setPage] = useState(1);
 
-  const params: TapiGetCustomersParams = useMemo(() => {
+  const [state_filter, setState_filter] = useState('');
+
+  const params = useMemo(() => {
     return {
-      populate: ['contacts'],
       page,
+      pageSize: 10,
+      filter: state_filter,
     };
-  }, [page]);
+  }, [page, state_filter]);
 
-  const { data, meta, update } = useCustomers(params);
-
-  const [selected, setSelected] = useState<TcustomerDto[]>([]);
+  const { data, meta, update } = useApiGetCustomerList({ params });
+  const [selected, setSelected] = useState<TgetCustomerList_Dto[]>([]);
 
   // --------------------------------------------------------------------------
-  const onRowClick = (record: TcustomerDto) => {
+  const onRowClick = (record: TgetCustomerList_Dto) => {
     if (limit === 1) {
       setSelected([record]);
     }
@@ -65,6 +68,20 @@ function Selector_customer({ onConfirm, onCancel, limit = 1 }: Tprops) {
 
   return (
     <Container_confirm
+      topRight={
+        <form
+          className="flex gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setState_filter(e.currentTarget.filter.value);
+          }}
+        >
+          <DataEntry_fong>
+            <Input name="filter" />
+          </DataEntry_fong>
+          <Btn theme="query">搜尋</Btn>
+        </form>
+      }
       footerRight={
         <>
           <Btn onClick={handle_cancel}>取消</Btn>
@@ -102,32 +119,37 @@ function Selector_customer({ onConfirm, onCancel, limit = 1 }: Tprops) {
 
 const selector_customer = (props?: Tprops) => {
   return modal_empty({
-    width: 1000,
+    width: 1200,
     content: <Selector_customer {...props} />,
   });
 };
 
 // ===========================================================================
 
-const columns: TableProps<TcustomerDto>['columns'] = [
+const columns: TableProps<TgetCustomerList_Dto>['columns'] = [
   {
     title: '客戶編號',
     dataIndex: 'customerNumber',
     width: 100,
-    // align: 'right',
   },
   {
     title: '客戶名稱',
     dataIndex: 'name',
-    // width: 250,
-    width: 'auto',
-    // align: 'right',
   },
   {
     title: '統一編號',
     dataIndex: 'taxId',
+    width: 100,
+  },
+  {
+    title: '電話',
+    dataIndex: 'phone',
     width: 150,
-    // align: 'right',
+  },
+  {
+    title: '傳真',
+    dataIndex: 'fax',
+    width: 150,
   },
 ];
 
