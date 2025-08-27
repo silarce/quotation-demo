caa66d6373eea15c6635817e9b4a479fe239fe13
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-27 11:48:20 +0800

    task:追加追減報價單-製作基本UI

--- pages/domestic/attachQuotationList/index.tsx
+++ pages/domestic/attachQuotationList/index.tsx
@@ -1,5 +1,13 @@
-// import Table_antd, { TableProps } from 'components/global/myAntd/table';
-import Row, { Cell } from 'components/global/gear/table/row';
+import { useRouter } from 'next/router';
+
+import classNames from 'classnames';
+
+import { Pagination } from 'antd';
+import Row, { Cell, Tprops_row, Tprops_cell } from 'components/global/gear/table/row';
+import { DataEntry_fong, Input } from 'components/global/gear/dataEntry';
+import Btn from 'components/global/gear/button/btn_fong';
+
+import scss from './index.module.scss';
 
 // ===========================================================================
 
@@ -14,58 +22,107 @@ interface TfakeData {
   projectProgress: string; //工地進度
 }
 
-interface TcolumnsItem<T extends object> {
+interface Tcolumns<T extends object> {
   key?: keyof T;
   title?: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
-  render?: (value: any, data: TfakeData) => React.ReactNode;
+  //
+  render?: (value: any, data: TfakeData, index: number) => React.ReactNode;
+}
+[];
+
+interface Tquery {
+  keyword?: string;
+  page?: string;
 }
 
-// interface Tcolumns<T extends object> {
-//   [key in keyof T]: TcolumnsItem<T>;
-// }
-
 // ===========================================================================
+
+// MARK: START
+
 export default function AttachQuotationList() {
+  const router = useRouter();
+  const query = router.query;
+  const { keyword, page = '1' } = query as Tquery;
+
   return (
     <div>
-      <div className="pageTop">
+      <div className="pageTop flex justify-between items-center">
         <div className="text-xl font-semibold">追加追減報價單</div>
+        <form
+          className="flex gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            const keyword = e.currentTarget.keyword.value;
+            router.replace({
+              query: {
+                ...query,
+                keyword,
+                page: 1,
+              },
+            });
+          }}
+        >
+          <DataEntry_fong>
+            <Input defaultValue={keyword ?? ''} name="keyword" autoComplete="off" />
+          </DataEntry_fong>
+
+          <Btn theme="query">搜尋</Btn>
+        </form>
       </div>
 
-      <div>
-        <Row fullWidth={true}>
-          {columns.map((col, index) => {
-            const { title, key, className, render } = col;
+      <div className={scss.table}>
+        <TheRow fullWidth={true} className={scss.thead}>
+          {columns.map((col) => {
+            const { title, key, className, style } = col;
 
             return (
-              <Cell key={key} className={className}>
+              <TheCell key={key} className={className} style={style}>
                 {title}
-              </Cell>
+              </TheCell>
             );
           })}
-        </Row>
+        </TheRow>
 
-        {fakeData.map((data, index) => (
-          <Row key={index} fullWidth={true}>
-            {columns.map((col, index) => {
-              const { title, key, className, render } = col;
+        {fakeData.map((data) => (
+          <TheRow key={data.id} fullWidth={true}>
+            {columns.map((col, kIndex) => {
+              const { key, className, style, render } = col;
 
               const value = key ? data[key] : undefined;
 
               return (
-                <Cell key={key || index} className={className}>
-                  {render ? render(value, data) : value}
-                </Cell>
+                <TheCell key={key || kIndex} className={className} style={style}>
+                  {render ? render(value, data, kIndex) : value}
+                </TheCell>
               );
             })}
-          </Row>
+          </TheRow>
         ))}
       </div>
+      <br />
+      <Pagination
+        current={Number(page)}
+        total={330}
+        pageSize={10}
+        align={'center'}
+        hideOnSinglePage={true}
+        showSizeChanger={false}
+        onChange={(v) => {
+          router.replace({
+            query: {
+              ...query,
+              page: v,
+            },
+          });
+        }}
+      />
     </div>
   );
 }
+
+// MARK: END
 
 // ================================================================================
 
@@ -101,79 +158,50 @@ const fakeData: TfakeData[] = [
 
 // ==============================================================================
 
-// const columns: Tcolumns<TfakeData>[] = [
-//   {
-//     title: '報價日期',
-//     key: 'quotationDate',
-//     className: 'w-[120px] border',
-//     // render: (record) => <span>{record.quotationDate}</span>,
-//   },
-//   {
-//     title: '專案名稱',
-//     key: 'projectName',
-//     className: 'w-[300px] border',
-//     // render: (record) => <span>{record.projectName}</span>,
-//   },
-//   {
-//     title: '客戶名稱',
-//     key: 'customerName',
-//     className: 'w-[150px] border',
-//     // render: (record) => <span>{record.customerName}</span>,
-//   },
-//   {
-//     title: '追加數量',
-//     key: 'quantity',
-//     className: 'w-[100px] border',
-//     // render: (record) => <span>{record.quantity}</span>,
-//   },
-//   {
-//     title: '追蹤狀態',
-//     key: 'trackProgress',
-//     className: 'w-[120px] border',
-//     // render: (record) => <span>{record.trackProgress}</span>,
-//   },
-//   {
-//     title: '工地進度',
-//     key: 'projectProgress',
-//     className: 'w-[120px] border',
-//     // render: (record) => <span>{record.projectProgress}</span>,
-//   },
-// ];
-const columns: TcolumnsItem<TfakeData>[] = [
+const columns: Tcolumns<TfakeData>[] = [
   {
     title: '報價日期',
     key: 'quotationDate',
-    className: 'w-[120px] border',
-    // render: (record) => <span>{record.quotationDate}</span>,
+    className: 'w-[120px] ',
+    style: { justifyContent: 'flex-start' },
+    render: (value, data) => <span>{data.quotationDate}</span>,
   },
   {
     title: '專案名稱',
     key: 'projectName',
-    className: 'w-[300px] border',
-    // render: (record) => <span>{record.projectName}</span>,
+    className: 'w-[300px] ',
+    style: { justifyContent: 'flex-start' },
   },
   {
     title: '客戶名稱',
     key: 'customerName',
-    className: 'w-[150px] border',
-    // render: (record) => <span>{record.customerName}</span>,
+    className: 'w-[150px] ',
+    style: { justifyContent: 'flex-start' },
   },
   {
     title: '追加數量',
     key: 'quantity',
-    className: 'w-[100px] border',
-    // render: (record) => <span>{record.quantity}</span>,
+    className: 'w-[100px] ',
+    style: { justifyContent: 'flex-start' },
   },
   {
     title: '追蹤狀態',
     key: 'trackProgress',
-    className: 'w-[120px] border',
-    // render: (record) => <span>{record.trackProgress}</span>,
+    className: 'w-[120px] ',
+    style: { justifyContent: 'flex-start' },
   },
   {
     title: '工地進度',
     key: 'projectProgress',
-    className: 'w-[120px] border',
-    // render: (record) => <span>{record.projectProgress}</span>,
+    className: 'w-[120px] ',
+    style: { justifyContent: 'flex-start' },
   },
 ];
+
+// ================================================================================
+
+const TheRow = ({ className, ...props }: Tprops_row) => <Row className={classNames(className, scss.row)} {...props} />;
+
+const TheCell = ({ className, ...props }: Tprops_cell) => (
+  <Cell className={classNames(className, scss.cell)} {...props} />
+);
