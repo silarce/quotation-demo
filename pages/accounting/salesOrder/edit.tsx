2e1d7dbf5fda9c1d9f7d5998696a5127b13a87d0
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-28 14:03:28 +0800

    refactor:銷貨單-將部分函式抽出SalesOrder以簡化SalesOrder內的實作

--- pages/accounting/salesOrder/edit.tsx
+++ pages/accounting/salesOrder/edit.tsx
@@ -51,189 +51,17 @@ export default function SalesOrder() {
   } = useApiGetSalesOrderById(query.id);
 
   const instance_salesOrder = useSalesOrder(salesOrderData);
-
   const instance_salesOrderItemArr = useSalesOrderItemArr(salesOrderData?.salesOrderItems);
 
   // ---------------------------------------------------------------------------
 
-  const createBody_post = () => {
-    const { state: state_salesOrder } = instance_salesOrder;
-    const { state: state_salesOrderItemArr } = instance_salesOrderItemArr;
-
-    const {
-      sourceType,
-      quotationNumber,
-      quotationContractNumber,
-      constructionSite,
-
-      customerId,
-      customerNumber,
-      customerName,
-      companyPhone,
-      companyFax,
-
-      salesAmount,
-      taxes,
-      totalAmount,
-      sourceType: 類別,
-      address,
-      invoiceType,
-      taxId,
-      taxDeductionCategory,
-      salesCurrency,
-      exchangeRate,
-      currencyAmount,
-    } = state_salesOrder;
-
-    const userIdNumber = userInfo?.employee?.idNumber;
-
-    const errorMessage: string[] = [];
-
-    !userIdNumber && errorMessage.push('User idNumber is undefined');
-    !sourceType && errorMessage.push('請選擇類別');
-    !salesCurrency && errorMessage.push('請選擇幣別');
-    // !exchangeRate && errorMessage.push('請輸入匯率');
-    // !currencyAmount && errorMessage.push('請輸入外幣金額');
-    !salesAmount && errorMessage.push('請輸入銷貨金額');
-    !taxes && errorMessage.push('請輸入稅額');
-    !totalAmount && errorMessage.push('請輸入銷售總總額');
-    !state_salesOrderItemArr.length && errorMessage.push('請新增銷貨明細');
-
-    if (errorMessage.length) {
-      myAlert.err({
-        title: '錯誤',
-        content: errorMessage.join('\n'),
-      });
-
-      return null;
-    }
-
-    const salesOrderItems: TsalesOrder_post_Dto['salesOrderItems'] = state_salesOrderItemArr.map((item, index) => {
-      const {
-        attachedToProductId,
-        productId,
-        productName,
-        productNumber,
-
-        quantity,
-        unitPrice,
-        amount,
-      } = item;
-
-      const salesOrderItem: TsalesOrder_post_Dto['salesOrderItems'][number] = {
-        id: null,
-        attachedToProductId,
-        salesOrderNumber: null,
-        itemNumber: `${index}`,
-
-        quantity: Number(quantity),
-        unitPrice: Number(unitPrice),
-        amount,
-        productId,
-        productName,
-        productNumber,
-      };
-
-      return salesOrderItem;
-    });
-
-    const body: TsalesOrder_post_Dto = {
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-      createdBy: userIdNumber!,
-      updatedBy: userIdNumber!,
-
-      customerId,
-      customerNumber,
-      customerName,
-      constructionSite,
-      companyPhone,
-      companyFax,
-      address,
-      salesCurrency,
-      exchangeRate: Number(exchangeRate),
-      currencyAmount: Number(currencyAmount),
-
-      salesAmount: Number(salesAmount),
-      taxes: Number(taxes),
-      changedAmount: null,
-      changedTaxes: null,
-      totalAmount: Number(totalAmount),
-
-      status: null,
-      sourceType: sourceType!,
-
-      sourceId: null,
-      quotationNumber,
-      quotationContractNumber,
-      taxId,
-      taxDeductionCategory,
-      invoiceType,
-      salesOrderItems,
-    };
-
-    return body;
-  };
-
-  const createBody_patch = () => {
-    if (!salesOrderData || !salesOrderData.salesOrderNumber) {
-      return;
-    }
-
-    const body_post = createBody_post();
-
-    if (!body_post) {
-      return body_post;
-    }
-
-    const { state: state_salesOrderItemArr } = instance_salesOrderItemArr;
-
-    const salesOrderItems = state_salesOrderItemArr.map((item, index) => {
-      const {
-        quantity,
-        unitPrice,
-        amount,
-
-        productName,
-        productNumber,
-      } = item;
-
-      const salesOrderItem: TsalesOrder_patch_Dto['salesOrderItems'][number] = {
-        attachedToProductId: item.attachedToProductId,
-        productId: item.productId,
-
-        quantity: Number(quantity),
-        unitPrice: Number(unitPrice),
-        amount,
-        productName,
-        productNumber,
-        itemNumber: `${index}`,
-
-        id: null,
-        salesOrderNumber: salesOrderData.salesOrderNumber,
-      };
-
-      return salesOrderItem;
-    });
-
-    const body: TsalesOrder_patch_Dto = {
-      ...body_post,
-      id: salesOrderData.id,
-      salesOrderNumber: salesOrderData.salesOrderNumber,
-      salesOrderItems,
-      itemNumber: '',
-    };
-
-    return body;
-  };
-
   const req_postOrPatch = async () => {
-    if (isNew) {
-      const body = createBody_post();
+    const boyd_post = createBody_post_({ userInfo, instance_salesOrder, instance_salesOrderItemArr });
 
-      if (body) {
+    if (isNew) {
+      if (boyd_post) {
         try {
-          const id = await apiPostSalesOrderData(body);
+          const id = await apiPostSalesOrderData(boyd_post);
           myAlert.success({
             title: '銷貨單已新增',
           });
@@ -246,11 +74,11 @@ export default function SalesOrder() {
         } catch (error) {}
       }
     } else {
-      const body = createBody_patch();
+      const body_patch = createBody_patch_({ body_post: boyd_post, salesOrderData, instance_salesOrderItemArr });
 
-      if (body) {
+      if (body_patch) {
         try {
-          await apiPatchSalesOrderData(body);
+          await apiPatchSalesOrderData(body_patch);
           await updateSalesOrder();
           myAlert.success({
             title: '銷貨單已更新',
@@ -259,6 +87,8 @@ export default function SalesOrder() {
       }
     }
   };
+
+  // ---------------------------------------------------------------------------
 
   const handle_salesOrderToAccountsReceivables = async () => {
     if (!salesOrderData?.id) {
@@ -275,114 +105,9 @@ export default function SalesOrder() {
     });
   };
 
-  // ---------------------------------------------------------------------------
+  const handle_importContract = () => handle_importContract_({ instance_salesOrder });
 
-  const handle_importContract = () => {
-    const { destroy } = modal_empty({
-      width: 'fit-content',
-      content: (
-        <Selector_quotation
-          onCancel={() => {
-            destroy();
-          }}
-          onConfirm={([quotation]) => {
-            if (!quotation) {
-              destroy();
-
-              return;
-            }
-
-            instance_salesOrder.clear();
-
-            const {
-              quotationNumber,
-              contractNumber,
-              projectName,
-              currency,
-              foreignTotal,
-              exchangeRate,
-              customerName,
-              customerNumber,
-              customerId,
-            } = quotation;
-
-            instance_salesOrder.setState((prev) => ({
-              ...prev,
-              quotationNumber,
-              quotationContractNumber: contractNumber || '',
-              constructionSite: projectName || '',
-
-              customerId,
-              customerName: customerName || '',
-              customerNumber,
-
-              salesCurrency: currency,
-              exchangeRate: `${exchangeRate || ''}`,
-              currencyAmount: `${foreignTotal || ''}`,
-            }));
-
-            destroy();
-          }}
-        />
-      ),
-    });
-  };
-
-  const handle_importOldProject = () => {
-    const { destroy } = selector_oldContract({
-      onConfirm: (oldContractArr) => {
-        const oldContract = oldContractArr?.[0];
-
-        if (!oldContract) {
-          destroy();
-
-          return;
-        }
-
-        const {
-          contractNumber,
-          projectName,
-          customerNumber,
-          customerName,
-          phone,
-          fax,
-          taxId,
-          taxDeductionCategory,
-          address,
-        } = oldContract;
-
-        instance_salesOrder.clear();
-
-        instance_salesOrder.setState((prev) => {
-          return {
-            ...prev,
-            quotationContractNumber: contractNumber,
-            constructionSite: projectName,
-            customerNumber,
-            customerName,
-            companyPhone: phone,
-            companyFax: fax,
-            address,
-          };
-        });
-
-        if (taxDeductionCategory) {
-          // 會觸發下方的useEffect
-          instance_salesOrder.setState((prev) => {
-            return {
-              ...prev,
-              taxDeductionCategory,
-            };
-          });
-        }
-
-        destroy();
-      },
-      onCancel() {
-        destroy();
-      },
-    });
-  };
+  const handle_importOldProject = () => handle_importOldProject_({ instance_salesOrder });
 
   const handle_reset = () => {
     modal_leave({
@@ -440,3 +165,309 @@ export default function SalesOrder() {
 
 // MARK: END
 // ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+
+// MARK:createBody_post_
+const createBody_post_ = ({
+  userInfo,
+  instance_salesOrder,
+  instance_salesOrderItemArr,
+}: {
+  userInfo: ReturnType<typeof useGlobal_userInfo>['userInfo'];
+  instance_salesOrder: ReturnType<typeof useSalesOrder>;
+  instance_salesOrderItemArr: ReturnType<typeof useSalesOrderItemArr>;
+}) => {
+  const { state: state_salesOrder } = instance_salesOrder;
+  const { state: state_salesOrderItemArr } = instance_salesOrderItemArr;
+
+  const {
+    sourceType,
+    quotationNumber,
+    quotationContractNumber,
+    constructionSite,
+
+    customerId,
+    customerNumber,
+    customerName,
+    companyPhone,
+    companyFax,
+
+    salesAmount,
+    taxes,
+    totalAmount,
+    sourceType: 類別,
+    address,
+    invoiceType,
+    taxId,
+    taxDeductionCategory,
+    salesCurrency,
+    exchangeRate,
+    currencyAmount,
+  } = state_salesOrder;
+
+  const userIdNumber = userInfo?.employee?.idNumber;
+
+  const errorMessage: string[] = [];
+
+  !userIdNumber && errorMessage.push('User idNumber is undefined');
+  !sourceType && errorMessage.push('請選擇類別');
+  !salesCurrency && errorMessage.push('請選擇幣別');
+  // !exchangeRate && errorMessage.push('請輸入匯率');
+  // !currencyAmount && errorMessage.push('請輸入外幣金額');
+  !salesAmount && errorMessage.push('請輸入銷貨金額');
+  !taxes && errorMessage.push('請輸入稅額');
+  !totalAmount && errorMessage.push('請輸入銷售總總額');
+  !state_salesOrderItemArr.length && errorMessage.push('請新增銷貨明細');
+
+  if (errorMessage.length) {
+    myAlert.err({
+      title: '錯誤',
+      content: errorMessage.join('\n'),
+    });
+
+    return null;
+  }
+
+  const salesOrderItems: TsalesOrder_post_Dto['salesOrderItems'] = state_salesOrderItemArr.map((item, index) => {
+    const {
+      attachedToProductId,
+      productId,
+      productName,
+      productNumber,
+
+      quantity,
+      unitPrice,
+      amount,
+    } = item;
+
+    const salesOrderItem: TsalesOrder_post_Dto['salesOrderItems'][number] = {
+      id: null,
+      attachedToProductId,
+      salesOrderNumber: null,
+      itemNumber: `${index}`,
+
+      quantity: Number(quantity),
+      unitPrice: Number(unitPrice),
+      amount,
+      productId,
+      productName,
+      productNumber,
+    };
+
+    return salesOrderItem;
+  });
+
+  const body: TsalesOrder_post_Dto = {
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    createdBy: userIdNumber!,
+    updatedBy: userIdNumber!,
+
+    customerId,
+    customerNumber,
+    customerName,
+    constructionSite,
+    companyPhone,
+    companyFax,
+    address,
+    salesCurrency,
+    exchangeRate: Number(exchangeRate),
+    currencyAmount: Number(currencyAmount),
+
+    salesAmount: Number(salesAmount),
+    taxes: Number(taxes),
+    changedAmount: null,
+    changedTaxes: null,
+    totalAmount: Number(totalAmount),
+
+    status: null,
+    sourceType: sourceType!,
+
+    sourceId: null,
+    quotationNumber,
+    quotationContractNumber,
+    taxId,
+    taxDeductionCategory,
+    invoiceType,
+    salesOrderItems,
+  };
+
+  return body;
+};
+
+// MARK:createBody_patch_
+const createBody_patch_ = ({
+  body_post,
+  salesOrderData,
+  instance_salesOrderItemArr,
+}: {
+  body_post: ReturnType<typeof createBody_post_>;
+  salesOrderData: ReturnType<typeof useApiGetSalesOrderById>['data'];
+  instance_salesOrderItemArr: ReturnType<typeof useSalesOrderItemArr>;
+}) => {
+  if (!salesOrderData || !salesOrderData.salesOrderNumber) {
+    return;
+  }
+
+  if (!body_post) {
+    return body_post;
+  }
+
+  const { state: state_salesOrderItemArr } = instance_salesOrderItemArr;
+
+  const salesOrderItems = state_salesOrderItemArr.map((item, index) => {
+    const {
+      quantity,
+      unitPrice,
+      amount,
+
+      productName,
+      productNumber,
+    } = item;
+
+    const salesOrderItem: TsalesOrder_patch_Dto['salesOrderItems'][number] = {
+      attachedToProductId: item.attachedToProductId,
+      productId: item.productId,
+
+      quantity: Number(quantity),
+      unitPrice: Number(unitPrice),
+      amount,
+      productName,
+      productNumber,
+      itemNumber: `${index}`,
+
+      id: null,
+      salesOrderNumber: salesOrderData.salesOrderNumber,
+    };
+
+    return salesOrderItem;
+  });
+
+  const body: TsalesOrder_patch_Dto = {
+    ...body_post,
+    id: salesOrderData.id,
+    salesOrderNumber: salesOrderData.salesOrderNumber,
+    salesOrderItems,
+    itemNumber: '',
+  };
+
+  return body;
+};
+
+// MARK:handle_importContract_
+
+const handle_importContract_ = ({ instance_salesOrder }: { instance_salesOrder: ReturnType<typeof useSalesOrder> }) => {
+  const { destroy } = modal_empty({
+    width: 'fit-content',
+    content: (
+      <Selector_quotation
+        onCancel={() => {
+          destroy();
+        }}
+        onConfirm={([quotation]) => {
+          if (!quotation) {
+            destroy();
+
+            return;
+          }
+
+          instance_salesOrder.clear();
+
+          const {
+            quotationNumber,
+            contractNumber,
+            projectName,
+            currency,
+            foreignTotal,
+            exchangeRate,
+            customerName,
+            customerNumber,
+            customerId,
+          } = quotation;
+
+          instance_salesOrder.setState((prev) => ({
+            ...prev,
+            quotationNumber,
+            quotationContractNumber: contractNumber || '',
+            constructionSite: projectName || '',
+
+            customerId,
+            customerName: customerName || '',
+            customerNumber,
+
+            salesCurrency: currency,
+            exchangeRate: `${exchangeRate || ''}`,
+            currencyAmount: `${foreignTotal || ''}`,
+          }));
+
+          destroy();
+        }}
+      />
+    ),
+  });
+};
+
+// MARK:handle_importOldProject_
+
+const handle_importOldProject_ = ({
+  instance_salesOrder,
+}: {
+  instance_salesOrder: ReturnType<typeof useSalesOrder>;
+}) => {
+  const { destroy } = selector_oldContract({
+    onConfirm: (oldContractArr) => {
+      const oldContract = oldContractArr?.[0];
+
+      if (!oldContract) {
+        destroy();
+
+        return;
+      }
+
+      const {
+        contractNumber,
+        projectName,
+        customerNumber,
+        customerName,
+        phone,
+        fax,
+        taxId,
+        taxDeductionCategory,
+        address,
+      } = oldContract;
+
+      instance_salesOrder.clear();
+
+      instance_salesOrder.setState((prev) => {
+        return {
+          ...prev,
+          quotationContractNumber: contractNumber,
+          constructionSite: projectName,
+          customerNumber,
+          customerName,
+          companyPhone: phone,
+          companyFax: fax,
+          address,
+        };
+      });
+
+      if (taxDeductionCategory) {
+        // 會觸發下方的useEffect
+        instance_salesOrder.setState((prev) => {
+          return {
+            ...prev,
+            taxDeductionCategory,
+          };
+        });
+      }
+
+      destroy();
+    },
+    onCancel() {
+      destroy();
+    },
+  });
+};
