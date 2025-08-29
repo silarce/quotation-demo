f1de67f3ef82e8b41c19969e757454bd1308b4d9
Silarce <65767828+silarce@users.noreply.github.com>
2025-08-29 14:21:24 +0800

    task:應收款列表-新增三個tab，目前api不支援

--- pages/accounting/accountsReceivableInquiry/index.tsx
+++ pages/accounting/accountsReceivableInquiry/index.tsx
@@ -1,13 +1,14 @@
 import { useMemo } from 'react';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import Router, { useRouter } from 'next/router';
 import Link from 'next/link';
 import classNames from 'classnames';
 
 import Table_antd, { TableProps } from 'components/global/myAntd/table';
-import { DataEntry_fong, Input, DatePicker } from 'components/global/gear/dataEntry';
+import { DataEntry_fong, Input } from 'components/global/gear/dataEntry';
 import Btn from 'components/global/gear/button/btn_fong';
-import { Form, Spin } from 'antd';
+
+import Tab from 'components/global/gear/button/tab';
 
 import {
   useApiGetAccountsReceivablesList,
@@ -18,17 +19,28 @@ import { getTaiwanDateStr, checkIsYMD } from 'js/utils/helpers/date/convertDate'
 import Icon_note from 'public/image/icon/fong/note.svg';
 import scss from './index.module.scss';
 
+// ==========================================================================
+
+interface Tquery {
+  keyword?: string;
+  page?: `${number}`;
+  paymentType?: string;
+}
+
+// ==========================================================================
+
 export default function AccountsReceivableInquiry() {
   const router = useRouter();
   const query = router.query as Tquery;
-  const { keyword, page } = router.query as Tquery;
+  const { keyword, page = '1', paymentType = '匯款' } = router.query as Tquery;
 
   const apiParam = useMemo(() => {
     return {
       filter: keyword,
-      page: Number(page) || 1,
+      page: Number(page) || 1, // 防範NaN
+      paymentType, // api還未支援
     };
-  }, [keyword, page]);
+  }, [keyword, page, paymentType]);
 
   const { data: accountsReceivablesArr, meta, isFetching } = useApiGetAccountsReceivablesList(apiParam);
 
@@ -37,7 +49,6 @@ export default function AccountsReceivableInquiry() {
   // ---------------------------------------------------------------------------
 
   const handel_search = (keyword: string) => {
-    // const isKeywordIsDate = !!keyword && dayjs(keyword).isValid();
     const isKeywordIsDate = !!checkIsYMD(keyword);
 
     if (isKeywordIsDate) {
@@ -52,76 +63,81 @@ export default function AccountsReceivableInquiry() {
     });
   };
 
+  const handle_paymentTypeChange = (paymentType: string) => {
+    router.replace({
+      query: {
+        ...query,
+        paymentType,
+        page: '1', // 換tab時，頁數回到1
+      },
+    });
+  };
+
   // MARK: RENDER
 
   return (
     <div>
       <div className={'pageTop flex justify-between'}>
-        <form
-          //
-          className={classNames('flex gap-4 items-center', scss.form)}
-          onSubmit={(e) => {
-            e.preventDefault();
-            handel_search(e.currentTarget.keyword.value as string);
-          }}
-        >
+        <div className="flex gap-4 items-center">
           <div className="text-base font-semibold">應收款列表</div>
-
-          <DataEntry_fong
-            childrenWrapperProps={{
-              className: scss.input,
+          <TheTab tabName="匯款" />
+          <TheTab tabName="票據" />
+          <TheTab tabName="現金" />
+        </div>
+        <div className="flex gap-4">
+          <form
+            className={classNames('flex gap-4 items-center', scss.form)}
+            onSubmit={(e) => {
+              e.preventDefault();
+              handel_search(e.currentTarget.keyword.value as string);
             }}
           >
-            <Input
-              name="keyword"
-              placeholder="輸入合約編號 / 客戶姓名 / 案場名稱 / 建立日期"
-              defaultValue={defaultSearchValue}
-            />
-          </DataEntry_fong>
+            <DataEntry_fong
+              childrenWrapperProps={{
+                className: scss.input,
+              }}
+            >
+              <Input
+                name="keyword"
+                placeholder="輸入合約編號 / 客戶姓名 / 案場名稱 / 建立日期"
+                defaultValue={defaultSearchValue}
+              />
+            </DataEntry_fong>
 
-          <Btn theme="query">搜索資料</Btn>
-        </form>
-        <div>
+            <Btn theme="query">搜索資料</Btn>
+          </form>
           <Link href={Router.pathname + '/salesInformation'}>
             <Btn theme="add">新增資料</Btn>
           </Link>
         </div>
       </div>
 
-      <Spin spinning={isFetching} delay={300}>
-        <Table_antd
-          columns={columns}
-          dataSource={accountsReceivablesArr ?? undefined}
-          scroll={{
-            y: '550px',
-          }}
-          pagination={{
-            current: Number(page || 1),
-            pageSize: meta?.pageSize,
-            total: meta?.itemCount,
-            onChange(page) {
-              router.replace({
-                query: {
-                  ...query,
-                  page: page,
-                },
-              });
-            },
-          }}
-        />
-      </Spin>
+      <Table_antd
+        columns={columns}
+        dataSource={accountsReceivablesArr ?? undefined}
+        scroll={{
+          y: '550px',
+        }}
+        pagination={{
+          current: Number(page || 1),
+          pageSize: meta?.pageSize,
+          total: meta?.itemCount,
+          onChange(page) {
+            router.replace({
+              query: {
+                ...query,
+                page: page,
+              },
+            });
+          },
+        }}
+        loading={{
+          spinning: isFetching,
+          delay: 300,
+        }}
+      />
     </div>
   );
-}
-
-// ========================================================================
-
-// MARK: TYPE
-
-interface Tquery {
-  keyword?: string;
-
-  page?: `${number}`;
 }
 
 // ========================================================================
@@ -226,4 +242,28 @@ const columns: TableProps<TaccountsReceivablesList_Dto>['columns'] = [
 
 const toLocaleString = (value: number | null) => {
   return value !== null ? '$' + value.toLocaleString() : '';
+};
+
+// ==========================================================================
+
+const TheTab = ({ tabName }: { tabName: string }) => {
+  const router = useRouter();
+  const query = router.query as Tquery;
+  const { paymentType } = router.query as Tquery;
+
+  const handle_paymentTypeChange = () => {
+    router.replace({
+      query: {
+        ...query,
+        paymentType: tabName,
+        page: '1', // 換tab時，頁數回到1
+      },
+    });
+  };
+
+  return (
+    <Tab active={paymentType === tabName} onClick={handle_paymentTypeChange}>
+      {tabName}
+    </Tab>
+  );
 };
