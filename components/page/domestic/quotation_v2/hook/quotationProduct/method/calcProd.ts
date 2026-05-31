0c85c61b4e49ed534b214d6182623f5c1b59844e
Silarce <silarceanoux@gmail.com>
2026-05-31 15:21:04 +0800

    fix: 修正ESLint提示警告

--- components/page/domestic/quotation_v2/hook/quotationProduct/method/calcProd.ts
+++ components/page/domestic/quotation_v2/hook/quotationProduct/method/calcProd.ts
@@ -24,7 +24,11 @@ const calcProdTotalPrice = ({
     const { quantity, price } = data_prod;
     const dualPrice = new Decimal(price || 0).mul(quantity || 0).toNumber();
     const unitPrice = new Decimal(price || 0).mul(priceDiscount_percent).toDecimalPlaces(0).toNumber();
-    const totalPrice = new Decimal(unitPrice).mul(quantity || 0).toDecimalPlaces(0).toNumber();
+    const totalPrice = new Decimal(unitPrice)
+      .mul(quantity || 0)
+      .toDecimalPlaces(0)
+      .toNumber();
+
     return {
       price: `${price}` as `${number}`,
       dualPrice: `${dualPrice}` as `${number}`,
@@ -46,7 +50,10 @@ const calcProdTotalPrice = ({
   });
 
   Object.values(data_componentDict).forEach((component) => {
-    if (!component) return;
+    if (!component) {
+      return;
+    }
+
     const { quantity: q, price } = component;
     const { dualPrice, totalPrice } = calcAllPrice({
       price: price || 0,
@@ -57,17 +64,11 @@ const calcProdTotalPrice = ({
     totalPriceTotal_com_decimal = totalPriceTotal_com_decimal.add(totalPrice);
   });
 
-  const price = new Decimal(0)
-    .add(dualPriceTotal_acce_decimal)
-    .add(dualPriceTotal_com_decimal)
-    .toNumber();
+  const price = new Decimal(0).add(dualPriceTotal_acce_decimal).add(dualPriceTotal_com_decimal).toNumber();
 
   const dualPrice = new Decimal(price).mul(quantity || 0).toNumber();
 
-  const unitPrice = new Decimal(0)
-    .add(totalPriceTotal_acce_decimal)
-    .add(totalPriceTotal_com_decimal)
-    .toNumber();
+  const unitPrice = new Decimal(0).add(totalPriceTotal_acce_decimal).add(totalPriceTotal_com_decimal).toNumber();
 
   const totalPrice = new Decimal(unitPrice).mul(quantity || 0).toNumber();
 
@@ -98,7 +99,11 @@ const calcAndRenewAllProdPrice_sideEffect = ({
     Object.entries(data_componentDict).forEach(([_key, com]) => {
       const key = _key as keyof typeof data_componentDict;
       const theComponent = data_componentDict[key];
-      if (!theComponent) return;
+
+      if (!theComponent) {
+        return;
+      }
+
       const { unitPrice, totalPrice } = calcAllPrice({
         price: com!.price || 0,
         quantity: com!.quantity || 0,
@@ -158,7 +163,10 @@ const calcAllPrice = ({
 }) => {
   const dualPrice = new Decimal(price || 0).mul(quantity).toDecimalPlaces(0).toNumber();
   const unitPrice = new Decimal(price || 0).mul(priceDiscount_percent).toDecimalPlaces(0).toNumber();
-  const totalPrice = new Decimal(unitPrice).mul(quantity || 0).toDecimalPlaces(0).toNumber();
+  const totalPrice = new Decimal(unitPrice)
+    .mul(quantity || 0)
+    .toDecimalPlaces(0)
+    .toNumber();
 
   return { price, dualPrice, unitPrice, totalPrice };
 };
@@ -173,6 +181,7 @@ const calcPriceDiscount_percent = ({
 }) => {
   const prodDiscount_percent = new Decimal(prodDiscount || 0).div(100).toNumber();
   const quotationDiscount_percent = new Decimal(quotationDiscount || 0).div(100).toNumber();
+
   return new Decimal(prodDiscount_percent).mul(quotationDiscount_percent).toNumber();
 };
 
@@ -182,30 +191,28 @@ function calcProdAllTotal({ state_prodDict }: { state_prodDict: TstateProdDict }
   Object.values(state_prodDict).forEach((prod) => {
     total_d = total_d.add(prod.data_prod.totalPrice || 0);
   });
+
   return total_d.toNumber();
 }
 
 const calcQtyModify = ({ modifyedProduct }: { modifyedProduct: TstateProd['modifyedProduct'] }) => {
-  if (!modifyedProduct) return 0;
+  if (!modifyedProduct) {
+    return 0;
+  }
+
   return Object.values(modifyedProduct)
     .reduce((current, prod) => current.add((prod as any).quantity || 0), new Decimal(0))
     .toNumber();
 };
 
-const calcQtyReduceModified = ({
-  stateProd,
-}: {
-  stateProd: Pick<TstateProd, 'qty_reduce' | 'modifyedProduct'>;
-}) => {
+const calcQtyReduceModified = ({ stateProd }: { stateProd: Pick<TstateProd, 'qty_reduce' | 'modifyedProduct'> }) => {
   const qty_reduce = Number(stateProd.qty_reduce || 0);
   const qty_modifyed = calcQtyModify({ modifyedProduct: stateProd.modifyedProduct });
+
   return new Decimal(qty_reduce).add(qty_modifyed).toNumber();
 };
 
-const calcProdRemain = ({
-  stateProd,
-  prodSource,
-}: XOR<{ stateProd: TstateProd }, { prodSource: TprodSource }>) => {
+const calcProdRemain = ({ stateProd, prodSource }: XOR<{ stateProd: TstateProd }, { prodSource: TprodSource }>) => {
   let qty_reduce: number | `${number}` = 0;
   let modifyedProduct: any = {};
   let quantity: number | `${number}` = 0;
@@ -222,13 +229,18 @@ const calcProdRemain = ({
   }
 
   const qty_modify = calcQtyModify({ modifyedProduct });
-  return new Decimal(quantity || 0).minus(qty_reduce || 0).minus(qty_modify || 0).toNumber();
+
+  return new Decimal(quantity || 0)
+    .minus(qty_reduce || 0)
+    .minus(qty_modify || 0)
+    .toNumber();
 };
 
 const calcProdDeductedPrice = ({ stateProd }: { stateProd: TstateProd }) => {
   const { qty_reduce, modifyedProduct } = stateProd;
   const qty_modify = calcQtyModify({ modifyedProduct });
   const qty = new Decimal(qty_reduce || 0).add(qty_modify || 0);
+
   return new Decimal(stateProd.data_prod.unitPrice || 0).mul(qty).mul(-1).toNumber();
 };
 
